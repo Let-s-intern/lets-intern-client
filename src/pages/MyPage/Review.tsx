b9a1b803aa15ef2a5302ca4581e5bc3358a7a80b
minseo-hong <mshong1007@gmail.com>
2023-11-10 03:55:04 +0900

    design: 여러가지 수정사항 반영

--- src/pages/MyPage/Review.tsx
+++ src/pages/MyPage/Review.tsx
@@ -4,7 +4,7 @@ import programs from '../../data/programs.json';
 
 const Review = () => {
   return (
-    <div className="mt-7">
+    <>
       <section>
         <SectionTitle>후기를 기다리고 있어요</SectionTitle>
         <ProgramListSlider programs={programs} cardType="참여 완료" />
@@ -13,7 +13,7 @@ const Review = () => {
         <SectionTitle className="mt-10">작성한 후기 확인하기</SectionTitle>
         <ProgramListSlider programs={programs} cardType="참여 완료" />
       </section>
-    </div>
+    </>
   );
 };
 
