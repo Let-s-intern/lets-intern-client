1bcb4e64189f04bcd95f0cb079b8c4f75c63ce45
minseo-hong <77859988+minseo-hong@users.noreply.github.com>
2023-12-26 18:11:02 +0900

    Revert "[Feat] 카카오 소셜 로그인 기능 추가"

--- src/components/Admin/Review/TableBody.tsx
+++ src/components/Admin/Review/TableBody.tsx
@@ -13,7 +13,7 @@ const TableBody = ({ programList, copyReviewCreateLink }: TableBodyProps) => {
   return (
     <thead>
       {programList.map((program: any) => (
-        <tr key={program.id}>
+        <tr>
           <TD whiteSpace="wrap">{program.title}</TD>
           <TD>{formatDateString(program.announcementDate)}</TD>
           <TD>
