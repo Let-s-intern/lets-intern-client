d0cd6cf7edb371292d9c0aae0e576b39dc51542c
minseo-hong <mshong1007@gmail.com>
2024-01-05 18:50:04 +0900

    fix: 모바일에서 버튼 위 선이 안 보이는 오류 수정

--- src/components/About/Section/BottomSection.tsx
+++ src/components/About/Section/BottomSection.tsx
@@ -5,7 +5,7 @@ import './BottomSection.scss';
 const BottomSection = () => {
   return (
     <section className="bottom-section">
-      <Link to="/" className="next-button">
+      <Link to="/" className="next-link-button">
         선순환에 동참하기
       </Link>
     </section>
