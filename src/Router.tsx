c40136ff7734595590ab68bd87f5eba37326512f
minseo-hong <mshong1007@gmail.com>
2024-01-26 01:13:38 +0900

    [Refactor] 홈페이지 컴포넌트들 리팩토링

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './components/Home/Home';
+import Home from './components/Home';
 import About from './components/About/About';
 import Programs from './components/Programs/Programs';
 import ProgramDetail from './components/ProgramDetail/ProgramDetail';
--- src/components/Home/components/ReviewSection/index.tsx
+++ src/components/Home/components/ReviewSection/index.tsx
@@ -1,37 +1,37 @@
-import Review from './Review';
-import reviews from '../../../data/home-reviews.json';
+import Review from './components/Review';
+import reviews from '../../../../data/home-reviews.json';
 
-import './ReviewSection.scss';
+import styles from './styles.module.scss';
 
 const ReviewSection = () => {
   return (
-    <section className="review-section">
-      <h2 className="section-title">
+    <section className="review-section overflow-hidden bg-[#232233] p-6">
+      <h2 className="mx-auto max-w-5xl text-[1.75rem] font-semibold text-white">
         참여자 후기로
         <br />
         증명합니다.
       </h2>
-      <div className="bottom-content review-container">
-        <div className="review-group">
-          <div className="row">
-            <div className="row-item">
+      <div className="bottom-content review-container mt-6">
+        <div className="review-group flex flex-col gap-4">
+          <div className={styles.row}>
+            <div className={styles.item}>
               {reviews.slice(0, 9).map((review, index) => (
                 <Review review={review} key={index} />
               ))}
             </div>
-            <div className="row-item">
+            <div className={styles.item}>
               {reviews.slice(0, 9).map((review, index) => (
                 <Review review={review} key={index} />
               ))}
             </div>
           </div>
-          <div className="row">
-            <div className="row-item">
+          <div className={styles.row}>
+            <div className={styles.item}>
               {reviews.slice(9).map((review, index) => (
                 <Review review={review} key={index} />
               ))}
             </div>
-            <div className="row-item">
+            <div className={styles.item}>
               {reviews.slice(9).map((review, index) => (
                 <Review review={review} key={index} />
               ))}
