4a9ed448b7a6552383a283412bde920ae8208cb2
minseo-hong <mshong1007@gmail.com>
2024-02-07 17:22:26 +0900

    [Design] 챌린지 어드민 공지사항 등록 모달 추가

--- src/components/admin/challenge/home/section/NoticeSection.tsx
+++ src/components/admin/challenge/home/section/NoticeSection.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from 'react-icons/io';
 
@@ -5,53 +7,59 @@ import RoundedBox from '../box/RoundedBox';
 import NoticeItem from '../item/NoticeItem';
 import SectionHeading from '../heading/SectionHeading';
 import Button from '../../ui/button/Button';
+import NoticeEditorModal from '../../ui/modal/NoticeEditorModal';
 
 const NoticeSection = () => {
+  const [isModalShown, setIsModalShown] = useState(false);
+
   return (
-    <RoundedBox
-      as="section"
-      className="flex w-[55%] flex-col justify-between px-8 py-6"
-    >
-      <div className="flex flex-col gap-4">
-        <div className="flex items-center justify-between">
-          <SectionHeading>공지사항</SectionHeading>
-          <Button>새 공지 등록</Button>
-        </div>
-        <div>
-          <ul className="flex flex-col gap-2">
-            <NoticeItem
-              title="공지사항을 작성해주세요. 길어지면 이렇게 해주시면 됩니다."
-              date="2024.01.26"
-            />
-            <NoticeItem
-              title="공지사항을 작성해주세요. 길어지면 이렇게 해주시면 됩니다."
-              date="2024.01.26"
-            />
-            <NoticeItem
-              title="공지사항을 작성해주세요. 길어지면 이렇게 해주시면 됩니다."
-              date="2024.01.26"
-            />
-          </ul>
-        </div>
-      </div>
-      <div className="flex items-center justify-center gap-5">
-        <div className="w-20" />
-        <div className="flex items-center gap-2">
-          <div className="flex items-center gap-3 text-sm">
-            <span className="cursor-pointer font-medium">1</span>
-            <span className="cursor-pointer">2</span>
+    <>
+      <RoundedBox
+        as="section"
+        className="flex w-[55%] flex-col justify-between px-8 py-6"
+      >
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-between">
+            <SectionHeading>공지사항</SectionHeading>
+            <Button onClick={() => setIsModalShown(true)}>새 공지 등록</Button>
           </div>
-          <span className="cursor-pointer">
-            <i>
-              <IoIosArrowForward />
-            </i>
-          </span>
+          <div>
+            <ul className="flex flex-col gap-2">
+              <NoticeItem
+                title="공지사항을 작성해주세요. 길어지면 이렇게 해주시면 됩니다."
+                date="2024.01.26"
+              />
+              <NoticeItem
+                title="공지사항을 작성해주세요. 길어지면 이렇게 해주시면 됩니다."
+                date="2024.01.26"
+              />
+              <NoticeItem
+                title="공지사항을 작성해주세요. 길어지면 이렇게 해주시면 됩니다."
+                date="2024.01.26"
+              />
+            </ul>
+          </div>
         </div>
-        <Link to="/admin/challenge/notice" className="w-20 text-sm">
-          전체보기
-        </Link>
-      </div>
-    </RoundedBox>
+        <div className="flex items-center justify-center gap-5">
+          <div className="w-20" />
+          <div className="flex items-center gap-2">
+            <div className="flex items-center gap-3 text-sm">
+              <span className="cursor-pointer font-medium">1</span>
+              <span className="cursor-pointer">2</span>
+            </div>
+            <span className="cursor-pointer">
+              <i>
+                <IoIosArrowForward />
+              </i>
+            </span>
+          </div>
+          <Link to="/admin/challenge/notice" className="w-20 text-sm">
+            전체보기
+          </Link>
+        </div>
+      </RoundedBox>
+      {isModalShown && <NoticeEditorModal setIsModalShown={setIsModalShown} />}
+    </>
   );
 };
 
--- src/components/admin/challenge/ui/button/Button.tsx
+++ src/components/admin/challenge/ui/button/Button.tsx
@@ -5,9 +5,10 @@ interface Props {
   children: React.ReactNode;
   className?: string;
   to?: string;
+  onClick?: () => void;
 }
 
-const Button = ({ children, className, to }: Props) => {
+const Button = ({ children, className, to, onClick }: Props) => {
   const buttonStyle = clsx(
     'rounded border border-zinc-600 px-4 py-[2px] text-xs',
     className,
@@ -15,13 +16,17 @@ const Button = ({ children, className, to }: Props) => {
 
   if (to) {
     return (
-      <Link to={to} className={clsx(buttonStyle, 'block')}>
+      <Link to={to} className={clsx(buttonStyle, 'block')} onClick={onClick}>
         {children}
       </Link>
     );
   }
 
-  return <button className={buttonStyle}>{children}</button>;
+  return (
+    <button className={buttonStyle} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
