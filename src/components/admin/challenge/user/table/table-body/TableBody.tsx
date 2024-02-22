6054c590d8e96a2059c4bac1667a9050cd1b39ee
minseo-hong <mshong1007@gmail.com>
2024-02-22 18:58:22 +0900

    [Design] 챌린지 어드민 참여자 페이지 제작 및 API 연결

--- src/components/admin/challenge/user/table/table-body/TableBody.tsx
+++ src/components/admin/challenge/user/table/table-body/TableBody.tsx
@@ -1,18 +1,39 @@
+import { useState } from 'react';
 import TableBodyRow from './TableBodyRow';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+import axios from '../../../../../../utils/axios';
 
 const TableBody = () => {
+  const params = useParams();
+  const [applicationList, setApplicationList] = useState<any>();
+
+  const getApplicationList = useQuery({
+    queryKey: ['application', 'admin', 'challenge', params.programId],
+    queryFn: async () => {
+      const res = await axios.get(
+        `/application/admin/challenge/${params.programId}`,
+      );
+      const data = res.data;
+      setApplicationList(data.applicationList);
+      return data;
+    },
+  });
+
+  const isLoading =
+    getApplicationList.isLoading || applicationList === undefined;
+
+  if (isLoading) {
+    return <></>;
+  }
+
   return (
     <div className="flex flex-col">
-      {Array.from({ length: 4 }, (_, index) => index + 1).map((th) => (
+      {applicationList.map((application: any) => (
         <TableBodyRow
-          key={th}
-          name="송민제"
-          isMember={true}
-          school="렛인대 전공 4학년"
-          email="minje0403@gmail.com"
-          tel="010-1111-1111"
-          inflowPath="인스타그램"
-          account="하나은행 123-455678-102848"
+          key={application.applicationId}
+          application={application}
         />
       ))}
     </div>
