6bfe6b198064e5c2fe6231294cbfb5939f7d5f92
minseo-hong <mshong1007@gmail.com>
2024-03-03 19:30:14 +0900

    [Design] 반려 미션 재제출 시 뱃지 지각으로 표시되도록 수정

--- src/utils/valid.ts
+++ src/utils/valid.ts
@@ -1,17 +1,14 @@
-// 정규식을 이용한 휴대폰 번호 유효성 검사
 const isValidPhoneNumber = (phoneNumber: string) => {
   const regex = /^01[0-9]-[0-9]{4}-[0-9]{4}$/;
 
   return regex.test(phoneNumber);
 };
 
-// 정규식을 이용한 이메일 유효성 검사
 const isValidEmail = (email: string) => {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return regex.test(email);
 };
 
-// 정규식을 이용한 비밀번호 유효성 검사
 const isValidPassword = (password: string) => {
   const regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\W).{8,}$/;
   return regex.test(password);
