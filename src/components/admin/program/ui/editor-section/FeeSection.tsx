af9dff64e8014151da6c998951fa9a4029adeea3
minseo-hong <mshong1007@gmail.com>
2024-04-02 13:05:30 +0900

    [Design] 프로그램 등록 페이지에 할인 금액 입력란 추가

--- src/components/admin/program/ui/editor-section/FeeSection.tsx
+++ src/components/admin/program/ui/editor-section/FeeSection.tsx
@@ -75,6 +75,15 @@ const FeeSection = ({ values, setValues }: Props) => {
               setValues({ ...values, accountNumber: e.target.value })
             }
           />
+          <Input
+            label="할인 금액"
+            type="number"
+            // value={values.accountNumber ? values.accountNumber : ''}
+            // placeholder="입금계좌 번호를 입력하세요"
+            // onChange={(e: any) =>
+            //   setValues({ ...values, accountNumber: e.target.value })
+            // }
+          />
           <DateTimeControl>
             <DateTimeLabel htmlFor="feeDueDate">입금 마감 기한</DateTimeLabel>
             <input
