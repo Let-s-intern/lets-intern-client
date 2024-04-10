3ff50daa7d87b995ee36daa43d53335acf5a5733
minseo-hong <mshong1007@gmail.com>
2024-04-10 20:28:22 +0900

    [Design] 홈페이지 장점 섹션 제작

---
+++ src/components/common/home/current/advantage/AdvantageItem.tsx
@@ -0,0 +1,22 @@
+interface AdvantageItemProps {
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+}
+
+const AdvantageItem = ({
+  description,
+  imageSrc,
+  imageAlt,
+}: AdvantageItemProps) => {
+  return (
+    <li className="w-[23rem] flex-auto flex-shrink-0 flex-grow-0">
+      <div className="rounded-xs w-full overflow-hidden">
+        <img src={imageSrc} alt={imageAlt} className="w-full" />
+      </div>
+      <p className="text-xs-0.875-medium mt-3">{description}</p>
+    </li>
+  );
+};
+
+export default AdvantageItem;
