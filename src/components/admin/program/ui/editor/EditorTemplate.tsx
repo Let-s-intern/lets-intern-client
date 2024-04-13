1fe71ac0dea963f708b34e0fa836bb7c748e447c
minseo-hong <mshong1007@gmail.com>
2024-04-13 12:43:06 +0900

    [Refactor] 에디터 템플릿 컴포넌트 제작

---
+++ src/components/admin/program/ui/editor/EditorTemplate.tsx
@@ -0,0 +1,47 @@
+import ActionButton from '../../../ui/button/ActionButton';
+
+interface EditorTemplateProps {
+  title: string;
+  onSubmit?: (e: React.FormEvent) => void;
+  submitButton: {
+    text: string;
+  };
+  cancelButton: {
+    text: string;
+    to?: string;
+    onClick?: () => void;
+  };
+  children: React.ReactNode;
+}
+
+const EditorTemplate = ({
+  title,
+  onSubmit,
+  submitButton,
+  cancelButton,
+  children,
+}: EditorTemplateProps) => {
+  return (
+    <main className="mx-auto mt-12 w-[36rem]">
+      <header>
+        <h1 className="text-2xl font-semibold">{title}</h1>
+      </header>
+      <form className="mt-4" onSubmit={onSubmit}>
+        <div className="flex flex-col gap-4">{children}</div>
+        <div className="mt-8 flex justify-end gap-2">
+          <ActionButton type="submit">{submitButton.text}</ActionButton>
+          <ActionButton
+            type="button"
+            to={cancelButton.to}
+            bgColor="gray"
+            onClick={cancelButton.onClick}
+          >
+            {cancelButton.text}
+          </ActionButton>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default EditorTemplate;
