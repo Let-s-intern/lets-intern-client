9e127f2fb64317e0e0898ccbae2b556ae2b1f8b1
Yongyeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2024-04-23 22:55:40 +0900

    [Refactor] className에 clsx 적용

--- src/components/common/ui/layout/nav/NavItem.tsx
+++ src/components/common/ui/layout/nav/NavItem.tsx
@@ -1,38 +1,24 @@
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
 interface NavItemProps {
   to?: string;
   active?: boolean;
+  as?: keyof JSX.IntrinsicElements;
   children: React.ReactNode;
 }
 
-const NavItem = ({ to, active, children }: NavItemProps) => {
-  if (active) {
-    return (
-      <Link
-        to={to || '#'}
-        className="text-1.125-bold cursor-pointer text-neutral-0 "
-      >
-        {children}
-      </Link>
-    );
-  }
-
-  if (to) {
-    return (
-      <Link
-        to={to}
-        className="text-1.125-medium cursor-pointer text-neutral-60 "
-      >
-        {children}
-      </Link>
-    );
-  }
+const NavItem = ({ to, active, as, children }: NavItemProps) => {
+  const className = clsx({
+    'text-1.125-bold text-neutral-0': active,
+    'text-1.125-medium text-neutral-60': !active && to,
+  });
+  const Element = as || Link;
 
   return (
-    <div className="text-1.125-medium cursor-pointer text-neutral-60">
+    <Element to={to || '#'} className={`${className} cursor-pointer`}>
       {children}
-    </div>
+    </Element>
   );
 };
 
