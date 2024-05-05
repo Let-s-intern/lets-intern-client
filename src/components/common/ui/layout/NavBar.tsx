be28e3a4d05b48d1e2d137ebf383cc7960335cc5
Yongyeon Kim <seyeon.dev@gmail.com>
2024-05-05 21:19:19 +0900

    Revert "[#494] 챌린지 대시보드 기능 추가 (main)" (#508)
    Revert "[#494] 챌린지 대시보드 기능 추가 (main) (#506)"
    
    This reverts commit 7f6bb00368f130300538def18710f9cfbccd84bb.

--- src/components/common/ui/layout/NavBar.tsx
+++ src/components/common/ui/layout/NavBar.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import axios from '../../../../../utils/axios';
-import NavItem from './NavItem';
-import SideNavItem from './SideNavItem';
+import styled from 'styled-components';
+import axios from '../../../../utils/axios';
+import TabBar from '../tab/TabBar';
+import TabItem from '../tab/TabItem';
+
+interface SideNavItemProps {
+  to: string;
+  onClick?: () => void;
+  children: string;
+}
 
 const NavBar = () => {
   const location = useLocation();
@@ -11,8 +18,9 @@ const NavBar = () => {
   const [user, setUser] = useState<any>(null);
   const [isAdmin, setIsAdmin] = useState(false);
   const [activeLink, setActiveLink] = useState<
-    'HOME' | 'ABOUT' | 'PROGRAM' | 'ADMIN' | 'BLOG' | ''
+    'HOME' | 'ABOUT' | 'PROGRAM' | 'ADMIN' | ''
   >('');
+  const [showTopTabBar, setShowTopTabBar] = useState(true);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -23,15 +31,23 @@ const NavBar = () => {
   };
 
   useEffect(() => {
+    if (
+      location.pathname.startsWith('/login') ||
+      location.pathname.startsWith('/signup') ||
+      location.pathname.startsWith('/find-password') ||
+      location.pathname.startsWith('/mypage')
+    ) {
+      setShowTopTabBar(false);
+      return;
+    } else {
+      setShowTopTabBar(true);
+    }
     if (location.pathname.startsWith('/about')) {
       setActiveLink('ABOUT');
     } else if (location.pathname.startsWith('/program')) {
       setActiveLink('PROGRAM');
     } else if (location.pathname.startsWith('/admin')) {
       setActiveLink('ADMIN');
-      // [논의] 블로그 주소 확인 필요
-    } else if (location.pathname.startsWith('/blog')) {
-      setActiveLink('BLOG');
     } else if (location.pathname.startsWith('/')) {
       setActiveLink('HOME');
     }
@@ -83,62 +99,77 @@ const NavBar = () => {
   return (
     <>
       {/* 상단 네비게이션 바 */}
-      <div className="fixed top-0 z-30 w-screen bg-static-100 px-5 sm:px-20 lg:px-28">
+      <div className="bg-static-100 fixed top-0 z-30 w-screen pe-[1.5rem] ps-[1.125rem] md:pe-[2.5rem]">
         <div className="mx-auto flex h-16 items-center justify-between">
-          <div className="flex items-center gap-6 sm:gap-9">
-            <Link to="/" className="w-12 sm:w-24">
-              <img
-                src="/logo/letscareer-logo-simple.png"
-                alt="Logo"
-                className="w-full sm:hidden"
-              />
-              <img
-                src="/logo/letscareer-logo.png"
-                alt="Logo"
-                className="hidden w-full sm:block"
-              />
-            </Link>
-            {/* 메뉴 아이템 */}
-            <NavItem to="/" active={activeLink === 'HOME'}>
-              홈
-            </NavItem>
-            <NavItem to="/program" active={activeLink === 'PROGRAM'}>
-              프로그램
-            </NavItem>
-            <NavItem to="/blog" active={activeLink === 'BLOG'}>
-              블로그
-            </NavItem>
-          </div>
-          <div className="flex items-center gap-4">
+          <Link to="/" className="h-10 w-10">
+            <img src="/logo/logo.png" alt="Logo" className="w-full" />
+          </Link>
+          <div className="hidden sm:block">
             {isLoggedIn ? (
-              <div className="hidden gap-2 sm:flex">
-                <span className="text-1.125-medium block">{user?.name} 님</span>
-                <img
-                  src="/icons/user-circle.svg"
-                  alt="User icon"
-                  className="w-1.75"
-                />
-              </div>
+              <MyInfoSpan className="gap-2">
+                <div className="flex gap-2">
+                  <span>
+                    <b>{user?.name}</b>님
+                  </span>
+                  <button onClick={handleLogout} className="text-[0.75rem]">
+                    로그아웃
+                  </button>
+                </div>
+                <Link
+                  to="/mypage/application"
+                  className="text-static-100 rounded-xxs bg-primary px-2 py-1 text-[0.75rem]"
+                >
+                  마이페이지
+                </Link>
+              </MyInfoSpan>
             ) : (
-              <div className="hidden items-center gap-2 sm:flex">
+              <div className="flex items-center gap-2">
                 <Link
                   to="/login"
-                  className="text-0.75 rounded-xxs bg-primary px-3 py-1 text-static-100"
+                  className="rounded-xxs bg-primary px-3 py-1 text-[0.75rem] text-white"
                 >
                   로그인
                 </Link>
-                <Link to="/signup" className="text-0.75 text-primary">
+                <Link to="/signup" className="text-[0.75rem] text-primary">
                   회원가입
                 </Link>
               </div>
             )}
-            <button type="button" onClick={toggleMenu}>
-              <img src="/icons/nav-icon.svg" alt="네비게이션 아이콘" />
-            </button>
           </div>
+          <button
+            type="button"
+            className="block rounded-md text-gray-500 hover:text-gray-600 sm:hidden"
+            onClick={toggleMenu}
+          >
+            <i>
+              <img src="/icons/nav-icon.svg" alt="네비게이션 아이콘" />
+            </i>
+          </button>
         </div>
       </div>
-
+      {/* 상단 메뉴 탭 바 */}
+      {showTopTabBar ? (
+        <div className="hidden h-9 sm:block">
+          <TabBar>
+            <TabItem to="/" active={activeLink === 'HOME'}>
+              홈
+            </TabItem>
+            <TabItem to="/about" active={activeLink === 'ABOUT'}>
+              브랜드 스토리
+            </TabItem>
+            <TabItem to="/program" active={activeLink === 'PROGRAM'}>
+              프로그램
+            </TabItem>
+            {isAdmin && (
+              <TabItem to="/admin" active={activeLink === 'ADMIN'}>
+                관리자 페이지
+              </TabItem>
+            )}
+          </TabBar>
+        </div>
+      ) : (
+        <div className="hidden h-9 sm:block" />
+      )}
       {/* 투명한 검정색 배경 */}
       <div
         className={`fixed inset-0 z-40 bg-black transition-opacity duration-300 ${
@@ -161,23 +192,21 @@ const NavBar = () => {
         </div>
         <div className="mt-4 flex justify-between">
           {isLoggedIn ? (
-            <span className="flex w-full items-center justify-between gap-4 px-4 text-neutral-0 sm:p-0">
+            <MyInfoSpan>
               <span>
-                환영합니다, <span className="text-primary">{user?.name}</span>님
+                환영합니다, <b>{user?.name}</b>님
               </span>
-              <button className="text-primary" onClick={handleLogout}>
-                로그아웃
-              </button>
-            </span>
+              <button onClick={handleLogout}>로그아웃</button>
+            </MyInfoSpan>
           ) : (
-            <div className="text-0.875 flex gap-6">
-              <Link className="text-primary" to="/login" onClick={closeMenu}>
+            <LoginLinkGroup>
+              <Link to="/login" onClick={closeMenu}>
                 로그인
               </Link>
               <Link to="/signup" onClick={closeMenu}>
                 회원가입
               </Link>
-            </div>
+            </LoginLinkGroup>
           )}
         </div>
         <div className="mt-5 flex flex-col gap-2">
@@ -207,3 +236,57 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+const SideNavItem = ({ to, onClick, children }: SideNavItemProps) => {
+  return (
+    <Link
+      to={to}
+      className="flex w-full cursor-pointer justify-between rounded-md bg-gray-100 px-7 py-5 text-neutral-grey"
+      onClick={onClick}
+    >
+      <span>{children}</span>
+      <i>
+        <img src="/icons/arrow-right.svg" alt="오른쪽 화살표" />
+      </i>
+    </Link>
+  );
+};
+
+const MyInfoSpan = styled.span`
+  color: #505050;
+  font-size: 0.875rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+  padding: 0 1rem;
+
+  @media screen and (min-width: 640px) {
+    padding: 0;
+  }
+
+  span {
+    b {
+      color: #6963f6;
+      font-weight: 400;
+    }
+  }
+
+  button {
+    color: #6963f6;
+  }
+`;
+
+const LoginLinkGroup = styled.div`
+  margin-left: 1rem;
+  font-size: 0.875rem;
+  display: flex;
+  gap: 1.5rem;
+
+  a {
+    &:nth-child(1) {
+      color: #6963f6;
+    }
+  }
+`;
