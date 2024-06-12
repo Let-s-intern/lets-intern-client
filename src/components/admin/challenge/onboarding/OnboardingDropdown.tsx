19680d3fc3b541a6675ac7c5f21c173a50e8d0ae
Kim Tae Hoon <eszqsc112@gmail.com>
2024-06-12 09:40:07 +0900

    [Feat] 어드민 챌린지 운영에서 프로그램 목록 보여주도록 수정

--- src/components/admin/challenge/onboarding/OnboardingDropdown.tsx
+++ src/components/admin/challenge/onboarding/OnboardingDropdown.tsx
@@ -4,11 +4,13 @@ import { useEffect, useRef, useState } from 'react';
 import { IoMdArrowDropdown } from 'react-icons/io';
 
 import axios from '../../../../utils/axios';
+import { IProgram } from '../../../../interfaces/Program.interface';
+import dayjs from 'dayjs';
 
 interface Props {
   className?: string;
-  selectedChallenge?: any;
-  setSelectedChallenge: (selectedChallenge: any) => void;
+  selectedChallenge?: IProgram | null;
+  setSelectedChallenge: (selectedChallenge: IProgram | null) => void;
 }
 
 const OnboardingDropdown = ({
@@ -17,7 +19,7 @@ const OnboardingDropdown = ({
   setSelectedChallenge,
 }: Props) => {
   const [isMenuShown, setIsMenuShown] = useState(false);
-  const [challengeList, setChallengeList] = useState<any>();
+  const [challengeList, setChallengeList] = useState<IProgram[]>([]);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
 
@@ -27,11 +29,13 @@ const OnboardingDropdown = ({
       const res = await axios.get('/program/admin', {
         params: { type: 'CHALLENGE' },
       });
-      const data = res.data;
-      setChallengeList(
-        data.programList
-          .filter((challenge: any) => challenge.th !== 0)
-          .sort((a: any, b: any) => b.th - a.th),
+      const data = res.data.data as {programList: IProgram[]};
+      console.log("data", data);
+      const list = [...data.programList];
+      list.sort((a, b) => {
+        return dayjs(a.programInfo.beginning).diff(dayjs(b.programInfo.beginning));
+      });
+      setChallengeList(list
       );
       return data;
     },
@@ -39,10 +43,8 @@ const OnboardingDropdown = ({
 
   const isLoading = getProgramList.isLoading || !challengeList;
 
-  const handleMenuItemClick = async (challengeId: number) => {
-    setSelectedChallenge(
-      challengeList.find((challenge: any) => challenge.id === challengeId),
-    );
+  const handleMenuItemClick = async (program: IProgram) => {
+    setSelectedChallenge(program);
     setIsMenuShown(false);
   };
 
@@ -63,6 +65,10 @@ const OnboardingDropdown = ({
     }
   }, [isMenuShown]);
 
+  useEffect(() => {
+    console.log("challengeList", challengeList);
+  }, [challengeList]);
+
   return (
     <div className={clsx('relative', className)} ref={dropdownRef}>
       <div
@@ -74,21 +80,22 @@ const OnboardingDropdown = ({
             'opacity-0': isLoading,
           })}
         >
-          {selectedChallenge ? `${selectedChallenge.th}기` : '선택'}
+          {selectedChallenge ? `${selectedChallenge.programInfo.title}` : '선택'}
         </span>
         <i className="text-xl">
           <IoMdArrowDropdown />
         </i>
       </div>
       {!isLoading && isMenuShown && (
-        <ul className="absolute bottom-0 w-24 translate-y-[calc(100%+0.25rem)] rounded bg-white shadow-md">
-          {challengeList.map((challenge: any) => (
+        <ul className="absolute bottom-0 w-64 translate-y-[calc(100%+0.25rem)] rounded bg-white shadow-md">
+          {challengeList.map((challenge, index) => (
             <li
-              key={challenge.id}
+              // key={challenge.programInfo.id}
+              key={index}
               className="cursor-pointer py-3 pl-4 pr-2 font-medium hover:bg-gray-50"
-              onClick={() => handleMenuItemClick(challenge.id)}
+              onClick={() => handleMenuItemClick(challenge)}
             >
-              {challenge.th}기
+              {challenge.programInfo.title}
             </li>
           ))}
         </ul>
--- src/pages/admin/challenge/ChallengeOnboarding.tsx
+++ src/pages/admin/challenge/ChallengeOnboarding.tsx
@@ -3,36 +3,37 @@ import { useNavigate } from 'react-router-dom';
 import { useQueryClient } from '@tanstack/react-query';
 
 import OnboardingDropdown from '../../../components/admin/challenge/onboarding/OnboardingDropdown';
+import { IProgram } from '../../../interfaces/Program.interface';
 
 const ChallengeOnboarding = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
-  const [selectedChallenge, setSelectedChallenge] = useState<any>();
+  const [selectedChallenge, setSelectedChallenge] = useState<IProgram | null>(null);
 
-  const handleConfirmButtonClick = () => {
-    localStorage.setItem('admin-challenge-id', `${selectedChallenge.id}`);
-    queryClient.invalidateQueries({ queryKey: ['program'] });
-    navigate(`/admin/challenge/${selectedChallenge.id}`);
-  };
+  // const handleConfirmButtonClick = () => {
+  //   localStorage.setItem('admin-challenge-id', `${selectedChallenge.id}`);
+  //   queryClient.invalidateQueries({ queryKey: ['program'] });
+  //   navigate(`/admin/challenge/${selectedChallenge.id}`);
+  // };
 
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <h1 className="text-xl font-semibold">챌린지 기수를 선택해주세요.</h1>
       {selectedChallenge && (
-        <p className="mt-2 text-neutral-grey">{selectedChallenge.title}</p>
+        <p className="mt-2 text-neutral-grey">{selectedChallenge.programInfo.title}</p>
       )}
       <OnboardingDropdown
         className="mt-4"
         selectedChallenge={selectedChallenge}
         setSelectedChallenge={setSelectedChallenge}
       />
-      <button
+      {/* <button
         className="mt-4 rounded bg-primary px-4 py-2 font-medium text-white"
         onClick={handleConfirmButtonClick}
       >
         확인
-      </button>
+      </button> */}
     </main>
   );
 };
