98c198b8b9bb98d9ae0430f6aa7216b1ac34e1b7
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-16 19:52:13 +0900

    [Feat] 홈 프로그램 미치보기 api 연동 및 날짜 상태 관리 추가

--- src/components/common/home/new/overview/ProgramOverviewListItem.tsx
+++ src/components/common/home/new/overview/ProgramOverviewListItem.tsx
@@ -1,6 +1,12 @@
 import { useMediaQuery } from '@mui/material';
 import { IProgram } from '../../../../../interfaces/interface';
+
 import ProgramListItem from './ProgramListItem';
+
+import { useQuery } from '@tanstack/react-query';
+
+import axios from '../../../../../utils/axios';
+import { useState, useCallback } from 'react';
 
 export interface ProgramOverviewListItemProps {
   title: string;
@@ -8,89 +14,102 @@ export interface ProgramOverviewListItemProps {
   imageColor?: 'blue' | 'green' | 'purple' | 'yellow';
 }
 
-const programOverviewList: IProgram[] = [
-  {
-    programInfo: {
-      id: 0,
-      programType: 'LIVE',
-      programStatusType: 'PROCEEDING',
-      title: 'AI 개발자의 커리어 세션',
-      thumbnail: '/images/home/home-program-default.png',
-      shortDesc: '29CM 백엔드 개발자',
-      createDate: '2024-06-13T13:50:16.388Z',
-      startDate: '2024-06-13T13:50:16.388Z',
-      endDate: '2024-06-13T13:50:16.388Z',
-      beginning: '2024-06-13T13:50:16.388Z',
-      deadline: '2024-06-13T13:50:16.388Z',
-    },
-    classificationList: [{ programClassification: 'CAREER_SEARCH' }],
-  },
-  {
-    programInfo: {
-      id: 1,
-      programType: 'CHALLENGE',
-      programStatusType: 'PROCEEDING',
-      title: '면접 완성 1주 챌린지',
-      thumbnail: '/images/home/home-program-default.png',
-      shortDesc: '1주만에 면접을 끝내보자',
-      createDate: '2024-06-13T13:50:16.388Z',
-      startDate: '2024-06-13T13:50:16.388Z',
-      endDate: '2024-06-13T13:50:16.388Z',
-      beginning: '2024-06-13T13:50:16.388Z',
-      deadline: '2024-06-13T13:50:16.388Z',
-    },
-    classificationList: [{ programClassification: 'DOCUMENT_PREPARATION' }],
-  },
-  {
-    programInfo: {
-      id: 2,
-      programType: 'VOD',
-      programStatusType: 'PROCEEDING',
-      title: 'VOD 클래스',
-      thumbnail: '/images/home/home-program-default.png',
-      shortDesc: 'VOD 클래스로 Skill up 하기',
-      createDate: '2024-06-13T13:50:16.388Z',
-      startDate: '2024-06-13T13:50:16.388Z',
-      endDate: '2024-06-13T13:50:16.388Z',
-      beginning: '2024-06-13T13:50:16.388Z',
-      deadline: '2024-06-13T13:50:16.388Z',
-    },
-    classificationList: [{ programClassification: 'MEETING_PREPARATION' }],
-  },
-];
+interface DateInfo {
+  year: number;
+  month: number;
+}
+
+const getMonthStartEndDates = (year: number, month: number) => {
+  const start = new Date(year, month - 1, 1);
+  const end = new Date(year, month, 0);
+  const startDate = start.toISOString().split('T')[0] + 'T00:00:00';
+  const endDate = end.toISOString().split('T')[0] + 'T23:59:59';
+  return { startDate, endDate };
+};
+
+const getPreviousMonth = (year: number, month: number): DateInfo => {
+  if (month === 1) {
+    return { year: year - 1, month: 12 };
+  } else {
+    return { year, month: month - 1 };
+  }
+};
+
+const getNextMonth = (year: number, month: number): DateInfo => {
+  if (month === 12) {
+    return { year: year + 1, month: 1 };
+  } else {
+    return { year, month: month + 1 };
+  }
+};
 
 const ProgramOverviewListItem = () => {
+  const [current, setCurrent] = useState<DateInfo>({
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1
+  });
+  const prevMonth = useCallback(() => getPreviousMonth(current.year, current.month), [current]);
+  const nextMonth = useCallback(() => getNextMonth(current.year, current.month), [current]);
   const matches = useMediaQuery('(min-width: 768px)');
+
+  const {isLoading, data} = useQuery<IProgram[]>({
+    queryKey: ['HomeProgram', current],
+    queryFn: async () => {
+      const { startDate, endDate } = getMonthStartEndDates(current.year, current.month);
+      const res = await axios.get(`/program`, {
+        params: {
+          size: 4,
+          startDate: startDate,
+          endDate: endDate
+        }
+      });
+      return res.data.data.programList;
+    },
+  });
+
+  const goToPreviousMonth = () => {
+    setCurrent(prevMonth());
+  };
+
+  const goToNextMonth = () => {
+    setCurrent(nextMonth());
+  };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="mt-6 overflow-hidden rounded-xs">
       <div className="flex items-center">
         {matches && (
           <div className="w-1/4 bg-neutral-80 py-3.5 text-center text-neutral-0/60">
-            2024년 5월
+            {`${prevMonth().year}년 ${prevMonth().month}월`}
           </div>
         )}
         <div className="flex w-full items-center justify-center gap-1 bg-primary-10 py-3.5 md:w-2/4">
           <img
             className="w-5 cursor-pointer"
+            onClick={() => goToPreviousMonth()}
             src="/icons/Chevron_Left_MD.svg"
             alt="이전 달"
           />
-          <span className="text-1">2024년 6월</span>
+          <span className="text-1">{`${current.year}년 ${current.month}월`}</span>
           <img
             className="w-5 cursor-pointer"
+            onClick={() => goToNextMonth()}
             src="/icons/Chevron_Right_MD.svg"
             alt="다음 달"
           />
         </div>
         {matches && (
           <div className="w-1/4 bg-[#E8F9F2] py-3.5 text-center text-neutral-0/60">
-            2024년 7월
+            {`${nextMonth().year}년 ${nextMonth().month}월`}
           </div>
         )}
       </div>
       <ul className=" grid grid-cols-1 gap-4 md:grid-cols-2">
-        {programOverviewList.map((program) => (
+        {data && data.map((program) => (
           <ProgramListItem
             key={program.programInfo.programType + program.programInfo.id}
             program={program}
