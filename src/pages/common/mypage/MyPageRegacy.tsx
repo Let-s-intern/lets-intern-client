e94e91474b6836b2dcaefcc59d1d96d441aa7ea9
Ginieee <kaj1226@naver.com>
2024-06-18 14:00:13 +0900

    [Feat] 마이페이지 새로운 디자인 적용 및 MyPageRegacy 적용

---
+++ src/pages/common/mypage/MyPageRegacy.tsx
@@ -0,0 +1,58 @@
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+import TabBar from '../../../components/common/ui/tab/TabBar';
+import TabItem from '../../../components/common/ui/tab/TabItem';
+
+const MyPageRegacy = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access-token');
+    const refreshToken = localStorage.getItem('refresh-token');
+
+    if (!accessToken || !refreshToken) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+  return (
+    <div className="mypage">
+      <nav>
+        <TabBar itemCount={3}>
+          <TabItem
+            to="/mypage/application"
+            {...(location.pathname === '/mypage/application' && {
+              active: true,
+            })}
+          >
+            신청현황
+          </TabItem>
+          <TabItem
+            to="/mypage/review"
+            {...(location.pathname.startsWith('/mypage/review') && {
+              active: true,
+            })}
+          >
+            후기작성
+          </TabItem>
+          <TabItem
+            to="/mypage/privacy"
+            {...(location.pathname === '/mypage/privacy' && {
+              active: true,
+            })}
+          >
+            개인정보
+          </TabItem>
+        </TabBar>
+      </nav>
+      <div className="block h-8 w-full sm:hidden"></div>
+      <div className="mx-auto mt-5 max-w-5xl">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default MyPageRegacy;
