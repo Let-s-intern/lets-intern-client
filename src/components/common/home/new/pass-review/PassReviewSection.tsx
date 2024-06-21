1da95fe297cd27738b5c6627b999e0d1bb90f8e1
Yongyeon Kim <seyeon.dev@gmail.com>
2024-06-22 01:27:38 +0900

    [Feat] 회사 로고 추가

--- src/components/common/home/new/pass-review/PassReviewSection.tsx
+++ src/components/common/home/new/pass-review/PassReviewSection.tsx
@@ -1,16 +1,38 @@
 import Heading from '../ui/Heading';
 
+const images = [
+  // 'cj-fresh.png',
+  'lg.avif',
+  'YBM.png',
+  'naver-webtoon.png',
+  'naver-cloud.png',
+  'nuvilab.png',
+  'samsung-bio.png',
+  'pwc.png',
+  'under-dogs.png',
+  'wadiz.png',
+  'catch-table.png',
+  'sparta.png',
+  'hive.svg',
+  'hyundai.svg',
+  'humax.svg',
+];
+
 const PassReviewSection = () => {
   return (
     <section>
       <Heading>렛츠커리어인들은 어디서 커리어를 시작했을까요?</Heading>
-      <div className="mt-6 flex flex-nowrap gap-x-1.5 overflow-x-auto md:gap-4 lg:gap-6 ">
-        {Array.from({ length: 4 }).map((_, index) => (
+      <div className="mt-6 flex flex-nowrap gap-x-1.5 overflow-visible overflow-x-auto md:gap-4 lg:gap-6">
+        {images.map((image) => (
           <div
-            key={index}
-            className="h-[6.25rem] w-[14.5rem] flex-shrink-0 overflow-hidden rounded-xs"
+            key={image}
+            className="h-12 shrink-0 px-3.5 py-2.5 md:h-16 lg:h-24"
           >
-            <div className="h-full w-full bg-neutral-80" />
+            <img
+              className="h-full w-full object-contain"
+              src={`/images/home/company/${image}`}
+              alt={image}
+            />
           </div>
         ))}
       </div>
