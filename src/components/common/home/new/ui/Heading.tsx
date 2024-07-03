7133e538af3be68c4d0cbfbcf1c4f05a84725936
Ginieee <kaj1226@naver.com>
2024-07-03 14:16:52 +0900

    [Fix] 헤더값 폰트 크기 수정

--- src/components/common/home/new/ui/Heading.tsx
+++ src/components/common/home/new/ui/Heading.tsx
@@ -3,7 +3,7 @@ interface HeadingProps {
 }
 
 const Heading = ({ children }: HeadingProps) => (
-  <h1 className="text-1.125-bold md:text-1.375-bold lg:text-1.5-bold xl:text-1.5-semibold flex flex-col gap-1 text-neutral-0 sm:px-5 md:flex-row ">
+  <h1 className="font-bold text-lg lg:font-bold lg:text-[1.375rem] flex flex-col gap-1 text-neutral-0 md:flex-row ">
     {children}
   </h1>
 );
