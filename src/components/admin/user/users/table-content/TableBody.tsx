9f7ee33f7795b3dc4c4608ca5746dd519436978c
Ginieee <kaj1226@naver.com>
2024-07-13 14:32:15 +0900

    [Feat] 어드민 > 회원DB에 회원삭제 모달 및 삭제 기능 추가

--- src/components/admin/user/users/table-content/TableBody.tsx
+++ src/components/admin/user/users/table-content/TableBody.tsx
@@ -1,26 +1,29 @@
 import dayjs from 'dayjs';
 
-import { IUser } from '../../../../../interfaces/User.interface';
+import { UserAdmin } from '../../../../../schema';
 import { convertTypeToBank } from '../../../../../utils/convertTypeToBank';
 import ActionButton from '../../../ui/button/ActionButton';
 import TD from '../../../ui/table/regacy/TD';
 
 interface TableBodyProps {
-  userList: IUser[];
+  userList: UserAdmin;
+  handleDeleteUser: (phoneNum: string) => void;
 }
 
-const TableBody = ({ userList }: TableBodyProps) => {
+const TableBody = ({ userList, handleDeleteUser }: TableBodyProps) => {
   return (
     <tbody>
       {userList.map((user) => (
         <tr key={user.id}>
           <TD>{user.name}</TD>
           <TD>{user.email}</TD>
-          <TD>{`${!!user.contactEmail ? user.contactEmail : '-'}`}</TD>
+          <TD>{`${user.contactEmail ? user.contactEmail : '-'}`}</TD>
           <TD>{user.phoneNum}</TD>
           {/* <TD>참여 프로그램 없음</TD> */}
           <TD>{dayjs(user.createdDate).format('YYYY-MM-DD (dd)')}</TD>
-          <TD>{convertTypeToBank(user.accountType) || '없음'}</TD>
+          <TD>
+            {user.accountType ? convertTypeToBank(user.accountType) : '없음'}
+          </TD>
           <TD>{user.accountNum || '없음'}</TD>
           <TD>{user.marketingAgree ? 'O' : 'X'}</TD>
           <TD>
@@ -31,7 +34,14 @@ const TableBody = ({ userList }: TableBodyProps) => {
               <ActionButton to={`/admin/users/${user.id}/edit`} bgColor="green">
                 수정
               </ActionButton>
-              <ActionButton bgColor="red">삭제</ActionButton>
+              <ActionButton
+                bgColor="red"
+                onClick={() => {
+                  handleDeleteUser(user.phoneNum);
+                }}
+              >
+                삭제
+              </ActionButton>
             </div>
           </TD>
         </tr>
