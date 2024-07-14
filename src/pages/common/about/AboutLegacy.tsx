ff6797a1fa51dc805ef8b201a35c82fa9b10aeae
Yongyeon Kim <seyeon.dev@gmail.com>
2024-07-14 12:28:25 +0900

    [Feat] 브랜드 페이지 헤더 추가

---
+++ src/pages/common/about/AboutLegacy.tsx
@@ -0,0 +1,24 @@
+import AboutHeader from '../../../components/common/about/header/AboutHeader';
+import EndSection from '../../../components/common/about/section/EndSection';
+import ProblemSection from '../../../components/common/about/section/ProblemSection';
+import ProgramMenuSection from '../../../components/common/about/section/ProgramMenuSection';
+import ResultSection from '../../../components/common/about/section/ResultSection';
+import SolutionSection from '../../../components/common/about/section/SolutionSection';
+
+const AboutLegacy = () => {
+  return (
+    <>
+      <AboutHeader />
+      <main>
+        <ProblemSection />
+        <SolutionSection />
+        <ProgramMenuSection />
+        <ResultSection />
+        <EndSection />
+        {/* <BottomSection /> */}
+      </main>
+    </>
+  );
+};
+
+export default AboutLegacy;
