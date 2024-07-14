1b5f5faefcde79159d0bc96d6f8a2483b105687a
Yongyeon Kim <seyeon.dev@gmail.com>
2024-07-14 16:25:08 +0900

    [Feat] 솔루션 세션 추가

--- src/components/common/about/section/SolutionSection.tsx
+++ src/components/common/about/section/SolutionSection.tsx
@@ -1,41 +1,52 @@
-import classes from './SolutionSection.module.scss';
+const CONTENT = [
+  {
+    imgName: 'lets-solution.svg',
+    imgWidth: 'w-[3.78rem]',
+    title: '렛츠(Let’s)!',
+    description:
+      "'함께 해보자'는 말처럼, 렛츠커리어는 독자적인 커리어 교육 프로그램으로 나만의 이야기를 만들어 가는 방법을 함께 합니다.",
+  },
+  {
+    imgName: 'career-solution.svg',
+    imgWidth: 'w-[4rem]',
+    title: '커리어 프로그램',
+    description:
+      '정보찾기부터 활용하기, 자신을 되돌아보기까지 여러 프로그램을 제공합니다. 막막함을 느끼지 않게 러닝 메이트가 되어 드립니다.',
+  },
+  {
+    imgName: 'community-solution.svg',
+    imgWidth: 'w-[4.3rem]',
+    title: '선순환 커뮤니티',
+    description:
+      '렛츠커리어 프로그램을 통해 성장한 참여자들은, 다른 이들의 시작을 돕는 커뮤니티의 일원이 될 수 있습니다.',
+  },
+];
 
 const SolutionSection = () => {
   return (
-    <section className={classes.section}>
-      <div className={classes.content}>
-        <h2>
-          이에 렛츠인턴은 취업 준비생들의
-          <br />
-          길라잡이가 되고자 합니다
-        </h2>
-        <p>
-          ‘렛츠(Let’s)=함께 해보자'라는 말처럼 렛츠인턴의 성장 프로그램으로{' '}
-          <br />
-          나만의 이야기를 만들어 가는 방법을 함께 합니다. <br />
-          여러 성장 프로그램을 통해 정보를 찾는 법부터 활용하는 법, 나라는
-          사람을 되돌아보기까지, <br />
-          참여자 모두 막막함을 느끼지 않도록 렛츠인턴 러닝메이트가 되어 함께하며
-          참여자들은 성장해 <br />
-          다른 이들의 시작을 도와주는 선순환 커뮤니티의 일원이 되어줍니다.
+    <section className="bg-neutral-100 px-5 py-[3.75rem]">
+      <h1 className="text-1-bold mb-2 text-neutral-40">커리어 성장</h1>
+      <div className="text-1.25-bold mb-[3.75rem]">
+        <span>
+          이제 <span className="text-primary">렛츠커리어</span>가
+        </span>
+        <p className="flex flex-col sm:flex-row sm:gap-1">
+          <span>취업준비생과 주니어의</span>
+          <span>길라잡이가 되겠습니다</span>
         </p>
-        <div className={classes.coreValueGroup}>
-          <div className={classes.coreValue}>
-            <span>힘이 되는</span>
-            <strong>Supportive</strong>
-            <div className={classes.circle} />
+      </div>
+      <div className="flex flex-col gap-20">
+        {CONTENT.map(({ title, description, imgName, imgWidth }) => (
+          <div key={title} className="flex items-center gap-6">
+            <img className={`h-full ${imgWidth}`} src={`/icons/${imgName}`} />
+            <div className="flex flex-col gap-2">
+              <h2 className="text-1-bold text-primary">{title}</h2>
+              <p className="text-0.875 text-justify text-neutral-40">
+                {description}
+              </p>
+            </div>
           </div>
-          <div className={classes.coreValue}>
-            <span>신뢰가 되는</span>
-            <strong>Reliable</strong>
-            <div className={classes.circle} />
-          </div>
-          <div className={classes.coreValue}>
-            <span>동기부여가 되는</span>
-            <strong>Motivative</strong>
-            <div className={classes.circle} />
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
