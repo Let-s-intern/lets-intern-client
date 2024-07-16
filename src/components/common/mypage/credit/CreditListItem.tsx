670500e48db960f3d6f723de1ca70f97506a0ea8
Ginieee <kaj1226@naver.com>
2024-07-16 10:06:01 +0900

    [Feat] 결제내역 아이템 레이아웃 구현

--- src/components/common/mypage/credit/CreditListItem.tsx
+++ src/components/common/mypage/credit/CreditListItem.tsx
@@ -1,21 +1,41 @@
+import { Link } from 'react-router-dom';
 import { PaymentType } from '../../../../api/paymentSchema';
+import CardStatus from './CardStatus';
 
 const CreditListItem = ({ payment }: { payment: PaymentType }) => {
   return (
-    <div>
-      <div>{payment.tossInfo.status}</div>
-      <div>
-        <img src={payment.programInfo.thumbnail || ''} />
-        <div>
-          <div>{payment.programInfo.title}</div>
-          <div>
-            <div>{payment.programInfo.price}</div>
-            <div>{payment.tossInfo.totalAmount}</div>
+    <Link
+      className="flex w-full flex-col items-start justify-center gap-y-2"
+      to={`/mypage/credit/${payment.programInfo.paymentId}`}
+    >
+      <CardStatus status={payment.tossInfo.status || ''} />
+      <div className="flex w-full items-center gap-x-[14px]">
+        <img
+          src={payment.programInfo.thumbnail || ''}
+          className="h-[97px] w-[137px] rounded-sm object-cover"
+        />
+        <div className="flex grow flex-col items-start justify-between gap-2 self-stretch">
+          <div className="text-xs font-semibold text-neutral-0 md:text-sm">
+            {payment.programInfo.title}
           </div>
-          <div>결제상세</div>
+          <div className="flex grow flex-col items-start justify-start">
+            <div className="text-xs font-medium text-neutral-40 line-through">
+              {payment.programInfo.price?.toLocaleString()}원
+            </div>
+            <div className="text-sm font-semibold text-neutral-0 md:text-base">
+              {payment.tossInfo.totalAmount}
+            </div>
+          </div>
+          <button className="flex items-center justify-start text-xs font-semibold text-primary md:text-sm">
+            결제상세
+            <img
+              src="/icons/Chevron_Right_MD_primary.svg"
+              className="h-4 w-4"
+            />
+          </button>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
