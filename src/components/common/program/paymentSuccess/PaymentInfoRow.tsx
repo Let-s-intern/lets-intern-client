8a4e9f97feb5cd01450939a1b5cb915812fedd02
Ginieee <kaj1226@naver.com>
2024-07-16 13:06:00 +0900

    [Fix] PaymentInfoRow에 subChild 넣을 수 있도록 수정

--- src/components/common/program/paymentSuccess/PaymentInfoRow.tsx
+++ src/components/common/program/paymentSuccess/PaymentInfoRow.tsx
@@ -1,9 +1,13 @@
+import { ReactNode } from 'react';
+
 const PaymentInfoRow = ({
   title,
   content,
+  subInfo,
 }: {
   title: string;
   content: string;
+  subInfo?: ReactNode;
 }) => {
   return (
     <div className="flex w-full items-center justify-start gap-x-2 px-3 py-2">
@@ -11,6 +15,9 @@ const PaymentInfoRow = ({
       <div className="flex grow items-center justify-end text-neutral-0">
         {content}
       </div>
+      {subInfo && (
+        <div className="flex w-full items-center justify-start">{subInfo}</div>
+      )}
     </div>
   );
 };
