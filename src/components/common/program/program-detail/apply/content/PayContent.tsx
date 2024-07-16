8c9881e8861a720358e8e3a8fbb43ce53b0b1ec5
Kim Tae Hoon <eszqsc112@gmail.com>
2024-07-16 23:13:32 +0900

    paymentMethodKey searchParams 잘 설정되도록 수정

--- src/components/common/program/program-detail/apply/content/PayContent.tsx
+++ src/components/common/program/program-detail/apply/content/PayContent.tsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import { useRef } from 'react';
 import { FaArrowLeft } from 'react-icons/fa6';
+import { twMerge } from 'tailwind-merge';
 import { ProgramQuery } from '../../../../../../api/program';
 import { ICouponForm } from '../../../../../../types/interface';
 import Input from '../../../../ui/input/Input';
@@ -25,6 +26,7 @@ interface PayContentProps {
   programQuery: ProgramQuery;
   programId: number;
   userInfo: UserInfo;
+  isMobile?: boolean;
 }
 
 const PayContent = ({
@@ -40,6 +42,7 @@ const PayContent = ({
   programQuery,
   programId,
   userInfo,
+  isMobile,
 }: PayContentProps) => {
   const topRef = useRef<HTMLDivElement>(null);
 
@@ -48,7 +51,7 @@ const PayContent = ({
   };
 
   return (
-    <div className="flex h-full flex-col gap-6 px-5">
+    <div className={twMerge('flex h-full flex-col gap-6', isMobile && 'px-5')}>
       {payInfo.challengePriceType !== 'FREE' &&
         payInfo.livePriceType !== 'FREE' && (
           <>
@@ -76,7 +79,7 @@ const PayContent = ({
             <h3 className="text-xsmall16 font-semibold text-neutral-0">
               참여자 정보
             </h3>
-            <div className="flex flex-col gap-3">
+            <div className="mb-4 flex flex-col gap-3">
               <div className="flex flex-col gap-1">
                 <label className="ml-3 text-xsmall14 font-semibold">이름</label>
                 <Input
@@ -110,7 +113,6 @@ const PayContent = ({
               </div>
             </div>
 
-            <hr className="bg-neutral-85" />
             <div className="font-semibold text-neutral-0">결제 정보</div>
             <CouponSection setCoupon={setCoupon} programType={programType} />
             <hr className="bg-neutral-85" />
@@ -123,7 +125,12 @@ const PayContent = ({
           </>
         )}
 
-      <div className="sticky bottom-0 -mx-5 -mb-3 flex gap-2 rounded-t-lg bg-white/[64%] px-5 py-3 shadow-05">
+      <div
+        className={twMerge(
+          'sticky bottom-0 -mx-5 flex gap-2 bg-white/[64%] px-5',
+          isMobile && '-mb-3 rounded-t-lg py-3 shadow-05',
+        )}
+      >
         <button
           className="flex w-full flex-1 items-center justify-center rounded-md border-2 border-primary bg-neutral-100 px-6 py-3 text-lg font-medium text-primary-dark"
           onClick={handleBackButtonClick}
