c47e96bb8e84a9f07104f393e04526dbdd209c4f
Yongyeon Kim <seyeon.dev@gmail.com>
2024-07-16 15:23:11 +0900

    제목이 밑줄 앞으로 오도록 수정

--- src/components/common/about/section/introduction/IntroTtile.tsx
+++ src/components/common/about/section/introduction/IntroTtile.tsx
@@ -5,10 +5,10 @@ interface IntroTitleProps {
 
 const IntroTitle = ({ title, lineWidth }: IntroTitleProps) => (
   <div className="relative flex flex-col md:items-center">
-    <h2 className="text-1-bold xl:text-1.25-bold">{title}</h2>
+    <h2 className="text-1-bold xl:text-1.25-bold z-10">{title}</h2>
     <div
       className={`absolute top-3 h-3 xl:top-4 ${lineWidth} bg-primary-xlight/60`}
-    ></div>
+    />
   </div>
 );
 
