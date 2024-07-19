7f226e9f68706ef7b158b826049e8a1fc9e940e1
Kim Tae Hoon <eszqsc112@gmail.com>
2024-07-19 22:34:29 +0900

    LC-229 신청 내역 분류 잘 들어가도록 수정 및 user/applications hook 처리

--- src/components/common/mypage/application/section/ApplySection.tsx
+++ src/components/common/mypage/application/section/ApplySection.tsx
@@ -1,13 +1,12 @@
-import { Link, useNavigate } from 'react-router-dom';
-
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
-import { ApplicationType } from '../../../../../pages/common/mypage/Application';
+import { MypageApplication } from '../../../../../api/application';
 import MoreButton from '../../ui/button/MoreButton';
 import ApplicationCard from '../../ui/card/root/ApplicationCard';
 
 interface ApplySectionProps {
-  applicationList: ApplicationType[];
+  applicationList: MypageApplication[];
   refetch: () => void;
 }
 
