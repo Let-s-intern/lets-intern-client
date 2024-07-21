26d14ec5ef853e4f5d1ba85b3d7af1562cc5f29b
Kim Tae Hoon <eszqsc112@gmail.com>
2024-07-22 00:28:42 +0900

    LC-285 LC-284 lexical 설치 및 기본 admin route 뚫어놓기

---
+++ src/pages/admin/BlogCreatePage.tsx
@@ -0,0 +1,11 @@
+import BlogPostEditor from '../../components/admin/BlogPostEditor';
+
+const BlogPostList = () => {
+  return (
+    <div>
+      <BlogPostEditor />
+    </div>
+  );
+};
+
+export default BlogPostList;
---
+++ src/pages/admin/BlogEditPage.tsx
@@ -0,0 +1,11 @@
+import BlogPostEditor from '../../components/admin/BlogPostEditor';
+
+const BlogEditPage = () => {
+  return (
+    <div>
+      <BlogPostEditor />
+    </div>
+  );
+};
+
+export default BlogEditPage;
