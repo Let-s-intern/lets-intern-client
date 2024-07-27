d1ecafc7466ca1bd55e1bd566651cbd83a1ab11f
Ginieee <kaj1226@naver.com>
2024-07-28 00:35:52 +0900

    [Feat] BlogCard 컴포넌트 생성

--- src/components/common/blog/BlogCard.tsx
+++ src/components/common/blog/BlogCard.tsx
@@ -1,23 +1,30 @@
-import { BlogInfoType } from '../../../api/blogSchema';
+import { TransformedBlogInfoType } from '../../../api/blogSchema';
 import { blogCategory } from '../../../utils/convert';
 import BlogHashtag from './BlogHashtag';
 
-const BlogCard = (blogInfo: BlogInfoType) => {
+const BlogCard = (blogInfo: TransformedBlogInfoType) => {
   return (
-    <div>
-      <span>{blogCategory[blogInfo.blogThumbnailInfo.category || '']}</span>
-      <div>
-        <div>
-          <div>
-            <h2>{blogInfo.blogThumbnailInfo.title}</h2>
-            <img
-              src={blogInfo.blogThumbnailInfo.thumbnail || ''}
-              alt="thumbnail"
-            />
+    <div className="flex w-full flex-col gap-y-2 py-3">
+      <span className="w-full text-xsmall16 font-bold text-primary">
+        {blogCategory[blogInfo.blogThumbnailInfo.category || '']}
+      </span>
+      <div className="flex w-full flex-col gap-y-4">
+        <div className="flex w-full gap-x-5">
+          <div className="flex flex-1 flex-col gap-y-2">
+            <h2 className="line-clamp-3 font-bold text-neutral-0">
+              {blogInfo.blogThumbnailInfo.title}
+            </h2>
+            <p className="line-clamp-6 text-xsmall16 text-neutral-20">
+              {blogInfo.blogThumbnailInfo.description}
+            </p>
           </div>
-          <p>{blogInfo.blogThumbnailInfo.description}</p>
+          <img
+            className="h-[60px] w-[100px] overflow-hidden rounded-md md:h-[90px] md:w-[130px]"
+            src={blogInfo.blogThumbnailInfo.thumbnail || ''}
+            alt="thumbnail"
+          />
         </div>
-        <div>
+        <div className="flex flex-wrap items-center gap-1.5">
           {blogInfo.tagDetailInfos.map((tag) => (
             <BlogHashtag
               key={tag.id}
