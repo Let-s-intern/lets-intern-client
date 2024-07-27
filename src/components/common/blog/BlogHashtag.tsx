bb27c1cd063d0067bbc91295adddbfb7671dc032
Ginieee <kaj1226@naver.com>
2024-07-27 13:38:29 +0900

    [Feat] BlogHashtag 구현

---
+++ src/components/common/blog/BlogHashtag.tsx
@@ -0,0 +1,18 @@
+const BlogHashtag = ({
+  text,
+  onClick,
+}: {
+  text: string;
+  onClick: () => void;
+}) => {
+  return (
+    <div
+      className="flex items-center justify-center rounded-full bg-[#F3F5FA] px-2.5 py-1 text-xsmall14 text-primary-dark"
+      onClick={onClick}
+    >
+      #{text}
+    </div>
+  );
+};
+
+export default BlogHashtag;
