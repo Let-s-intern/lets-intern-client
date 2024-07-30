8b600f9ee462405bdf058dcea060455e054e2354
Kim Tae Hoon <eszqsc112@gmail.com>
2024-07-31 08:46:19 +0900

    react-router 경로 다시 그루핑

--- pages/(catch_all)/+Page.tsx
+++ pages/(catch_all)/+Page.tsx
@@ -1,6 +1,6 @@
 import { clientOnly } from 'vike-react/clientOnly';
 
-const App = clientOnly(() => import('../src/App'));
+const App = clientOnly(() => import('../../src/App'));
 
 export function Page() {
   return <App />;
