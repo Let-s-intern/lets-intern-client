0174cdebcc2c02e739baf362e1181f1a852d9821
yyeonkim <seyeon.dev@gmail.com>
2024-08-04 16:54:51 +0900

    프로그램 생성/수정 시 필독사항 숨김

--- src/components/admin/program/ui/editor/ProgramInputContent.tsx
+++ src/components/admin/program/ui/editor/ProgramInputContent.tsx
@@ -4,8 +4,7 @@ import {
   InputLabel,
   MenuItem,
   OutlinedInput,
-  Select,
-  TextField,
+  Select
 } from '@mui/material';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { useMemo, useRef } from 'react';
@@ -598,7 +597,7 @@ const ProgramInputContent = ({
                 }}
               />
             )}
-            {(value.program === 'CHALLENGE' || value.program === 'LIVE') && (
+            {/* {(value.program === 'CHALLENGE' || value.program === 'LIVE') && (
               <TextField
                 label="필독사항"
                 name="criticalNotice"
@@ -614,7 +613,7 @@ const ProgramInputContent = ({
                 multiline
                 rows={4}
               />
-            )}
+            )} */}
             {(value.program === 'CHALLENGE' || value.program === 'LIVE') && (
               <FAQEditor
                 programType={value.program}
