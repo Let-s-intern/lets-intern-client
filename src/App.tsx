6e281c95b963d7e0a6e683116eaaf0fcddb6782a
Yongyeon Kim <seyeon.dev@gmail.com>
2024-08-06 23:13:59 +0900

    Pretendard Variable 로컬 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import { Helmet } from 'react-helmet';
 import { ZodError } from 'zod';
 
 import Router from './Router';
-
+import './fonts/font.css';
 import './index.css';
 import './styles/apply.scss';
 import './styles/blog.css';
