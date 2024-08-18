cc6c6ee316a0b713c0d7cf4e51d2a8953fa77141
Ginieee <kaj1226@naver.com>
2024-08-18 13:58:51 +0900

    feat: 스코어 하단에 수료증 발급 호버 이벤트 구현

--- src/components/common/challenge/dashboard/section/ScoreSection.tsx
+++ src/components/common/challenge/dashboard/section/ScoreSection.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ScoreTooltipQuestion from '../../ui/tooltip-question/ScoreTooltipQuestion';
 
 interface Props {
@@ -6,14 +7,15 @@ interface Props {
 }
 
 const ScoreSection = ({ totalScore, currentScore }: Props) => {
+  const [isHoverButton, setIsHoverButton] = useState(false);
   return (
-    <section className="flex w-full flex-col rounded-xl border border-[#E4E4E7] p-6">
+    <section className="relative flex w-full flex-col gap-y-4 rounded-xl border border-[#E4E4E7] p-6">
       <div className="flex flex-1 flex-col gap-2">
         <div className="flex items-center gap-2">
           <h2 className="font-semibold text-[#4A495C]">미션 점수 현황</h2>
           <ScoreTooltipQuestion />
         </div>
-        <div className="flex flex-1 items-center justify-start">
+        <div className="flex items-center justify-start">
           <div className="flex items-end">
             <span className="text-3xl font-bold text-primary">
               {currentScore}
@@ -24,6 +26,30 @@ const ScoreSection = ({ totalScore, currentScore }: Props) => {
           </div>
         </div>
       </div>
+      <button
+        className={`flex items-center justify-center rounded-sm border-2 bg-neutral-100 px-4 py-1.5 text-xsmall14 font-medium outline-none ${currentScore < 80 ? 'cursor-not-allowed border-neutral-80 text-neutral-35' : 'border-primary text-primary-dark'}`}
+        onClick={() => {
+          if (currentScore < 80) return;
+          // openModal();
+        }}
+        onMouseEnter={() => setIsHoverButton(true)}
+        onMouseLeave={() => setIsHoverButton(false)}
+      >
+        수료증 발급
+      </button>
+      {isHoverButton && (
+        <div className="absolute bottom-[15px] left-1/2 w-[240px] -translate-x-1/2 translate-y-full transform p-4 pt-[29px] text-xsmall14 text-[#333]">
+          <div className="absolute bottom-0 left-0 z-0 h-[calc(100%-14px)] w-full shadow-[0_0_24px_rgba(204,204,206,0.27)]" />
+          <img
+            className="absolute bottom-0 left-0 z-0 h-full w-full object-fill"
+            src="../public/images/textbox.svg"
+          />
+          <p className="relative z-10">
+            마지막 미션까지 <span className="font-bold">총 80점 이상</span>을
+            획득하면 수료증을 발급 받을 수 있어요!
+          </p>
+        </div>
+      )}
     </section>
   );
 };
