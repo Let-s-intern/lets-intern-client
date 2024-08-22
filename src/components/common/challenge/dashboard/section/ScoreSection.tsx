5ef03aedade2a4fc273fb80bc3420c4872b44e02
Ginieee <kaj1226@naver.com>
2024-08-22 14:37:04 +0900

    fix: 말풍선 src 변경 및 문구 변경, 활성화 시점에 종료여부 반영

--- src/components/common/challenge/dashboard/section/ScoreSection.tsx
+++ src/components/common/challenge/dashboard/section/ScoreSection.tsx
@@ -7,6 +7,7 @@ interface Props {
   totalScore: number;
   currentScore: number;
   programName: string;
+  isProgramDone: boolean;
   desc: string;
   startDate: string;
   endDate: string;
@@ -17,6 +18,7 @@ const ScoreSection = ({
   totalScore,
   currentScore,
   programName,
+  isProgramDone,
   desc,
   startDate,
   endDate,
@@ -43,14 +45,14 @@ const ScoreSection = ({
           </div>
         </div>
       </div>
-      {currentScore < 80 ? (
+      {currentScore < 80 || !isProgramDone ? (
         <button
           className={`flex cursor-not-allowed items-center justify-center rounded-sm border-2 border-neutral-80 bg-neutral-100 px-4 py-1.5 text-xsmall14 font-medium text-neutral-35 outline-none`}
           onMouseEnter={() => {
-            currentScore < 80 && setIsHoverButton(true);
+            setIsHoverButton(true);
           }}
           onMouseLeave={() => {
-            currentScore < 80 && setIsHoverButton(false);
+            setIsHoverButton(false);
           }}
         >
           수료증 발급
@@ -72,15 +74,15 @@ const ScoreSection = ({
         />
       )}
       {isHoverButton && (
-        <div className="absolute bottom-[15px] left-1/2 w-[240px] -translate-x-1/2 translate-y-full transform p-4 pt-[29px] text-xsmall14 text-[#333]">
+        <div className="absolute bottom-[15px] left-1/2 w-[240px] -translate-x-1/2 translate-y-full transform px-[13.5px] pb-4 pt-[29px] text-xsmall14 text-[#333]">
           <div className="absolute bottom-0 left-0 z-0 h-[calc(100%-14px)] w-full shadow-[0_0_24px_rgba(204,204,206,0.27)]" />
           <img
             className="absolute bottom-0 left-0 z-0 h-full w-full object-fill"
-            src="../public/images/textbox.png"
+            src="/images/textbox.png"
           />
           <p className="relative z-10">
-            마지막 미션까지 <span className="font-bold">총 80점 이상</span>을
-            획득하면 수료증을 발급 받을 수 있어요!
+            챌린지 종료 후 <span className="font-bold">총 80점 이상</span>을
+            획득한 경우에 수료증을 발급받을 수 있어요!
           </p>
         </div>
       )}
