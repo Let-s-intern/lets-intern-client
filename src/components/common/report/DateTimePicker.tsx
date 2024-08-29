90995f41dff0614b1c2cc83609c302378ada9b65
Yongyeon Kim <seyeon.dev@gmail.com>
2024-08-29 18:13:39 +0900

    1:1 첨삭 시간 옵션 추가

--- src/components/common/report/DateTimePicker.tsx
+++ src/components/common/report/DateTimePicker.tsx
@@ -3,7 +3,7 @@ import { DatePicker } from '@mui/x-date-pickers';
 
 import './date-pickers-toolbar.scss';
 
-const timeOptions = [10, 11, 12, 13, 14];
+const timeOptions = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 const dateTimePickerSx = {
   width: '50%',
 
