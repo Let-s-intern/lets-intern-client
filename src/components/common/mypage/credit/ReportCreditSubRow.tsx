068ffe295e7a89f61957126f59dc1e9fb9a55732
Ginieee <kaj1226@naver.com>
2024-09-17 22:16:08 +0900

    [Fix] SubRow ㄴ 기호 변경

--- src/components/common/mypage/credit/ReportCreditSubRow.tsx
+++ src/components/common/mypage/credit/ReportCreditSubRow.tsx
@@ -8,7 +8,7 @@ const ReportCreditSubRow = ({
   return (
     <div className="flex w-full flex-col">
       <div className="flex w-full items-center justify-between gap-x-3 py-2 text-xsmall14 text-neutral-45">
-        <div>⨽ {title}</div>
+        <div>└ {title}</div>
         <div className="flex grow items-center justify-end">{content}</div>
       </div>
     </div>
