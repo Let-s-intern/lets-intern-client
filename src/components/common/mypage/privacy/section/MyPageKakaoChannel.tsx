8f53585005bdc3b02592d58e4f37f34bf6ce02ca
Ginieee <kaj1226@naver.com>
2024-10-10 21:09:14 +0900

    [Feat] 마이페이지용 톡채널 추가 버튼 생성

---
+++ src/components/common/mypage/privacy/section/MyPageKakaoChannel.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+
+const MyPageKakaoChannel = () => {
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init('fe2307dd60e05ff8cbb06d777a13e31c');
+    }
+  }, []);
+
+  const onClickAddChannel = () => {
+    window.Kakao.Channel.followChannel({
+      channelPublicId: '_tCeHG',
+    })
+      .then((response: any) => {})
+      .catch((error: any) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div
+      className="flex cursor-pointer items-center justify-between rounded-md bg-[#FEE500] px-5 py-4"
+      onClick={onClickAddChannel}
+    >
+      <div>
+        <h1 className="text-lg font-semibold text-neutral-0 text-opacity-[88%]">
+          렛츠커리어 채널 친구 추가
+        </h1>
+        <p className="text-sm text-neutral-0 text-opacity-[52%]">
+          렛츠커리어 카카오톡 채널을 추가하고,
+          <br />
+          프로그램 모집 및 혜택 알림을 가장 먼저 받을 수 있어요.
+        </p>
+      </div>
+      <img
+        src="/images/kakao_channel.png"
+        width={50}
+        height={50}
+        alt="카카오 채널 추가"
+      />
+    </div>
+  );
+};
+
+export default MyPageKakaoChannel;
