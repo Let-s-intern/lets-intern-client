15c9f470f8b203c4c0e282a5c91b7045e07dd175
Yongyeon Kim <seyeon.dev@gmail.com>
2024-10-27 12:37:15 +0900

    [Feat] 챌린지 수정에 커리큘럼 세션 추가

--- src/pages/admin/program/ChallengeCurriculum.tsx
+++ src/pages/admin/program/ChallengeCurriculum.tsx
@@ -1,12 +1,12 @@
 import { Button, IconButton } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
+import dayjs, { Dayjs } from 'dayjs';
 import { MdDelete } from 'react-icons/md';
 
 import { ChallengeContent } from '@/types/interface';
 import { generateRandomNumber } from '@/utils/random';
 import { Heading2 } from '@components/admin/ui/heading/Heading2';
 import Input from '@components/ui/input/Input';
-import dayjs, { Dayjs } from 'dayjs';
 
 interface ChallengeCurriculumProps {
   curriculum: ChallengeContent['curriculum'];
