605621b06d86d0d5cb3dc39f814604353b7d6c1c
Kim Tae Hoon <eszqsc112@gmail.com>
2024-10-28 23:49:21 +0900

    챌린지 포인트 USER 추가

--- src/components/ChallengeView.tsx
+++ src/components/ChallengeView.tsx
@@ -25,7 +25,10 @@ const ChallengeView: React.FC<{ challenge: ChallengeIdSchema }> = ({
       <div className="px-5 lg:px-10 xl:px-52">
         <Header programTitle={challenge.title ?? ''} />
         <div>네비게이션 바</div>
-        <ChallengePointView point={receivedContent.challengePoint} />
+        <ChallengePointView
+          className="mb-14 sm:mb-44"
+          point={receivedContent.challengePoint}
+        />
         <div>특별 챌린지</div>
         <div>합격자 후기</div>
         <div>프로그램 소개 평균 10초</div>
--- src/components/challenge-view/ChallengePointView.tsx
+++ src/components/challenge-view/ChallengePointView.tsx
@@ -1,15 +1,40 @@
 import { ChallengePoint } from '@/types/interface';
+import Balancer from 'react-wrap-balancer';
 
-const ChallengePointView = ({ point }: { point: ChallengePoint }) => {
+const ChallengePointView = ({
+  point,
+  className,
+}: {
+  point: ChallengePoint;
+  className?: string;
+}) => {
   return (
-    <div>
-      <h2>Challenge Point</h2>
-      <p>{point.weekText}</p>
-      <ul>
-        {point.list?.map((item) => (
-          <li key={item.id}>
-            <h3>{item.title}</h3>
-            <p>{item.subtitle}</p>
+    <div className={className}>
+      <h2 className="sr-only">챌린지 포인트</h2>
+      <p className="mb-10 break-keep text-small20 font-bold text-neutral-0 sm:mb-20">
+        이력서 & 자기소개서 챌린지를 통해{' '}
+        <span className="text-[rgba(255,156,52,1)]">하루 30분</span>, 단{' '}
+        {point.weekText} 안에 이런걸 얻어갈 수 있어요
+      </p>
+      <ul className="space-y-4">
+        {point.list?.map((item, index) => (
+          <li
+            key={item.id}
+            className="flex flex-col items-center gap-4 self-stretch rounded-md bg-[#EEFAFF] px-8 pb-10 pt-8"
+          >
+            <div className="break-keep text-center">
+              <span className="rounded-md bg-[#14BCFF] px-3.5 py-1.5 text-small18 font-semibold text-white">
+                Point {index + 1}
+              </span>
+            </div>
+            <div>
+              <h3 className="mb-2 break-keep text-center text-small20 font-bold text-neutral-0">
+                <Balancer>{item.title}</Balancer>
+              </h3>
+              <p className="break-keep text-center text-xsmall16 font-medium text-neutral-40">
+                <Balancer>{item.subtitle}</Balancer>
+              </p>
+            </div>
           </li>
         ))}
       </ul>
