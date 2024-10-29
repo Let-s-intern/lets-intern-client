15c68b1337e4b6bf391582de05cf38ecb2d41458
Ginieee <kaj1226@naver.com>
2024-10-30 02:13:23 +0900

    [Feat] 프로그램 상페 네비게이션 바 추가

--- src/components/ChallengeView.tsx
+++ src/components/ChallengeView.tsx
@@ -5,6 +5,9 @@ import { ChallengeIdSchema } from '@/schema';
 import { ChallengeContent } from '@/types/interface';
 import Header from '@components/common/program/program-detail/header/Header';
 import { useMemo } from 'react';
+import ChallengeNavigation, {
+  challengeNavigateItems,
+} from './challenge-view/ChallengeNavigation';
 import ChallengePointView from './challenge-view/ChallengePointView';
 import LexicalContent from './common/blog/LexicalContent';
 
@@ -24,42 +27,45 @@ const ChallengeView: React.FC<{ challenge: ChallengeIdSchema }> = ({
   }, [challenge.desc]);
 
   return (
-    <div>
+    <div className="flex w-full flex-col">
       <pre>{JSON.stringify(JSON.parse(challenge.desc || '{}'), null, 2)}</pre>
-      <div className="overflow-x-hidden px-5 lg:px-10 xl:px-52">
-        <Header programTitle={challenge.title ?? ''} />
-
-        <div>네비게이션 바</div>
-        <ChallengePointView
-          className="mb-14 sm:mb-44"
-          point={receivedContent.challengePoint}
-        />
-
-        <div>특별 챌린지, 합격자 후기 (TODO: 본 제목 삭제)</div>
-        <div>
-          {receivedContent.mainDescription?.root ? (
-            <LexicalContent node={receivedContent.mainDescription?.root} />
-          ) : null}
+      <div>
+        <div className="flex w-full px-5 lg:px-10 xl:px-52">
+          <Header programTitle={challenge.title ?? ''} />
         </div>
+        <ChallengeNavigation navItems={challengeNavigateItems} />
+        <div className="flex w-full flex-col overflow-x-hidden px-5 lg:px-10 xl:px-52">
+          <ChallengePointView
+            className="mb-14 sm:mb-44"
+            point={receivedContent.challengePoint}
+          />
 
-        <section className="lg:py-50 lg:gap-50 flex flex-col gap-20 py-16">
-          <ChallengeIntro />
-          <ChallengeCheckList />
-          <ChallengeResult />
-        </section>
+          <div>특별 챌린지, 합격자 후기 (TODO: 본 제목 삭제)</div>
+          <div>
+            {receivedContent.mainDescription?.root ? (
+              <LexicalContent node={receivedContent.mainDescription?.root} />
+            ) : null}
+          </div>
 
-        <div>
-          이 모든 고민을 한번에 해결! 서류 합격률을 300% 높일 수 있는 렛츠커리어
-          챌린지
+          <section className="lg:py-50 lg:gap-50 flex flex-col gap-20 py-16">
+            <ChallengeIntro />
+            <ChallengeCheckList />
+            <ChallengeResult />
+          </section>
+
+          <div>
+            이 모든 고민을 한번에 해결! 서류 합격률을 300% 높일 수 있는
+            렛츠커리어 챌린지
+          </div>
+          <div>커리큘럼</div>
+          <div>차별점</div>
+          <div>여기서 끝이 아니죠 챌린지 참여자만을 위한 트리플 혜택!</div>
+          <div>누적 참여자 1,900+명 참여 만족도 4.9점</div>
+          <div>후기</div>
+          <div>블로그 후기</div>
+          <div>faq</div>
+          <div>모집 개요</div>
         </div>
-        <div>커리큘럼</div>
-        <div>차별점</div>
-        <div>여기서 끝이 아니죠 챌린지 참여자만을 위한 트리플 혜택!</div>
-        <div>누적 참여자 1,900+명 참여 만족도 4.9점</div>
-        <div>후기</div>
-        <div>블로그 후기</div>
-        <div>faq</div>
-        <div>모집 개요</div>
       </div>
     </div>
   );
