e6a66b316f655226ebdb869688365b8c44d8f51b
Yongyeon Kim <seyeon.dev@gmail.com>
2024-10-30 19:09:42 +0900

    [Feat] 모바일 커리큘럼 추가

---
+++ src/pages/common/program/ChallengeCurriculum.tsx
@@ -0,0 +1,54 @@
+import dayjs from 'dayjs';
+import { IoIosArrowDown } from 'react-icons/io';
+
+import { ChallengeCurriculum as ChallengeCurriculumType } from '@/types/interface';
+import Heading2 from '@components/common/program/program-detail/Heading2';
+import SuperTitle from '@components/common/program/program-detail/SuperTitle';
+
+const superTitle = '취업 트랜드를 반영한 체계적인 커리큘럼';
+const title = ['기초부터 결과물까지 가져가는', '완벽한 취업 준비 2주 커리큘럼'];
+
+interface ChallengeCurriculumProps {
+  curriculum: ChallengeCurriculumType[];
+}
+
+function ChallengeCurriculum({ curriculum }: ChallengeCurriculumProps) {
+  if (curriculum === undefined) return <></>;
+
+  return (
+    <section
+      id="curriculum"
+      className="-mx-5 bg-neutral-95 px-5 py-16 lg:-mx-10 lg:px-10 lg:pb-36 lg:pt-28 xl:-mx-52 xl:px-52"
+    >
+      <SuperTitle className="mb-6 text-neutral-45 lg:mb-12">
+        커리큘럼
+      </SuperTitle>
+      <SuperTitle className="mb-1 text-[#00A8EB]">{superTitle}</SuperTitle>
+      <Heading2 className="mb-10 lg:mb-20">{title.join('\n')}</Heading2>
+
+      <div className="flex flex-col gap-4">
+        {curriculum.map((item, index) => (
+          <div key={item.id} className="rounded-md bg-white p-5 pt-4">
+            <div className="mb-2 flex items-center gap-2.5 text-xsmall16 font-semibold text-[#00A8EB]">
+              <span>{index + 1}회차</span>
+              <span>
+                {dayjs(item.startDate).format('M/D')}-
+                {dayjs(item.endDate).format('M/D')}
+              </span>
+            </div>
+            <div className="flex items-center justify-between">
+              <span className="text-small18 font-bold text-neutral-0">
+                {item.title}
+              </span>
+              <IoIosArrowDown color="#ACAFB6" size={24} />
+            </div>
+            <div className="mt-3.5 whitespace-pre-line rounded-md bg-neutral-100 px-4 py-3 text-xsmall14 text-neutral-0">
+              {item.content}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+export default ChallengeCurriculum;
