2b91b84fec70ada3650cb96026bc94d5a57d356a
Yongyeon Kim <seyeon.dev@gmail.com>
2024-10-31 00:32:25 +0900

    [Feat] 모바일 FAQ 추가

--- src/pages/common/program/ChallengeFaq.tsx
+++ src/pages/common/program/ChallengeFaq.tsx
@@ -1,9 +1,12 @@
-import { useGetChallengeFaq } from '@/api/challenge';
-import Heading2 from '@components/common/program/program-detail/Heading2';
-import SuperTitle from '@components/common/program/program-detail/SuperTitle';
 import { ReactNode, useState } from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
 import { useParams } from 'react-router-dom';
 import { twJoin } from 'tailwind-merge';
+
+import { useGetChallengeFaq } from '@/api/challenge';
+import { Faq } from '@/schema';
+import Heading2 from '@components/common/program/program-detail/Heading2';
+import SuperTitle from '@components/common/program/program-detail/SuperTitle';
 
 const superTitle = '자주 묻는 질문';
 const title = '궁금한 점이 있으신가요?';
@@ -39,12 +42,21 @@ function ChallengeFaq() {
         ))}
       </div>
 
-      <div>
-        {faqList.map((faq) => (
-          <div key={faq.id}></div>
-        ))}
+      <div className="mb-10 flex flex-col gap-3">
+        {faqList.map((faq) => {
+          if (faq.category === categoryList[selectedIndex])
+            return <FaqCard key={faq.id} faq={faq} />;
+        })}
       </div>
-      <div></div>
+
+      <div className="flex flex-col items-center gap-3 rounded-md bg-neutral-95 px-8 py-4">
+        <span className="text-xsmall14 font-semibold text-neutral-35">
+          아직 궁금증이 풀리이 않았다면?
+        </span>
+        <button className="rounded-sm border border-neutral-70 bg-white px-5 py-3">
+          1:1 채팅 문의하기
+        </button>
+      </div>
     </section>
   );
 }
@@ -69,6 +81,34 @@ function FaqCategory({
       onClick={onClick}
     >
       {children}
+    </div>
+  );
+}
+
+function FaqCard({ faq }: { faq: Faq }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div
+      key={faq.id}
+      className="overflow-hidden rounded-xxs border border-neutral-80"
+    >
+      <div className="flex items-center justify-between bg-neutral-100 px-4 py-5">
+        <span className="text-xsmall14 font-semibold text-neutral-0">
+          {faq.question}
+        </span>
+        <IoIosArrowDown
+          className={twJoin('cursor-pointer', isOpen && 'rotate-180')}
+          size={24}
+          color="#7A7D84"
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </div>
+      {isOpen && (
+        <div className="border-t border-neutral-80 px-5 py-3 text-xxsmall12 text-neutral-35">
+          {faq.answer}
+        </div>
+      )}
     </div>
   );
 }
