99eeb39a5dffffc264631d1a9fae4db7d0d39c08
Yongyeon Kim <seyeon.dev@gmail.com>
2024-10-30 20:46:26 +0900

    [Feat] 데스크탑 커리큘럼 추가

--- src/pages/common/program/ChallengeCurriculum.tsx
+++ src/pages/common/program/ChallengeCurriculum.tsx
@@ -4,6 +4,7 @@ import { IoIosArrowDown } from 'react-icons/io';
 import { ChallengeCurriculum as ChallengeCurriculumType } from '@/types/interface';
 import Heading2 from '@components/common/program/program-detail/Heading2';
 import SuperTitle from '@components/common/program/program-detail/SuperTitle';
+import { useMediaQuery } from '@mui/material';
 import { useState } from 'react';
 import { twJoin } from 'tailwind-merge';
 
@@ -15,7 +16,9 @@ interface ChallengeCurriculumProps {
 }
 
 function ChallengeCurriculum({ curriculum }: ChallengeCurriculumProps) {
-  const [isOpen, setIsOpen] = useState(Array(curriculum.length).fill(false));
+  const [selectedIndex, setIsSelectedIndex] = useState(0);
+
+  const isDesktop = useMediaQuery('(min-width:991px)');
 
   if (curriculum === undefined) return <></>;
 
@@ -30,12 +33,74 @@ function ChallengeCurriculum({ curriculum }: ChallengeCurriculumProps) {
       <SuperTitle className="mb-1 text-[#00A8EB]">{superTitle}</SuperTitle>
       <Heading2 className="mb-10 lg:mb-20">{title.join('\n')}</Heading2>
 
-      <div className="flex flex-col gap-4">
-        {curriculum.map((item) => (
-          <CurriculumItem key={item.id} item={item} />
-        ))}
-      </div>
+      {isDesktop ? (
+        // 데스크탑 커리큘럼
+        <div className="flex w-full overflow-hidden rounded-md bg-white">
+          <div className="flex w-1/3 flex-col gap-2.5 border-r border-neutral-80 px-10 py-8">
+            {curriculum.map((item, index) => (
+              <SidebarMenu
+                key={item.id}
+                item={item}
+                selected={index === selectedIndex}
+                onClick={() => setIsSelectedIndex(index)}
+              />
+            ))}
+          </div>
+          <div className="w-2/3 px-16 py-10">
+            <span className="text-medium24 font-bold text-neutral-0">
+              {curriculum[selectedIndex].title}
+            </span>
+            <div className="mt-8 whitespace-pre-line bg-neutral-100 px-8 py-5">
+              {curriculum[selectedIndex].content}
+            </div>
+          </div>
+        </div>
+      ) : (
+        // 모바일 커리큘럼
+        <div className="flex flex-col gap-4">
+          {curriculum.map((item) => (
+            <CurriculumItem key={item.id} item={item} />
+          ))}
+        </div>
+      )}
     </section>
+  );
+}
+
+function SidebarMenu({
+  item,
+  selected,
+  onClick,
+}: {
+  item: ChallengeCurriculumType;
+  selected: boolean;
+  onClick?: () => void;
+}) {
+  return (
+    <div
+      key={item.id}
+      className={twJoin(
+        'cursor-pointer rounded-md px-8 py-3',
+        selected && 'bg-[#EEFAFF]',
+      )}
+      onClick={onClick}
+    >
+      <div className="mb-2 flex items-center gap-3 text-small20 font-semibold text-[#00A8EB]">
+        <span>{item.session}</span>
+        <span>
+          {dayjs(item.startDate).format('M/D')}-
+          {dayjs(item.endDate).format('M/D')}
+        </span>
+      </div>
+      <span
+        className={twJoin(
+          'text-medium22 font-bold',
+          selected ? 'text-neutral-0' : 'text-neutral-50',
+        )}
+      >
+        {item.title}
+      </span>
+    </div>
   );
 }
 
