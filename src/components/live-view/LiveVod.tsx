ffde24addda7973f7c0135569be90c387b5cc847
Yongyeon Kim <seyeon.dev@gmail.com>
2024-10-31 16:28:49 +0900

    [Feat] VOD 안내 문구 추가

---
+++ src/components/live-view/LiveVod.tsx
@@ -0,0 +1,16 @@
+const title = 'VOD 녹화본 제공 🎬';
+
+function LiveVod() {
+  return (
+    <div className="rounded-md bg-neutral-95 px-5 py-6">
+      <span className="text-small18 font-bold text-primary">{title}</span>
+      <p className="mt-2.5 text-justify text-xsmall16 font-medium text-neutral-40">
+        결제하신 모든 분들께{' '}
+        <span className="text-primary">세션 VOD 녹화본</span>이 이메일로
+        전송됩니다. LIVE 시간에 일정이 맞지 않더라도 결제 후 편하게 받아보세요!
+      </p>
+    </div>
+  );
+}
+
+export default LiveVod;
