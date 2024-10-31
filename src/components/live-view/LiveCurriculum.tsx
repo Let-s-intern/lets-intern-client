162568a64e695b7e31f9591e907436efe1b91c00
Yongyeon Kim <seyeon.dev@gmail.com>
2024-10-31 20:06:02 +0900

    [Feat] 라이브 커리큘럼 반응형 구현

--- src/components/live-view/LiveCurriculum.tsx
+++ src/components/live-view/LiveCurriculum.tsx
@@ -13,33 +13,38 @@ interface LiveCurriculumProps {
 
 function LiveCurriculum({ curriculum, mentorJob }: LiveCurriculumProps) {
   return (
-    <section className="py-16">
-      <SuperTitle className="text-neutral-45">커리큘럼</SuperTitle>
-      <SuperTitle className="text-primary">{superTitle}</SuperTitle>
-      <Heading2 className="mb-8">{title}</Heading2>
+    <section className="py-16 lg:py-32">
+      <SuperTitle className="mb-6 text-neutral-45 lg:mb-10">
+        커리큘럼
+      </SuperTitle>
+      <SuperTitle className="mb-1 text-primary">{superTitle}</SuperTitle>
+      <Heading2 className="mb-8 lg:mb-20">{title}</Heading2>
 
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 lg:items-center">
         {curriculum.map((item, index) => (
-          <div key={item.id} className="overflow-hidden rounded-md">
-            <div className="flex gap-3 bg-primary px-6 py-4">
+          <div
+            key={item.id}
+            className="overflow-hidden rounded-md lg:flex lg:h-[148px]"
+          >
+            <div className="flex gap-3 bg-primary px-6 py-4 lg:w-[264px] lg:p-6">
               <div className="flex h-5 w-5 items-center justify-center rounded-full bg-primary-30 text-xxsmall12 font-semibold text-primary">
                 {index + 1}
               </div>
-              <div className="flex w-full justify-between">
+              <div className="flex w-full justify-between lg:flex-col">
                 <div className="flex flex-col gap-1">
-                  <span className="text-xxsmall12 font-medium text-primary-10">
+                  <span className="text-xxsmall12 font-medium text-primary-10 lg:text-xsmall14">
                     {mentorJob}
                   </span>
-                  <span className="text-small18 font-semibold text-white">
+                  <span className="text-small18 font-semibold text-white lg:text-small20">
                     {item.title}
                   </span>
                 </div>
-                <span className="text-xsmall16 font-medium text-white">
+                <span className="text-xsmall16 font-medium text-white lg:text-small20">
                   {item.time}
                 </span>
               </div>
             </div>
-            <div className="whitespace-pre-line bg-neutral-95 px-8 py-4">
+            <div className="whitespace-pre-line bg-neutral-95 px-8 py-4 text-xsmall14 text-neutral-0 lg:w-[620px] lg:py-6 lg:text-small20">
               {item.content}
             </div>
           </div>
