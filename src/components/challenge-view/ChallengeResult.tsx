193b9e0eed5bbcaf5811db1749db077b81b45f2f
Yongyeon Kim <seyeon.dev@gmail.com>
2024-11-02 19:05:05 +0900

    [Feat] 콘텐츠를 이미지 요소로 변경

--- src/components/challenge-view/ChallengeResult.tsx
+++ src/components/challenge-view/ChallengeResult.tsx
@@ -1,5 +1,5 @@
 import { useMediaQuery } from '@mui/material';
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { FaCheck } from 'react-icons/fa6';
 
 import { twMerge } from '@/lib/twMerge';
@@ -13,19 +13,15 @@ const superTitle = '이 모든 고민을 한번에 해결!';
 const title = ['서류 합격률을 300% 높일 수 있는', '렛츠커리어 챌린지'];
 const PERSONAL_STATEMENT_CONTENT = [
   {
-    beforeContent:
-      '저는 어릴 때부터 교육 산업에 관심이 있었고, 계속 발전해나가는 A사에 매력을 느낍니다. A사는 특히 한국뿐 아니라 글로벌 시장에서도 영향력을 발휘하고 있어 성장 가능성이 크다고 생각합니다. 유명무실한 A사의 실적에 제가 기여하여 함께 성장하는 직원이 되도록 하겠습니다.',
+    beforeImg: '',
     beforeCaption: '누구나 쓸 수 있는 추상적인 지원동기',
-    afterContent:
-      '[A사 가치의 실현에 동참]\n교육에서 상생 가치의 중요성을 깊이 인식하고, 이를 실천하는 과정에서 보람을 느끼기 때문에, A사의 ESG 가치의 실현 과정에 동참하고 싶습니다. 저소득 중고등학교 멘토링 봉사 활동을 주기적으로 참여하며, 프로그램을 기획·운영한 경험이 있습니다.',
+    afterImg: '',
     afterCaption: 'A사의 가치 기술과 관련된 경험 연결',
   },
   {
-    beforeContent:
-      '저는 서비스 기획자가 되기 위해서 이제까지 다양한 활동을 해왔습니다. IT 연합 동아리를 통해 팀 프로젝트로 앱 기획을 진행했습니다. 또한, 학교 경영학 수업 조별과제에서 리더를 맡아 효율적으로 업무를 분배해서 A+를 받았습니다. 그리고…',
+    beforeImg: '',
     beforeCaption: '직무 경험을 구구절절 나열하는 방식',
-    afterContent:
-      '서비스 기획을 하기 위해서는 데이터 기반의 논리적인 의사 결정 역량이 가장 중요하다고 생각합니다. 실제로 저는 IT 연합 동아리에서 사용자 인터뷰를 바탕으로 pain point를 도출한 이후 서비스 기획안을 작성하여 설득력을 높인 적 있습니다.',
+    afterImg: '',
     afterCaption: '나만의 장단점 및 역량 키워드 도출',
   },
 ];
@@ -72,6 +68,21 @@ interface ChallengeResultProps {
 }
 
 function ChallengeResult({ colors, challengeType }: ChallengeResultProps) {
+  const isDesktop = useMediaQuery('(min-width: 991px)');
+
+  const contents = useMemo(() => {
+    const { PORTFOLIO, PERSONAL_STATEMENT } = challengeTypeSchema.enum;
+
+    switch (challengeType) {
+      case PORTFOLIO:
+        return PORTFOLIO_CONTENT;
+      case PERSONAL_STATEMENT:
+        return PERSONAL_STATEMENT_CONTENT;
+      default:
+        return CAREER_START_CONTENT;
+    }
+  }, [challengeType]);
+
   return (
     <section>
       <SuperTitle
@@ -83,54 +94,47 @@ function ChallengeResult({ colors, challengeType }: ChallengeResultProps) {
       <Heading2 className="mb-10 md:mb-20">{title.join('\n')}</Heading2>
 
       <div className="z-10 -mx-5 overflow-x-auto pl-5 sm:mx-0 sm:pl-0">
-        <div className="flex w-fit flex-col gap-8 md:gap-16">
-          <ResultContent colors={colors} challengeType={challengeType} />
+        <div className="flex min-w-fit flex-col gap-8 md:gap-16">
+          {contents.map((content) => (
+            <div
+              key={content.beforeCaption}
+              className="flex flex-nowrap items-start gap-2 md:gap-3"
+            >
+              <div className="flex flex-1 flex-col items-center gap-4">
+                <BadgedBox badgeContent="Before">
+                  <ResultImg
+                    src={content.beforeImg}
+                    alt={content.beforeCaption}
+                  />
+                </BadgedBox>
+                <span className="text-xsmall14 font-semibold text-neutral-0 md:text-small20">
+                  {content.beforeCaption}
+                </span>
+              </div>
+              <div className="flex flex-1 flex-col items-center gap-4">
+                <BadgedBox badgeContent="After" colors={colors}>
+                  <ResultImg
+                    src={content.afterImg}
+                    alt={content.afterCaption}
+                  />
+                </BadgedBox>
+                <div className="flex items-start gap-1">
+                  <FaCheck
+                    className="mt-1"
+                    color={colors.primary}
+                    size={isDesktop ? 20 : 16}
+                  />
+                  <span className="text-xsmall14 font-semibold text-neutral-0 md:text-small20">
+                    {content.afterCaption}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
   );
-}
-
-function ResultContent({ colors, challengeType }: ChallengeResultProps) {
-  const isDesktop = useMediaQuery('(min-width: 991px)');
-
-  const { PERSONAL_STATEMENT, PORTFOLIO } = challengeTypeSchema.enum;
-
-  if (challengeType === PERSONAL_STATEMENT)
-    return PERSONAL_STATEMENT_CONTENT.map((content) => (
-      <div
-        key={content.beforeContent}
-        className="flex flex-nowrap items-center gap-2 md:gap-3"
-      >
-        <div className="flex flex-1 flex-col items-center gap-4">
-          <BadgedBox badgeContent="Before">
-            <p className="mx-4 mt-4 h-full whitespace-pre-line bg-white px-6 pt-7 text-[8px] font-medium md:mx-8 md:mt-8 md:px-12 md:pt-14 md:text-xsmall16">
-              {content.beforeContent}
-            </p>
-          </BadgedBox>
-          <span className="text-xsmall14 font-semibold text-neutral-0 md:text-small20">
-            {content.beforeCaption}
-          </span>
-        </div>
-        <div className="flex flex-1 flex-col items-center gap-4">
-          <BadgedBox badgeContent="After" colors={colors}>
-            <p className="mx-4 mt-4 h-full whitespace-pre-line bg-white px-6 pt-7 text-[8px] font-medium md:mx-8 md:mt-8 md:px-12 md:pt-14 md:text-xsmall16">
-              {content.afterContent}
-            </p>
-          </BadgedBox>
-          <div className="flex items-center gap-1">
-            <FaCheck color={colors.primary} size={isDesktop ? 20 : 16} />
-            <span className="text-xsmall14 font-semibold text-neutral-0 md:text-small20">
-              {content.afterCaption}
-            </span>
-          </div>
-        </div>
-      </div>
-    ));
-
-  if (challengeType === PORTFOLIO) return <></>;
-
-  return <></>;
 }
 
 function BadgedBox({
@@ -166,6 +170,16 @@ function BadgedBox({
       {children}
     </OutlinedBox>
   );
+}
+
+function ResultImg({
+  src,
+  alt,
+}: React.DetailedHTMLProps<
+  React.ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+>) {
+  return <img className="aspect-video h-auto w-52" src={src} alt={alt} />;
 }
 
 export default ChallengeResult;
