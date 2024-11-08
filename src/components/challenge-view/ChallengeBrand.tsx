b8969f0e846625c454ca0ed712c2b20dd0b5d631
Yongyeon Kim <seyeon.dev@gmail.com>
2024-11-08 11:10:12 +0900

    [Style] 챌린지 브랜드 스타일 수정

--- src/components/challenge-view/ChallengeBrand.tsx
+++ src/components/challenge-view/ChallengeBrand.tsx
@@ -1,5 +1,4 @@
 import { ChallengeColor } from '@components/ChallengeView';
-import { useMediaQuery } from '@mui/material';
 
 const brandInfo = {
   totalParticipants: '2000+',
@@ -9,10 +8,8 @@ const brandInfo = {
 };
 
 const ChallengeBrand = ({ colors }: { colors: ChallengeColor }) => {
-  const isDesktop = useMediaQuery('(min-width: 991px)');
-
   return (
-    <section className="flex w-full flex-col gap-y-8 py-8 md:gap-y-20 md:pb-40 md:pt-10">
+    <section className="flex w-full flex-col gap-y-8 py-8 md:gap-y-20 md:pb-32 md:pt-10">
       <div className="flex w-full flex-col whitespace-pre text-small20 font-bold md:items-center md:text-xlarge28">
         <p>취업 준비의 든든한 지원군,</p>
         <p>
@@ -57,15 +54,12 @@ const ChallengeBrand = ({ colors }: { colors: ChallengeColor }) => {
       </div>
       <div className="flex w-full flex-col items-center justify-center gap-y-6 md:gap-y-8">
         <iframe
-          width="560"
-          height={isDesktop ? '560' : '315'}
           src="https://www.youtube.com/embed/_xTOxSSqgzA"
           title="YouTube video player"
-          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-          className="aspect-video w-full max-w-3xl"
-        ></iframe>
+          className="aspect-video w-full"
+        />
         <div
           className="flex w-full items-center justify-center rounded-md px-4 py-[14px] md:py-5"
           style={{ backgroundColor: colors.primaryLight }}
