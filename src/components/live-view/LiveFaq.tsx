057f69e99e630085c2cafb8da634edc50a8806c9
Ginieee <kaj1226@naver.com>
2024-11-10 13:40:01 +0900

    [Fix] FAQ 가장 바깥에 id 삽입

--- src/components/live-view/LiveFaq.tsx
+++ src/components/live-view/LiveFaq.tsx
@@ -4,6 +4,7 @@ import { useGetLiveFaq } from '@/api/program';
 import { FaqCard } from '@components/challenge-view/ChallengeFaq';
 import Heading2 from '@components/common/program/program-detail/Heading2';
 import SuperTitle from '@components/common/program/program-detail/SuperTitle';
+import { LIVE_FAQ_ID } from '@components/ProgramDetailNavigation';
 
 const superTitle = '자주 묻는 질문';
 const title = '궁금한 점이 있으신가요?';
@@ -18,9 +19,9 @@ function LiveFaq() {
   if (!faqList) return <></>;
 
   return (
-    <div
-      id="faq"
-      className="pt-20 md:flex md:flex-col md:items-center md:pt-40"
+    <section
+      id={LIVE_FAQ_ID}
+      className="live-faq flex w-full max-w-[1000px] flex-col px-5 py-20 md:items-center md:px-10 md:py-40 lg:px-0"
     >
       <SuperTitle className="mb-6 text-primary md:mb-12">FAQ</SuperTitle>
       <SuperTitle className="text-primary">{superTitle}</SuperTitle>
@@ -31,7 +32,7 @@ function LiveFaq() {
           <FaqCard key={faq.id} faq={faq} />
         ))}
       </div>
-    </div>
+    </section>
   );
 }
 
