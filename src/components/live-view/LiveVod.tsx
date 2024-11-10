f7f775f0ecffc178a0c34d6a64eb283df0d68f31
Yongyeon Kim <seyeon.dev@gmail.com>
2024-11-10 20:04:35 +0900

    [Style] vod 제공 안내 줄바꿈 적용

--- src/components/live-view/LiveVod.tsx
+++ src/components/live-view/LiveVod.tsx
@@ -9,7 +9,8 @@ function LiveVod() {
       <p className="mt-2.5 text-justify text-xsmall16 font-medium text-neutral-40 md:text-center md:text-small20">
         결제하신 모든 분들께{' '}
         <span className="text-primary">세션 VOD 녹화본</span>이 이메일로
-        전송됩니다. LIVE 시간에 일정이 맞지 않더라도 결제 후 편하게 받아보세요!
+        전송됩니다. <br className="hidden md:block" />
+        LIVE 시간에 일정이 맞지 않더라도 결제 후 편하게 받아보세요!
       </p>
     </div>
   );
