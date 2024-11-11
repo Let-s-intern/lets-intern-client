e774f11e3bd4a826938c05a2628ea99fb4e24f07
Yongyeon Kim <seyeon.dev@gmail.com>
2024-11-11 14:55:30 +0900

    [Fix] faq 카테고리 로직 수정 초기화 시 setContent 실행

--- src/pages/admin/program/ChallengeFaqCategory.tsx
+++ src/pages/admin/program/ChallengeFaqCategory.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { useGetFaq } from '@/api/faq';
 import { ChallengeContent } from '@/types/interface';
 import { Heading2 } from '@components/admin/ui/heading/Heading2';
@@ -5,16 +7,26 @@ import Input from '@components/ui/input/Input';
 
 interface ChallengeFaqCategoryProps {
   faqCategory: ChallengeContent['faqCategory'];
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  setContent: React.Dispatch<React.SetStateAction<ChallengeContent>>;
 }
 
 function ChallengeFaqCategory({
   faqCategory,
-  onChange,
+  setContent,
 }: ChallengeFaqCategoryProps) {
   const { data } = useGetFaq('CHALLENGE');
 
   const categoryList = [...new Set(data?.faqList.map((faq) => faq.category))];
+
+  useEffect(() => {
+    if (!data || (faqCategory && faqCategory.length > 0)) return;
+
+    // [Default] 입력값이 없으면 모든 카테고리를 포함하도록 초기화
+    setContent((prev) => ({
+      ...prev,
+      faqCategory: categoryList as string[],
+    }));
+  }, [data, faqCategory, categoryList]);
 
   return (
     <>
@@ -26,13 +38,18 @@ function ChallengeFaqCategory({
         placeholder="카테고리를 순서대로 입력해주세요 (예: 참여 중, 참여 전, 미션)"
         size="small"
         defaultValue={
-          faqCategory
+          faqCategory && faqCategory.length > 0
             ? faqCategory.join(', ')
-            : categoryList
+            : categoryList && categoryList.length > 0
               ? categoryList.join(', ')
-              : ''
+              : undefined
         }
-        onChange={onChange}
+        onChange={(e) => {
+          setContent((prev) => ({
+            ...prev,
+            faqCategory: e.target.value.split(',').map((item) => item.trim()),
+          }));
+        }}
       />
     </>
   );
