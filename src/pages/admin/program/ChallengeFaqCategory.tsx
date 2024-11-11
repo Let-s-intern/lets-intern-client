5f8e3bb1d8825dd145ee15b34ee49d32c8eb4d10
Yongyeon Kim <seyeon.dev@gmail.com>
2024-11-11 12:35:50 +0900

    [Feat] FAQ 디폴트 값을 모든 카테고리로 설정

--- src/pages/admin/program/ChallengeFaqCategory.tsx
+++ src/pages/admin/program/ChallengeFaqCategory.tsx
@@ -1,3 +1,4 @@
+import { useGetFaq } from '@/api/faq';
 import { ChallengeContent } from '@/types/interface';
 import { Heading2 } from '@components/admin/ui/heading/Heading2';
 import Input from '@components/ui/input/Input';
@@ -11,6 +12,10 @@ function ChallengeFaqCategory({
   faqCategory,
   onChange,
 }: ChallengeFaqCategoryProps) {
+  const { data } = useGetFaq('CHALLENGE');
+
+  const categoryList = [...new Set(data?.faqList.map((faq) => faq.category))];
+
   return (
     <>
       <Heading2 className="mb-3">FAQ 카테고리 순서</Heading2>
@@ -20,7 +25,13 @@ function ChallengeFaqCategory({
         name="faqCategory"
         placeholder="카테고리를 순서대로 입력해주세요 (예: 참여 중, 참여 전, 미션)"
         size="small"
-        defaultValue={faqCategory ? faqCategory.join(', ') : ''}
+        defaultValue={
+          faqCategory
+            ? faqCategory.join(', ')
+            : categoryList
+              ? categoryList.join(', ')
+              : ''
+        }
         onChange={onChange}
       />
     </>
