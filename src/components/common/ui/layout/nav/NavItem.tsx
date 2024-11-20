f834421151b1f7a8d4168a8daca24df9bee77be8
Ginieee <kaj1226@naver.com>
2024-11-21 00:19:24 +0900

    [Fix] NavBar에 subChildren 등 props 수정

--- src/components/common/ui/layout/nav/NavItem.tsx
+++ src/components/common/ui/layout/nav/NavItem.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 interface NavItemProps {
@@ -6,22 +7,48 @@ interface NavItemProps {
   active?: boolean;
   as?: keyof JSX.IntrinsicElements;
   children: React.ReactNode;
+  subChildren?: {
+    to: string;
+    children: string;
+  }[];
 }
 
-const NavItem = ({ to, active, as, children }: NavItemProps) => {
-  const Wrapper = as || Link;
+const NavItem = ({ to, active, children, subChildren }: NavItemProps) => {
+  const [isOpen, setIsOpen] = useState(false);
   const style = {
-    'text-1.125-bold text-neutral-0': active,
-    'text-1.125-medium text-neutral-60': !active,
+    'text-1.125-bold text-neutral-0': active || (subChildren && isOpen),
+    'text-1.125-medium text-neutral-60': !active || (subChildren && !isOpen),
   };
 
-  return (
-    <Wrapper
-      to={to || '#'}
-      className={clsx(style, 'hidden cursor-pointer xl:block')}
+  return subChildren ? (
+    <div
+      className={clsx(style, 'relative hidden cursor-pointer xl:block')}
+      onClick={() => setIsOpen(!isOpen)}
     >
       {children}
-    </Wrapper>
+      {subChildren && isOpen && (
+        <div className="absolute left-0 top-[calc(100%+25px)] z-10 flex w-full flex-col rounded-md bg-white py-1 shadow-lg">
+          {subChildren.map((child) => (
+            <Link
+              key={child.to}
+              to={child.to}
+              target="_blank"
+              rel="noreferrer"
+              className="px-4 py-2.5 text-xsmall16 font-medium text-neutral-60 hover:text-black"
+            >
+              {child.children}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  ) : (
+    <Link
+      to={to || '#'}
+      className={clsx(style, 'relative hidden cursor-pointer xl:block')}
+    >
+      {children}
+    </Link>
   );
 };
 
