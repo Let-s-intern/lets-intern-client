8df7abe11d7ee757ea891af616268790b86b8156
Ginieee <kaj1226@naver.com>
2024-12-01 00:39:47 +0900

    [Feat] useGetTotalReview 생성

--- src/api/challenge.ts
+++ src/api/challenge.ts
@@ -1,8 +1,10 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { z } from 'zod';
 import {
   challengeTitleSchema,
   faqSchema,
   getChallengeIdSchema,
+  reviewSchema,
 } from '../schema';
 import axios from '../utils/axios';
 
@@ -91,5 +93,18 @@ export const useGetChallengeFaq = (challengeId: number | string) => {
       const res = await axios.get(`/challenge/${challengeId}/faqs`);
       return faqSchema.parse(res.data.data);
     },
+  });
+};
+
+export const useGetTotalReview = (
+  type: 'CHALLENGE' | 'LIVE' | 'VOD' | 'REPORT',
+) => {
+  return useQuery({
+    queryKey: ['useGetTotalReview', type],
+    queryFn: async () => {
+      const res = await axios.get('/review');
+      return z.array(reviewSchema).parse(res.data.data);
+    },
+    refetchOnWindowFocus: false,
   });
 };
--- src/schema.ts
+++ src/schema.ts
@@ -122,6 +122,16 @@ export type Faq = z.infer<typeof faq>;
 
 export const faqSchema = z.object({ faqList: z.array(faq) });
 
+export const reviewSchema = z.object({
+  id: z.number(),
+  nps: z.number(),
+  npsAns: z.string(),
+  npsCheckAns: z.boolean(),
+  content: z.string(),
+  score: z.number(),
+  createdDate: z.string(),
+});
+
 const missionStatusType = z.union([
   z.literal('WAITING'),
   z.literal('CHECK_DONE'),
