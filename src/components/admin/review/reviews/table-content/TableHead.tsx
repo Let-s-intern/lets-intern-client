3926030b81f6cbc47f1e9617c5b73e2f7e9b70f4
Ginieee <kaj1226@naver.com>
2024-12-01 21:28:28 +0900

    [Fix] ReviewsTableHead 정렬조건 추가

--- src/components/admin/review/reviews/table-content/TableHead.tsx
+++ src/components/admin/review/reviews/table-content/TableHead.tsx
@@ -1,11 +1,46 @@
-import TH from '../../../ui/table/regacy/TH';
+import TH, { THProps } from '../../../ui/table/regacy/TH';
 
-const ReviewsTableHead = () => {
+export interface ReviewsTableHeadProps {
+  filter: {
+    programTitle: THProps['inOrder'];
+    createdDate: THProps['inOrder'];
+  };
+  setFilter: (filter: {
+    programTitle: THProps['inOrder'];
+    createdDate: THProps['inOrder'];
+  }) => void;
+}
+
+const ReviewsTableHead = ({ filter, setFilter }: ReviewsTableHeadProps) => {
+  const handleProgramTitleHeadClick = () => {
+    if (filter.programTitle === 'ASCENDING') {
+      setFilter({ ...filter, programTitle: 'DESCENDING' });
+    } else if (filter.programTitle === 'DESCENDING') {
+      setFilter({ ...filter, programTitle: null });
+    } else if (filter.programTitle === null) {
+      setFilter({ ...filter, programTitle: 'ASCENDING' });
+    }
+  };
+
+  const handleCreatedDateHeadClick = () => {
+    if (filter.createdDate === 'ASCENDING') {
+      setFilter({ ...filter, createdDate: 'DESCENDING' });
+    } else if (filter.createdDate === 'DESCENDING') {
+      setFilter({ ...filter, createdDate: null });
+    } else if (filter.createdDate === null) {
+      setFilter({ ...filter, createdDate: 'ASCENDING' });
+    }
+  };
+
   return (
     <thead>
       <tr>
-        <TH>작성 일자</TH>
-        <TH>프로그램 명</TH>
+        <TH inOrder={filter.createdDate} onClick={handleCreatedDateHeadClick}>
+          작성 일자
+        </TH>
+        <TH inOrder={filter.programTitle} onClick={handleProgramTitleHeadClick}>
+          프로그램 명
+        </TH>
         <TH>이름</TH>
         <TH>NPS 점수</TH>
         <TH>NPS 이유</TH>
