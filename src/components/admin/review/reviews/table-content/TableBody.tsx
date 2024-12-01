bc94f864edb1e7b1655cd44b481852b1705c5dbc
Ginieee <kaj1226@naver.com>
2024-12-01 21:28:08 +0900

    [Fix] 리뷰 TableBody에 filter 및 노출여부 변경 전달

--- src/components/admin/review/reviews/table-content/TableBody.tsx
+++ src/components/admin/review/reviews/table-content/TableBody.tsx
@@ -1,19 +1,38 @@
+import { useEditReviewVisible } from '@/api/challenge';
 import { ReviewType } from '@/schema';
+import { Checkbox } from '@mui/material';
 import dayjs from 'dayjs';
 import TD from '../../../ui/table/regacy/TD';
 
 interface ReviewTableBodyProps {
+  type: string;
+  programTitle?: string | null;
+  createDate?: string | null;
   reviewList: ReviewType[];
 }
 
-const TableBody = ({ reviewList }: ReviewTableBodyProps) => {
+const TableBody = ({
+  type,
+  programTitle,
+  createDate,
+  reviewList,
+}: ReviewTableBodyProps) => {
+  const { mutate: editReviewVisible } = useEditReviewVisible();
+
+  const handleVisibleCheckboxClicked = (
+    reviewId: number,
+    isVisible: boolean,
+  ) => {
+    editReviewVisible({ reviewId, isVisible, type, programTitle, createDate });
+  };
+
   return (
     <thead>
       {reviewList.map((review) => (
         <tr key={review.id}>
           <TD>{dayjs(review.createdDate).format('YYYY.MM.DD')}</TD>
-          <TD>프로그램명</TD>
-          <TD>이름</TD>
+          <TD>{review.programTitle ?? '프로그램 없음'}</TD>
+          <TD>{review.name ?? '-'}</TD>
           <TD>{review.nps}</TD>
           <TD>
             <p className="mx-auto w-full max-w-60 whitespace-pre-wrap break-words text-center">
@@ -23,7 +42,14 @@ const TableBody = ({ reviewList }: ReviewTableBodyProps) => {
           <TD>{review.npsCheckAns ? '추천' : '비추천'}</TD>
           <TD>{review.score}</TD>
           <TD>{review.content}</TD>
-          <TD>노출여부</TD>
+          <TD>
+            <Checkbox
+              checked={review.isVisible ?? false}
+              onChange={() =>
+                handleVisibleCheckboxClicked(review.id, !review.isVisible)
+              }
+            />
+          </TD>
         </tr>
       ))}
     </thead>
