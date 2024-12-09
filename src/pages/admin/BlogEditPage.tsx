e3eef40c0ed8104beb52efb0ec5a8e5ad9d5bfc1
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-09 11:53:33 +0900

    [Fix] 블로그 게시 로직 수정 게시 일자가 있으면 그대로, 없으면 현재 날짜로 설정

--- src/pages/admin/BlogEditPage.tsx
+++ src/pages/admin/BlogEditPage.tsx
@@ -132,8 +132,10 @@ const BlogEditPage = () => {
       tagList: editingValue.tagList.map((tag) => tag.id),
       displayDate:
         name === 'publish'
-          ? dayjs().format('YYYY-MM-DDTHH:mm')
-          : dateTime?.format('YYYY-MM-DDTHH:mm') || '',
+          ? dateTime
+            ? dateTime?.format('YYYY-MM-DDTHH:mm')
+            : dayjs().format('YYYY-MM-DDTHH:mm')
+          : (dateTime?.format('YYYY-MM-DDTHH:mm') ?? ''),
     });
 
     setSnackbar('블로그가 수정되었습니다.');
