fddfe70264a32c1b37e811eac81245ccc43c0731
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-16 16:42:32 +0900

    [Style] 취업 연구팀 소개 섹션 스타일 수정

--- src/components/common/report/ResearchTeamSection.tsx
+++ src/components/common/report/ResearchTeamSection.tsx
@@ -4,7 +4,7 @@ import { ReportColors } from '@/types/interface';
 import DetailHeading2 from './DetailHeading2';
 import SuperTitle from './SuperTitle';
 
-const SUPER_TITLE = '이력서 작성 현황 체크리스트';
+const SUPER_TITLE = '믿고 맡겨도 될까? 더이상 걱정마세요.';
 const HEADING =
   '당신을 합격의 길로 이끌,\n렛츠커리어 취업 연구팀을 소개합니다.';
 
@@ -20,18 +20,18 @@ const ResearchTeamSection = ({ colors }: ResearchTeamSectionProps) => {
   };
 
   return (
-    <div className="max-w-[1000px] px-5 py-14 lg:px-0">
-      <SuperTitle className="mb-1 md:mb-3" style={superTitleStyle}>
+    <div className="max-w-[1000px] px-5 py-14 md:pb-36 md:pt-32 lg:px-0">
+      <SuperTitle className="mb-2 md:mb-3" style={superTitleStyle}>
         {SUPER_TITLE}
       </SuperTitle>
       <DetailHeading2>{HEADING}</DetailHeading2>
-      <p className="mb-8 mt-5 text-center text-xsmall14">
+      <p className="mb-8 mt-5 text-center text-xsmall14 md:mb-16 md:mt-8 md:text-small20">
         렛츠커리어 취업 연구팀은
-        <br />
+        <br className="md:hidden" />
         대기업 및 IT 업계 다양한 직무 현직자들로 구성되어,
         <br />
         최신 채용 트렌드와 서류 심사 기준을 반영한
-        <br />
+        <br className="md:hidden" />
         실질적이고 전문적인 피드백을 제공합니다.
       </p>
       <div className="w-full">
