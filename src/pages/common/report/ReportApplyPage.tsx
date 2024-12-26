90220c1c7693426b2b5a93e191cee4a89ffeb05e
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-26 18:05:51 +0900

    [Chore] console.log 삭제

--- src/pages/common/report/ReportApplyPage.tsx
+++ src/pages/common/report/ReportApplyPage.tsx
@@ -40,8 +40,6 @@ const ReportApplyPage = () => {
     validate,
   } = useReportApplicationStore();
 
-  console.log('신청서:', reportApplication);
-
   const convertFile = async () => {
     // 파일 변환
     if (applyFile) {
