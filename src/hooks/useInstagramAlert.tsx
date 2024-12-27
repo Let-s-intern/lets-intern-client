502364641a06b335752ce8a8605ffaca34c6e587
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-27 18:39:34 +0900

    [Refactor] 인스타그램 알람 커스텀 훅 분리

---
+++ src/hooks/useInstagramAlert.tsx
@@ -0,0 +1,13 @@
+import { useRef, useState } from 'react';
+
+export default function useInstagramAlert() {
+  const isInstagram = useRef(
+    typeof window !== 'undefined'
+      ? window.navigator.userAgent.includes('Instagram')
+      : false,
+  );
+
+  const [showInstagramAlert, setShowInstagramAlert] = useState(false);
+
+  return { isInstagram, showInstagramAlert, setShowInstagramAlert };
+}
