368dcbd2fc6c794547a8095b4fcf5092db3820fe
Ginieee <kaj1226@naver.com>
2024-12-29 04:32:18 +0900

    [Fix] 스텝 색상 변경

--- src/components/common/report/ReportIntroSection.tsx
+++ src/components/common/report/ReportIntroSection.tsx
@@ -118,8 +118,9 @@ const ReportIntroSection = ({ type }: ReportIntroSectionProps) => {
   const stepStyle = {
     backgroundColor:
       type === 'PERSONAL_STATEMENT'
-        ? personalStatementColors.F9EEFF
+        ? personalStatementColors.CA60FF
         : resumeColors._4FDA46,
+    color: type === 'PERSONAL_STATEMENT' ? 'white' : 'black',
   };
   const boxStyle = {
     backgroundColor:
@@ -251,7 +252,7 @@ const ReportIntroSection = ({ type }: ReportIntroSectionProps) => {
               style={boxStyle}
             >
               <span
-                className="mt-1 flex h-4 w-4 shrink-0 items-center justify-center rounded-full text-xxsmall12 font-semibold text-black md:h-8 md:w-8 md:text-small20"
+                className="mt-1 flex h-4 w-4 shrink-0 items-center justify-center rounded-full text-xxsmall12 font-semibold md:h-[30px] md:w-[30px] md:text-small20"
                 style={stepStyle}
               >{`${index + 1}`}</span>
               <div className="flex flex-1 flex-col items-start gap-y-1 text-start text-xsmall14 font-medium md:text-small20">
