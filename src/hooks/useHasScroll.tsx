79322d12dacf7067aa6096ea6a4d210230c2bf47
Yongyeon Kim <seyeon.dev@gmail.com>
2024-12-30 10:40:07 +0900

    [Fix] 랜더링 후 스크롤 길이 체크

--- src/hooks/useHasScroll.tsx
+++ src/hooks/useHasScroll.tsx
@@ -17,8 +17,9 @@ const throttle = (callback: () => void, delay: number) => {
  * 스크롤이 생기면 hasScroll을 true로 설정, 아니면 false로 설정
  */
 export default function useHasScroll() {
-  const scrollRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>();
   const [hasScroll, setHasScroll] = useState(true);
+  const [mounted, setMounted] = useState(false);
 
   const onResize = () => {
     setHasScroll(
@@ -26,14 +27,19 @@ export default function useHasScroll() {
     );
   };
 
+  // 랜더링 후 스크롤 길이 체크하도록 보장
+  useEffect(() => setMounted(true), []);
+
   useEffect(() => {
+    if (!mounted) return;
+
     onResize(); // 최초 실행
     window.addEventListener('resize', throttle(onResize, 250));
 
     return () => {
       window.removeEventListener('resize', throttle(onResize, 250));
     };
-  }, []);
+  }, [mounted]);
 
   return { scrollRef, hasScroll };
 }
