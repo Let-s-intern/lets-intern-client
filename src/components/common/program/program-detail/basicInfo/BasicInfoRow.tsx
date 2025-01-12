6a762f050202922da421a7e900e3399f543a4dbe
Ginieee <kaj1226@naver.com>
2025-01-12 21:41:03 +0900

    [Fix] BasicInfoRow flex-col로 수정

--- src/components/common/program/program-detail/basicInfo/BasicInfoRow.tsx
+++ src/components/common/program/program-detail/basicInfo/BasicInfoRow.tsx
@@ -8,14 +8,14 @@ export interface BasicInfoRowProps {
 
 const BasicInfoRow = ({ icon, title, content }: BasicInfoRowProps) => {
   return (
-    <div className="flex w-full items-start justify-between">
+    <div className="flex w-full flex-col gap-y-2">
       <div className="flex shrink-0 items-center gap-2">
         {icon}
         <span className="text-xsmall16 font-semibold text-neutral-0">
           {title}
         </span>
       </div>
-      <div className="whitespace-pre text-pretty break-all text-end text-xsmall14 font-normal text-neutral-0 xs:text-xsmall16">
+      <div className="whitespace-pre text-pretty break-all text-xsmall14 font-normal text-neutral-0 xs:text-xsmall16">
         {content}
       </div>
     </div>
