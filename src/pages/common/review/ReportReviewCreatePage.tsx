29c45e2cc666d3f4cf2609adc559f4526a6fd851
Yongyeon Kim <seyeon.dev@gmail.com>
2025-01-18 10:19:31 +0900

    [Feat] 서류 진단 후기 작성 페이지 추가

--- src/pages/common/review/ReportReviewCreatePage.tsx
+++ src/pages/common/review/ReportReviewCreatePage.tsx
@@ -17,7 +17,7 @@ import TenScore from '@components/common/review/score/TenScore';
 import BackHeader from '@components/common/ui/BackHeader';
 import BaseButton from '@components/common/ui/button/BaseButton';
 
-const ChallengeReviewCreatePage = () => {
+const ReportReviewCreatePage = () => {
   const params = useParams();
   const navigate = useNavigate();
   const isDesktop = useMediaQuery('(min-width:768px)');
@@ -30,12 +30,12 @@ const ChallengeReviewCreatePage = () => {
   const [npsAns, setNpsAns] = useState('');
   ////
 
-  const programId = params.programId;
+  const reportId = params.reportId;
 
-  const { data: programTitle } = useQuery({
-    queryKey: ['program', programId],
+  const { data: reportTitle } = useQuery({
+    queryKey: ['report', reportId],
     queryFn: async () => {
-      const res = await axios.get(`/challenge/${programId}/title`);
+      const res = await axios.get(`/report/${reportId}/title`);
       return res.data.data.title;
     },
     retry: 1,
@@ -66,10 +66,10 @@ const ChallengeReviewCreatePage = () => {
           {/* 만족도 평가 */}
           <section>
             <ReviewQuestion required className="mb-1">
-              1. {josa(programTitle ?? '', '은/는')} 어떠셨나요?
+              1. {josa(reportTitle ?? '', '은/는')} 어떠셨나요?
             </ReviewQuestion>
             <ReviewInstruction className="mb-5">
-              {programTitle}의 만족도를 0~10점 사이로 평가해주세요!
+              {reportTitle}의 만족도를 0~10점 사이로 평가해주세요!
             </ReviewInstruction>
             <TenScore tenScore={satisfaction} setTenScore={setSatisfaction} />
           </section>
@@ -77,15 +77,15 @@ const ChallengeReviewCreatePage = () => {
           {/* 추천 정도*/}
           <section>
             <ReviewQuestion required className="mb-1">
-              2. {josa(programTitle ?? '', '을/를')} 주변에 얼마나 추천하고
+              2. {josa(reportTitle ?? '', '을/를')} 주변에 얼마나 추천하고
               싶으신가요?
             </ReviewQuestion>
             <ReviewInstruction className="mb-5">
-              {programTitle}의 만족도를 0~10점 사이로 평가해주세요!
+              {reportTitle}의 만족도를 0~10점 사이로 평가해주세요!
             </ReviewInstruction>
             {/* [참고] 몇 점을 선택하냐에 따라 질문이 증식함 */}
             <RecommendReviewField
-              programTitle={programTitle}
+              programTitle={reportTitle}
               tenScore={tenScore}
               setTenScore={setTenScore}
               hasRecommendationExperience={hasRecommendationExperience}
@@ -95,30 +95,31 @@ const ChallengeReviewCreatePage = () => {
             />
           </section>
 
-          {/* 목표 달성 */}
+          {/* 서류 작성 고민 */}
           <section>
             <ReviewQuestion required className="mb-5">
-              3. {josa(programTitle ?? '', '을/를')} 참여하기 전의 목표를 어떻게
-              달성하셨나요?
+              3. {reportTitle} 이용 후에 서류 작성 고민이 해결되셨나요?*
             </ReviewQuestion>
             <GoalOrConcernsBox className="mb-3">
               <span className="text-xsmall14">
                 {/* TODO: 사용자 이름 넣어야 함 */}
-                🎯 <b>김렛츠</b>님이 작성하신 챌린지 시작 전 목표
+                🤔 <b>김렛츠</b>님이 작성하신 서류 고민
               </span>
               <br />
-              {/* TODO: 사용자가 설정한 목표가 들어가야 함 */}
+              {/* TODO: 사용자가 설정한 고민이 들어가야 함 */}
               <p className="text-xsmall16 font-bold">
-                “이번에는 꼭 서류 합격률 50%가 넘는 이력서를 만들어보자!”
+                내 서류는 완벽한데 도대체 왜 떨어지는지 모르겠어요!!
               </p>
             </GoalOrConcernsBox>
-            <ReviewTextarea placeholder="챌린지 참여 전의 목표를 어느 정도 달성하셨는지, 그 과정에서 챌린지가 어떤 도움을 주었는지 작성해주세요." />
+            <ReviewTextarea
+              placeholder={`${reportTitle} 이용 전의 고민을 어느 정도 해결하셨는지, 그 과정에서 ${reportTitle}가 어떤 도움을 주었는지 작성해주세요.`}
+            />
           </section>
 
           {/* 만족했던 점 */}
           <section>
             <ReviewQuestion required className="mb-5">
-              4. {josa(programTitle ?? '', '을/를')} 참여하면서 가장 만족했던
+              4. {josa(reportTitle ?? '', '을/를')} 참여하면서 가장 만족했던
               점을 남겨주세요!
             </ReviewQuestion>
             <ReviewTextarea placeholder="가장 도움이 되었던 미션이나, 학습 콘텐츠와 같이 참여하면서 가장 만족했던 점을 자유롭게 작성해주세요." />
@@ -127,7 +128,7 @@ const ChallengeReviewCreatePage = () => {
           {/* 아쉬웠던 점 */}
           <section>
             <ReviewQuestion required className="mb-5">
-              5. {josa(programTitle ?? '', '을/를')} 참여하면서 가장 아쉬웠던
+              5. {josa(reportTitle ?? '', '을/를')} 참여하면서 가장 아쉬웠던
               점을 남겨주세요!
             </ReviewQuestion>
             <ReviewTextarea placeholder="참여하면서 아쉬웠던 점이나 추가되었으면 좋겠는 내용이 있다면 자유롭게 작성해주세요." />
@@ -145,4 +146,4 @@ const ChallengeReviewCreatePage = () => {
   );
 };
 
-export default ChallengeReviewCreatePage;
+export default ReportReviewCreatePage;
