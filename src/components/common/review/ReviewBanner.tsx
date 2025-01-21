ec47ff1b4de11e983838d8c6b18fd1c7b2b0b3fd
Yongyeon Kim <seyeon.dev@gmail.com>
2025-01-21 21:15:59 +0900

    [Chore] 배너 문구 수정

--- src/components/common/review/ReviewBanner.tsx
+++ src/components/common/review/ReviewBanner.tsx
@@ -10,6 +10,24 @@ const heading = {
   interview: '프로그램 참여자 인터뷰',
 };
 
+const description = {
+  blog: (
+    <>
+      렛츠커리어 프로그램 참여자들이 직접 작성한 생생한 블로그 후기를 한곳에
+      모았습니다.
+      <br />
+      실제 참여자들의 솔직한 이야기를 지금 바로 확인해보세요!
+    </>
+  ),
+  program: (
+    <>
+      렛츠커리어만의 취업 노하우가 잔뜩 담긴 프로그램에 참여한
+      <br className="hidden md:block" />
+      참여자들의 100% 솔직 후기를 가감 없이 그대로 보여드립니다.
+    </>
+  ),
+};
+
 type HeadingKey = keyof typeof heading;
 
 function ReviewBanner() {
@@ -19,15 +37,14 @@ function ReviewBanner() {
     <div className="relative flex bg-secondary px-5 md:justify-center py-10 h-[11.125rem] md:h-[10.75rem]">
       {/* 본문 */}
       <div className="flex flex-col z-10 gap-1 w-full md:gap-2 max-w-[1100px]">
+        {/* pathname에 따라 제목 불러오기 */}
         <h1 className="text-white md:text-medium24 text-small20 font-bold">
           {pathname === '/review'
             ? heading.all
             : heading[pathname.slice(8) as HeadingKey]}
         </h1>
         <p className="text-neutral-90 md:text-xsmall16 text-xsmall14 text-justify md:text-left">
-          렛츠커리어만의 취업 노하우가 잔뜩 담긴 프로그램에 참여한{' '}
-          <br className="hidden md:block" />
-          참여자들의 100% 솔직 후기를 가감 없이 그대로 보여드립니다.
+          {pathname === '/review/blog' ? description.blog : description.program}
         </p>
       </div>
 
