0163e5fb10a2f2b1dbb2786927644eb9ff8ea525
Yongyeon Kim <seyeon.dev@gmail.com>
2025-01-21 18:13:19 +0900

    [Feat] 블로그 리뷰 카드 링크 연결 및 이미지 스타일 수정

--- src/components/common/review/BlogReviewCard.tsx
+++ src/components/common/review/BlogReviewCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { BlogReview } from '@/api/review';
 import { YYYY_MM_DD } from '@/data/dayjsFormat';
 import dayjs from '@/lib/dayjs';
+import Link from 'next/link';
 
 interface Props {
   blogReview: BlogReview;
@@ -12,7 +13,10 @@ interface Props {
 
 const BlogReviewCard = ({ blogReview }: Props) => {
   return (
-    <div className="p-4 border rounded-sm gap-4 md:gap-0 flex md:justify-between flex-col md:flex-row border-neutral-80">
+    <Link
+      href={blogReview.url ?? ''}
+      className="p-4 border rounded-sm gap-4 md:gap-0 flex md:justify-between flex-col md:flex-row border-neutral-80"
+    >
       <div>
         <span className="text-xsmall14 font-bold text-primary mb-2">
           {blogReview.programTitle}
@@ -31,15 +35,16 @@ const BlogReviewCard = ({ blogReview }: Props) => {
         </span>
       </div>
 
-      <div className="w-40 h-[5.625rem] md:w-60 md:h-[8.5rem] bg-neutral-85 rounded-sm">
+      <div className="w-40 h-[5.625rem] relative overflow-hidden md:w-60 md:h-[8.5rem] bg-neutral-85 rounded-sm">
         <Image
-          src={''}
+          // 이미지 주소 교체해야 함
+          src="/images/community2.png"
           alt={blogReview.title + ' 블로그 썸네일'}
           fill
           objectFit="cover"
         />
       </div>
-    </div>
+    </Link>
   );
 };
 
