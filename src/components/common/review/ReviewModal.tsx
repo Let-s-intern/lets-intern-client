12a5fd6d9b13d3018d78acbc1f6015dc6ff94688
Ginieee <kaj1226@naver.com>
2025-01-24 22:45:00 +0900

    [Fix] LastMissionSubmitModal 생성

--- src/components/common/review/ReviewModal.tsx
+++ src/components/common/review/ReviewModal.tsx
@@ -11,16 +11,28 @@ interface Props {
   children: React.ReactNode;
   disabled?: boolean;
   onSubmit?: () => void; // 리뷰 등록하기
+  title?: string;
+  isLastMission?: boolean;
+  onClose?: () => void;
 }
 
 /** 마이페이지 후기 작성(생성) 모달
  * [주의] 모바일에서는 모달처럼 안 보임 */
-function ReviewModal({ children, disabled, onSubmit }: Props) {
+function ReviewModal({
+  children,
+  disabled,
+  onSubmit,
+  title,
+  isLastMission,
+  onClose,
+}: Props) {
   const navigate = useNavigate();
 
   const isDesktop = useMediaQuery('(min-width:768px)');
   const { scrollRef, hasScroll } = useHasScroll();
   useControlScroll(isDesktop); // 데스크탑(모달)에서는 body 스크롤 제어
+
+  const buttonText = isLastMission ? '제출하기' : '등록하기';
 
   return (
     // 바탕
@@ -29,22 +41,25 @@ function ReviewModal({ children, disabled, onSubmit }: Props) {
         <div
           ref={scrollRef}
           className={twMerge(
-            'w-full px-5 pb-8 md:h-full md:w-[45rem] md:overflow-y-auto md:pb-32 md:pl-12 md:pt-6',
+            'w-full px-5 pb-8 md:h-full md:w-[45rem] md:overflow-y-auto md:pl-12 md:pt-6',
             // 스크롤 너비를 padding에서 제외 (치우침 방지)
             hasScroll ? 'pr-8' : 'pr-12',
+            isLastMission ? 'md:pb-40' : 'md:pb-32',
           )}
         >
           <div className="flex items-center justify-between">
             <BackHeader to="/mypage/review" hideBack={isDesktop}>
-              후기 작성하기
+              {title ?? '후기 작성하기'}
             </BackHeader>
             {/* 데스크탑 전용 닫기 버튼 */}
-            <img
-              src="/icons/menu_close_md.svg"
-              alt="close"
-              className="hidden h-6 w-6 cursor-pointer md:block"
-              onClick={() => navigate('/mypage/review')}
-            />
+            {!isLastMission && (
+              <img
+                src="/icons/menu_close_md.svg"
+                alt="close"
+                className="hidden h-6 w-6 cursor-pointer md:block"
+                onClick={() => navigate('/mypage/review')}
+              />
+            )}
           </div>
 
           <div className="flex flex-col gap-16 md:gap-8">
@@ -56,15 +71,27 @@ function ReviewModal({ children, disabled, onSubmit }: Props) {
               onClick={onSubmit}
               disabled={disabled}
             >
-              등록하기
+              {buttonText}
             </BaseButton>
           </div>
         </div>
         {/* 데스크탑 버튼 (아래 고정) */}
-        <div className="sticky inset-x-0 bottom-0 hidden bg-white px-14 pb-8 pt-6 drop-shadow-[0_-3px_6px_0_rgba(0,0,0,0.04)] md:block">
-          <BaseButton className="w-full" onClick={onSubmit} disabled={disabled}>
-            등록하기
+        <div className="sticky inset-x-0 bottom-0 hidden gap-y-4 bg-white px-14 pb-8 pt-6 drop-shadow-[0_-3px_6px_0_rgba(0,0,0,0.04)] md:flex flex-col items-center">
+          <BaseButton
+            className="w-full text-small18 font-medium"
+            onClick={onSubmit}
+            disabled={disabled}
+          >
+            {buttonText}
           </BaseButton>
+          {isLastMission && (
+            <span
+              className="text-small18 font-medium text-neutral-50 cursor-pointer hover:underline"
+              onClick={() => onClose && onClose()}
+            >
+              다음에 할게요
+            </span>
+          )}
         </div>
       </main>
     </div>
