c189bb2bc6ddad99e0a7e6750e915a324f092f4d
Ginieee <kaj1226@naver.com>
2025-01-24 15:07:09 +0900

    [Fix] ReviewTextarea props 수정

--- src/components/common/review/ReviewTextarea.tsx
+++ src/components/common/review/ReviewTextarea.tsx
@@ -2,16 +2,8 @@ import { memo } from 'react';
 
 import TextArea, { TextAreaProps } from '../ui/input/TextArea';
 
-interface Props extends TextAreaProps {}
-
-function ReviewTextarea(props: Props) {
-  return (
-    <TextArea
-      wrapperClassName="h-28"
-      maxLength={500}
-      placeholder={props.placeholder}
-    />
-  );
+function ReviewTextarea(props: TextAreaProps) {
+  return <TextArea {...props} wrapperClassName="h-28" maxLength={500} />;
 }
 
 export default memo(ReviewTextarea);
