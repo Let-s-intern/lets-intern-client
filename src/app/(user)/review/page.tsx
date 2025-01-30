5fd844e5cb3b4ec4ed3f81d959d073f791fc79ee
Yongyeon Kim <seyeon.dev@gmail.com>
2025-01-30 16:09:19 +0900

    [Style] 리뷰 메인 페이지 간격, 페딩 수정

--- src/app/(user)/review/page.tsx
+++ src/app/(user)/review/page.tsx
@@ -3,29 +3,14 @@ import MoreHeader from '@components/common/ui/MoreHeader';
 
 const Page = () => {
   return (
-    // TODO: 레이아웃 파일로 만들어보기
-    <div className="flex w-full py-6 px-5 md:py-0 lg:p-0 md:pr-5">
-      <nav className="w-full">
-        <ul>
-          <li>
-            <ProgramReviewSection />
-          </li>
-          <li>
-            <MoreHeader
-              title="블로그 후기"
-              subtitle="NN개"
-              href="/review/blog"
-            />
-          </li>
-          <li>
-            <MoreHeader
-              title="프로그램 참여자 인터뷰"
-              subtitle="NN개"
-              href="/review/interview"
-            />
-          </li>
-        </ul>
-      </nav>
+    <div className="w-full px-5 md:px-0 md:flex md:flex-col md:gap-[4.25rem]">
+      <ProgramReviewSection />
+      <MoreHeader title="블로그 후기" subtitle="NN개" href="/review/blog" />
+      <MoreHeader
+        title="프로그램 참여자 인터뷰"
+        subtitle="NN개"
+        href="/review/interview"
+      />
     </div>
   );
 };
