26c3d548c4a2bed85a3fa0bd10b2ea0975457f4d
Ginieee <kaj1226@naver.com>
2025-02-01 21:37:52 +0900

    [Fix] 리뷰 내브바에 미션 수행 후기 추가

--- src/components/common/review/ReviewNavBar.tsx
+++ src/components/common/review/ReviewNavBar.tsx
@@ -8,13 +8,17 @@ import { ReactNode } from 'react';
 import HorizontalRule from '@components/ui/HorizontalRule';
 
 function ReviewNavBar() {
+  const pathname = usePathname();
   return (
     <>
       {/* 모바일 네비 바 */}
       <div>
         <nav className="px-5 md:hidden">
           <ul className="flex items-stretch">
-            <MobileNavItem href="/review/program">
+            <MobileNavItem
+              href="/review/program"
+              subHref={['/review/mission', '/review/program']}
+            >
               프로그램 참여
               <br />
               후기
@@ -26,6 +30,17 @@ function ReviewNavBar() {
               참여자 인터뷰
             </MobileNavItem>
           </ul>
+          {(pathname === '/review/mission' ||
+            pathname === '/review/program') && (
+            <ul className="flex items-stretch gap-1 my-3">
+              <MobileNavSubItem href="/review/mission">
+                미션 수행 후기
+              </MobileNavSubItem>
+              <MobileNavSubItem href="/review/program">
+                프로그램 참여 후기
+              </MobileNavSubItem>
+            </ul>
+          )}
         </nav>
         <HorizontalRule className="h-3 w-full" />
       </div>
@@ -33,9 +48,18 @@ function ReviewNavBar() {
       {/* 데스크탑 네비 바 */}
       <nav className="md:block hidden mr-8 relative">
         <ul className="flex flex-col sticky top-20 w-[11.5rem] items-stretch">
-          <DesktopNavItem href="/review/program">
+          <DesktopNavItem
+            href="/review/mission"
+            subHref={['/review/mission', '/review/program']}
+          >
             프로그램 참여 후기
           </DesktopNavItem>
+          <DesktopNavSubItem href="/review/mission">
+            미션 수행 후기
+          </DesktopNavSubItem>
+          <DesktopNavSubItem href="/review/program">
+            프로그램 참여 후기
+          </DesktopNavSubItem>
           <DesktopNavItem href="/review/blog">블로그 후기</DesktopNavItem>
           <DesktopNavItem href="/review/interview">
             프로그램 참여자 인터뷰
@@ -51,12 +75,14 @@ export default ReviewNavBar;
 function MobileNavItem({
   children,
   href,
+  subHref,
 }: {
   children: ReactNode;
   href: string;
+  subHref?: string[];
 }) {
   const pathname = usePathname();
-  const active = pathname === href;
+  const active = pathname === href || (subHref && subHref.includes(pathname));
   return (
     <li
       className={clsx(
@@ -76,7 +102,7 @@ function MobileNavItem({
   );
 }
 
-function DesktopNavItem({
+function MobileNavSubItem({
   children,
   href,
 }: {
@@ -86,9 +112,57 @@ function DesktopNavItem({
   const pathname = usePathname();
   const active = pathname === href;
   return (
+    <li
+      className={clsx('px-2.5 h-7 text-center rounded-full', {
+        'bg-primary-20': active,
+      })}
+    >
+      <Link
+        className={`text-xxsmall12 ${active ? 'text-primary font-semibold' : 'text-neutral-35'}`}
+        href={href}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+function DesktopNavItem({
+  children,
+  href,
+  subHref,
+}: {
+  children: ReactNode;
+  href: string;
+  subHref?: string[];
+}) {
+  const pathname = usePathname();
+  const active = pathname === href || (subHref && subHref.includes(pathname));
+  return (
     <li className="py-2 px-3">
       <Link
         className={`text-xsmall16 ${active ? 'text-primary font-semibold' : 'text-neutral-35'}`}
+        href={href}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+function DesktopNavSubItem({
+  children,
+  href,
+}: {
+  children: ReactNode;
+  href: string;
+}) {
+  const pathname = usePathname();
+  const active = pathname === href;
+  return (
+    <li className={`py-2 px-4 rounded-xxs ${active ? 'bg-primary-5' : ''}`}>
+      <Link
+        className={`text-xsmall14 ${active ? 'text-primary font-medium' : 'text-neutral-35'}`}
         href={href}
       >
         {children}
