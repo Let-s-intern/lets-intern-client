a38af5788c9070c3702c2a44d8d1e8053122613a
Taehoon Kim <eszqsc112@gmail.com>
2025-02-01 17:42:10 +0900

    LC-1457 수료증 발급을 위한 react-to-print 동작하도록 버전 업 및 수정

--- src/components/common/challenge/dashboard/section/ScoreSection.tsx
+++ src/components/common/challenge/dashboard/section/ScoreSection.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { useRef, useState } from 'react';
-import ReactToPrint from 'react-to-print';
+import { useReactToPrint } from 'react-to-print';
 import ScoreTooltipQuestion from '../../ui/tooltip-question/ScoreTooltipQuestion';
 import CertificatePaper from '../CertificatePaper';
 
@@ -26,10 +28,18 @@ const ScoreSection = ({
 }: Props) => {
   const [isHoverButton, setIsHoverButton] = useState(false);
   const certificateRef = useRef<HTMLDivElement>(null);
+  const reactToPrintFn = useReactToPrint({
+    contentRef: certificateRef,
+
+    documentTitle: `${programName} 수료증`,
+    onAfterPrint: () => {
+      console.log('수료증 출력 완료');
+    },
+  });
 
   return (
     <section className="relative flex w-full flex-col gap-y-4 rounded-xl border border-[#E4E4E7] p-6">
-      <div className="flex flex-1 flex-col gap-2">
+      <div className="flex flex-col flex-1 gap-2">
         <div className="flex items-center gap-2">
           <h2 className="font-semibold text-[#4A495C]">미션 점수 현황</h2>
           <ScoreTooltipQuestion />
@@ -58,26 +68,18 @@ const ScoreSection = ({
           수료증 발급
         </button>
       ) : (
-        <ReactToPrint
-          trigger={() => (
-            <button
-              className={`flex items-center justify-center rounded-sm border-2 border-primary bg-neutral-100 px-4 py-1.5 text-xsmall14 font-medium text-primary-dark outline-none`}
-            >
-              수료증 발급
-            </button>
-          )}
-          content={() => certificateRef.current}
-          documentTitle={`${programName} 수료증`}
-          onAfterPrint={() => {
-            console.log('수료증 출력 완료');
-          }}
-        />
+        <button
+          onClick={() => reactToPrintFn()}
+          className={`flex items-center justify-center rounded-sm border-2 border-primary bg-neutral-100 px-4 py-1.5 text-xsmall14 font-medium text-primary-dark outline-none`}
+        >
+          수료증 발급
+        </button>
       )}
       {isHoverButton && (
         <div className="absolute bottom-[15px] left-1/2 w-[240px] -translate-x-1/2 translate-y-full transform px-[13.5px] pb-4 pt-[29px] text-xsmall14 text-[#333]">
           <div className="absolute bottom-0 left-0 z-0 h-[calc(100%-14px)] w-full shadow-[0_0_24px_rgba(204,204,206,0.27)]" />
           <img
-            className="absolute bottom-0 left-0 z-0 h-full w-full object-fill"
+            className="absolute bottom-0 left-0 z-0 object-fill w-full h-full"
             src="/images/textbox.png"
           />
           <p className="relative z-10">
@@ -87,7 +89,7 @@ const ScoreSection = ({
         </div>
       )}
       {/* A4 크기 */}
-      <div className="fixed left-0 top-0 z-50 hidden h-screen w-screen items-center justify-center overflow-hidden bg-white">
+      <div className="fixed top-0 left-0 z-50 items-center justify-center hidden w-screen h-screen overflow-hidden bg-white">
         <CertificatePaper
           ref={certificateRef}
           programName={programName}
