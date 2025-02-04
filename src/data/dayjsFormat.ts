639a1de638b83aad743fdfd3d197efee1ef2e2f8
Yongyeon Kim <seyeon.dev@gmail.com>
2025-02-04 11:17:28 +0900

    [Fix] dayjs 포맷 수정

--- src/data/dayjsFormat.ts
+++ src/data/dayjsFormat.ts
@@ -2,12 +2,12 @@ const LOCALIZED_YYYY_MDdd_HHmm = 'YYYY년 M월 D일(dd) HH시 mm분';
 const LOCALIZED_YYYY_MDdd_HH = 'YYYY년 M월 D일(dd) HH시';
 const LOCALIZED_YYYY_MD_Hm = 'YYYY년 M월 D일 H시 m분';
 const YYYY_MM_DD = 'YYYY. MM. DD';
-const YYYY_MMDD_THHmmss = '2025-01-26T06:30:56';
+const YYYY_MMDD_THHmmss = 'YYYY-MM-DDTHH:mm:ss';
 
 export {
   LOCALIZED_YYYY_MD_Hm,
   LOCALIZED_YYYY_MDdd_HH,
   LOCALIZED_YYYY_MDdd_HHmm,
-  YYYY_MMDD_THHmmss,
   YYYY_MM_DD,
+  YYYY_MMDD_THHmmss,
 };
