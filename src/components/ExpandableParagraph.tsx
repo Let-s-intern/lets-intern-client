66b654b6e638ea25dec9ee54ca32ab04a28f8ef1
Taehoon Kim <eszqsc112@gmail.com>
2025-02-06 00:56:20 +0900

    LC-1536 더보기/접기 텍스트 구현 방식 완전히 수정 (사파리 이슈로 line-clamp + float 요소 잘 안됨)

--- src/components/ExpandableParagraph.tsx
+++ src/components/ExpandableParagraph.tsx
@@ -16,18 +16,15 @@ const ExpandableParagraph = ({
 }) => {
   const pRef = useRef<HTMLParagraphElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
-  const [clientHeight, setClientHeight] = useState<number | null>(null);
-  const [scrollHeight, setScrollHeight] = useState<number | null>(null);
   const [lineHeight, setLineHeight] = useState<number | null>(null);
   const [expanding, setExpanding] = useState(false);
+  const originalRef = useRef<HTMLParagraphElement>(null);
 
   useEffect(() => {
     const observer = new ResizeObserver(() => {
       if (!pRef.current) {
         return;
       }
-      setClientHeight(pRef.current.clientHeight);
-      setScrollHeight(pRef.current.scrollHeight);
       setLineHeight(parseFloat(getComputedStyle(pRef.current).lineHeight));
     });
     if (pRef.current) {
@@ -39,69 +36,57 @@ const ExpandableParagraph = ({
     };
   }, []);
 
-  const isOverflown =
-    scrollHeight && clientHeight && scrollHeight > clientHeight;
+  const maxHeight = lineHeight ? lineHeight * lineClamp : undefined;
 
-  const buttonMarginTop = (lineHeight ?? 0) * (lineClamp - 1);
+  const isOverflown =
+    originalRef.current?.clientHeight &&
+    maxHeight &&
+    originalRef.current?.clientHeight > maxHeight;
 
   return (
-    <div
-      className={twMerge(
-        'relative',
-        expanding
-          ? ''
-          : lineClamp === 1
-            ? 'line-clamp-1'
-            : lineClamp === 2
-              ? 'line-clamp-2'
-              : lineClamp === 3
-                ? 'line-clamp-3'
-                : lineClamp === 4
-                  ? 'line-clamp-4'
-                  : lineClamp === 5
-                    ? 'line-clamp-5'
-                    : null,
+    <>
+      <div className={twMerge(className, 'h-0 overflow-hidden')}>
+        <p ref={originalRef}>{content}</p>
+      </div>
 
-        className,
-      )}
-    >
-      <button
-        className={twMerge(
-          'float-right text-primary [shape-outside:border-box] hover:underline',
-          expanding && 'hidden',
-          !isOverflown && 'hidden',
-          buttonClassName,
-        )}
-        onClick={(e) => {
-          e.stopPropagation();
-          setExpanding((prev) => !prev);
-        }}
-        style={{
-          marginTop: `${buttonMarginTop}px`,
-        }}
-        ref={buttonRef}
-      >
-        더보기
-      </button>
-
-      <p ref={pRef}>
-        {content}
-        {expanding ? (
-          <button
-            className={twMerge(
-              'ml-0.5 text-primary hover:underline',
-              buttonClassName,
-            )}
-            onClick={(e) => {
-              e.stopPropagation();
-              setExpanding((prev) => !prev);
-            }}
-          >
-            접기
-          </button>
-        ) : null}
-      </p>
-    </div>
+      <div className={twMerge('relative w-full', className)}>
+        <p
+          ref={pRef}
+          className="relative overflow-hidden"
+          style={{ maxHeight: expanding ? undefined : maxHeight }}
+        >
+          {content}
+          {!isOverflown ? null : !expanding ? (
+            <button
+              className={twMerge(
+                'z-1 absolute bottom-0 right-0 block bg-gradient-to-r from-transparent via-white via-40% to-white pl-8 text-primary hover:underline',
+                buttonClassName,
+              )}
+              onClick={(e) => {
+                e.stopPropagation();
+                setExpanding((prev) => !prev);
+              }}
+              ref={buttonRef}
+            >
+              더보기
+            </button>
+          ) : (
+            <button
+              className={twMerge(
+                'ml-0.5 text-primary hover:underline',
+                buttonClassName,
+              )}
+              onClick={(e) => {
+                e.stopPropagation();
+                setExpanding((prev) => !prev);
+              }}
+            >
+              접기
+            </button>
+          )}
+        </p>
+      </div>
+    </>
   );
 };
 
