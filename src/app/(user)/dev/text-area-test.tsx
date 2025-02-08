da1bcf6080476c96563eab5b722e6fdf6df7ca28
Taehoon Kim <eszqsc112@gmail.com>
2025-02-08 12:58:24 +0900

    LC-1558 챌린지 대시보드 추가정보 입력 border-radius 적용

---
+++ src/app/(user)/dev/text-area-test.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import TextArea from '@components/common/ui/input/TextArea';
+import { useState } from 'react';
+
+export default function TextAreaTest() {
+  const [value, setValue] = useState('');
+
+  return (
+    <TextArea
+      id="goal"
+      name="goal"
+      wrapperClassName="h-28"
+      placeholder={`챌린지를 신청한 목적과 계기,\n또는 챌린지 참여를 통해 이루고 싶은 목표를 자유롭게 작성해주세요.`}
+      className="text-xsmall14 font-normal"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      maxLength={200}
+    />
+  );
+}
