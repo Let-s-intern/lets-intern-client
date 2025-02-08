2df765bab1c3d5e227decc09fd91700af6c28bcc
Ginieee <kaj1226@naver.com>
2025-02-09 00:48:26 +0900

    [Fix] 리뷰필터 폰트 크기 변경

--- src/components/common/review/ReviewFilter.tsx
+++ src/components/common/review/ReviewFilter.tsx
@@ -168,7 +168,7 @@ function ReviewFilter({
 
       <div className="relative flex flex-col">
         <div
-          className={`flex min-w-[8.5rem] max-w-fit cursor-pointer items-center gap-2 rounded-xxs border bg-[#FBFBFC] px-3 py-2 text-xxsmall12 md:text-xsmall16 ${isOpen ? 'border-primary' : 'border-neutral-90'}`}
+          className={`flex min-w-[8.5rem] max-w-fit cursor-pointer items-center gap-2 rounded-xxs border bg-[#FBFBFC] px-3 py-2 text-xsmall14 md:text-xsmall16 ${isOpen ? 'border-primary' : 'border-neutral-90'}`}
           onClick={() => setIsOpen((prev) => !prev)}
         >
           <span className="font-medium text-neutral-20">{label}</span>
