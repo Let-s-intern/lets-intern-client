8c2f9d15975c63c89a99bcebb2e9ddbc2029c32a
Yongyeon Kim <seyeon.dev@gmail.com>
2025-02-12 22:06:36 +0900

    [Style] 필터와 리스트 간격 수정 및 불필요한 코드 삭제

--- src/app/(user)/review/blog/page.tsx
+++ src/app/(user)/review/blog/page.tsx
@@ -1,16 +1,8 @@
 import BlogReviewWrapper from '@components/common/review/BlogReviewWrapper';
 
 export default async function Page() {
-  // // [테스트] 파비콘 가져오기
-  // const data = await client<BlogReviewList>('/v2/review/blog', {
-  //   method: 'GET',
-  // });
-  // const text = await fetch(data.reviewList[1].url ?? '').then((res) =>
-  //   res.text(),
-  // );
-
   return (
-    <div className="px-5 md:px-0 w-full">
+    <div className="w-full px-5 md:px-0">
       <BlogReviewWrapper />
     </div>
   );
