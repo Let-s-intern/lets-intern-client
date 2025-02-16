27b042b6cb936b6497854c50cfc29c1e58eca254
Yongyeon Kim <seyeon.dev@gmail.com>
2025-02-16 18:18:41 +0900

    [Feat] 블로그 카드 컴포넌트 수정

--- src/components/common/blog/BlogCard.tsx
+++ src/components/common/blog/BlogCard.tsx
@@ -1,59 +1,33 @@
-import dayjs from '@/lib/dayjs';
-import { useRouter } from 'next/navigation';
-import { BlogInfoSchema } from '../../../api/blogSchema';
-import { blogCategory } from '../../../utils/convert';
-import { getBlogPathname } from '../../../utils/url';
-import BlogHashtag from './BlogHashtag';
+import { ReactNode } from 'react';
 
-interface BlogCardProps {
-  blogInfo: BlogInfoSchema;
+interface Props {
+  title: string;
+  date?: string;
+  superTitle: string;
+  buttonItem?: ReactNode;
+  thumbnailItem: ReactNode;
 }
 
-const BlogCard = ({ blogInfo }: BlogCardProps) => {
-  const router = useRouter();
-
+const BlogCard = ({
+  title,
+  date,
+  superTitle,
+  buttonItem,
+  thumbnailItem,
+}: Props) => {
   return (
-    <div
-      className="flex flex-col w-full py-3 cursor-pointer gap-y-2"
-      onClick={() => {
-        router.push(getBlogPathname(blogInfo.blogThumbnailInfo));
-      }}
-    >
-      <span className="w-full font-bold text-xsmall16 text-primary">
-        {blogCategory[blogInfo.blogThumbnailInfo.category || '']}
-      </span>
-      <div className="flex flex-col w-full gap-y-4">
-        <div className="flex w-full gap-x-5">
-          <div className="flex flex-col flex-1 gap-y-2">
-            <h2 className="font-bold line-clamp-3 text-neutral-0">
-              {blogInfo.blogThumbnailInfo.title}{' '}
-              {!blogInfo.blogThumbnailInfo.isDisplayed && (
-                <span className="text-xsmall14 text-system-error">
-                  (비공개)
-                </span>
-              )}
-            </h2>
-            <p className="line-clamp-6 text-xsmall16 text-neutral-20">
-              {blogInfo.blogThumbnailInfo.description}
-            </p>
-            <p className="w-full text-xsmall14 text-neutral-45">
-              {blogInfo.blogThumbnailInfo.displayDate
-                ? dayjs(blogInfo.blogThumbnailInfo.displayDate).format(
-                    'YYYY년 MM월 DD일',
-                  )
-                : null}
-            </p>
-          </div>
-          <img
-            className="h-[68px] w-[100px] overflow-hidden rounded-md object-cover md:h-[90px] md:w-[130px]"
-            src={blogInfo.blogThumbnailInfo.thumbnail || ''}
-            alt="thumbnail"
-          />
-        </div>
-        <div className="flex flex-wrap items-center gap-1.5">
-          {blogInfo.tagDetailInfos.map((tag) => (
-            <BlogHashtag key={tag.id} text={tag.title || ''} tagId={tag.id} />
-          ))}
+    <div className="flex flex-col gap-3">
+      <div className="relative aspect-[4/3] w-full overflow-hidden rounded-sm bg-neutral-70">
+        {thumbnailItem}
+      </div>
+      <div className="flex flex-col gap-2">
+        <span className="text-xsmall14 font-semibold text-primary">
+          {superTitle}
+        </span>
+        <h3 className="text-xsmall16 font-bold text-neutral-0">{title}</h3>
+        <div className="flex items-center justify-between py-2">
+          <span className="text-xxsmall12 text-neutral-40">{date}</span>
+          {buttonItem}
         </div>
       </div>
     </div>
