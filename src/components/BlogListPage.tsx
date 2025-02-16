3d57fd7c23eaf00d2342f1e2d5e60c31f2adaabb
Yongyeon Kim <seyeon.dev@gmail.com>
2025-02-16 13:08:05 +0900

    [Style] 블로그 상단 배너 스타일 수정

--- src/components/BlogListPage.tsx
+++ src/components/BlogListPage.tsx
@@ -12,7 +12,6 @@ const BlogListPageContent = () => {
   const params = useSearchParams();
   const [category, setCategory] = useState<string | null>(
     params?.get('category') ?? null,
-    // params.get('category'),
   );
   const { data, fetchNextPage, hasNextPage, isLoading } =
     useInfiniteBlogListQuery({
@@ -33,19 +32,25 @@ const BlogListPageContent = () => {
 
   return (
     <div className="flex w-full flex-1 flex-col items-center">
-      <div className="flex w-full items-center justify-center gap-y-1 bg-blog-banner-sm py-10 md:bg-blog-banner-md lg:bg-blog-banner-lg">
-        <div className="flex w-full max-w-[1200px] flex-col gap-y-1 px-[25px] md:gap-y-2 md:px-[140px]">
-          <h1 className="text-xl font-bold text-neutral-100">
-            {category !== null ? blogCategory[category] : '렛츠커리어 블로그'}
+      {/* 블로그 배너 */}
+      <div className="w-full bg-blog-banner-sm bg-cover bg-no-repeat px-5 py-8 md:bg-blog-banner-md md:px-0 md:py-10 lg:bg-blog-banner-lg">
+        <div className="mx-auto flex w-full max-w-[1100px] flex-col gap-1 md:gap-2">
+          <h1 className="text-small20 font-bold text-neutral-100 md:text-medium24">
+            {category ? blogCategory[category] : '렛츠커리어 블로그'}
           </h1>
-          <p className="text-xsmall14 text-white/85">
+          <p className="text-xsmall14 text-white/90 md:text-xsmall16">
+            취업 준비부터 실무까지, 성공적인 커리어의 시작을 돕습니다.{' '}
+            <br className="hidden md:block" />
             렛츠커리어의 독자적인 커리어 교육 콘텐츠를 확인해보세요.
           </p>
         </div>
       </div>
+
+      {/* 블로그 콘텐츠 */}
       <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center px-5 md:px-20 lg:px-10">
         <div className="flex w-full flex-1 flex-col items-center md:px-[100px] md:pb-10">
           <div className="flex w-full flex-col items-center gap-y-8 pb-8 pt-5 md:py-8">
+            {/* 카테고리 */}
             <div className="flex w-full flex-wrap gap-2 py-2">
               <BlogCategoryTag
                 category="전체"
@@ -83,6 +88,7 @@ const BlogListPageContent = () => {
                 onClick={() => setCategory('JUNIOR_STORIES')}
               />
             </div>
+            {/* 리스트 */}
             <InfiniteScroll
               className="w-full flex-1 gap-y-[30px]"
               hasMore={hasNextPage}
