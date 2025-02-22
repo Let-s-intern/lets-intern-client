24d6f0803d8e73369f2a8a1aaad06215e3f1cf95
Ginieee <kaj1226@naver.com>
2025-02-22 22:32:34 +0900

    [Feat] 큐레이션 생성 페이지 UI 구현

--- src/router-pages/admin/home/curation/HomeCurationCreatePage.tsx
+++ src/router-pages/admin/home/curation/HomeCurationCreatePage.tsx
@@ -1,9 +1,32 @@
+import { CurationItemType } from '@/api/curation';
+import CurationInfoSection from '@components/admin/home/curation/section/CurationInfoSection';
+import CurationItemsSection from '@components/admin/home/curation/section/CurationItemsSection';
+import CurationVisibleSection from '@components/admin/home/curation/section/CurationVisibleSection';
+import Header from '@components/admin/ui/header/Header';
+import Heading from '@components/admin/ui/heading/Heading';
+import { useState } from 'react';
+
 const HomeCurationCreatePage = () => {
+  const [curationItems, setCurationItems] = useState<CurationItemType[]>([]);
+
   return (
-    <div>
-      create
-      <div>create</div>
-    </div>
+    <>
+      <div className="mx-6 mb-40 mt-6">
+        <Header>
+          <Heading>홈 큐레이션 생성</Heading>
+        </Header>
+        <div className="flex w-full flex-col gap-y-8">
+          <div className="flex w-full gap-x-5">
+            <CurationInfoSection />
+            <CurationVisibleSection />
+          </div>
+          <CurationItemsSection
+            curationItems={curationItems}
+            setCurationItems={setCurationItems}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
