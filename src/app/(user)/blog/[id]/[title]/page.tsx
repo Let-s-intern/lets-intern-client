fc4e9cc9afb4ca5892f97c21419b8a71b9f4cb1a
Yongyeon Kim <seyeon.dev@gmail.com>
2025-02-23 14:18:06 +0900

    [Feat] 블로그 공유 섹션 추가

--- src/app/(user)/blog/[id]/[title]/page.tsx
+++ src/app/(user)/blog/[id]/[title]/page.tsx
@@ -12,11 +12,15 @@ import {
 import BlogCTA from '@components/common/blog/BlogCTA';
 import BlogHashtag from '@components/common/blog/BlogHashtag';
 import BlogHomeButton from '@components/common/blog/BlogHomeButton';
+import BlogKakaoShareBtn from '@components/common/blog/BlogKakaoShareBtn';
 import BlogLinkShareBtn from '@components/common/blog/BlogLilnkShareBtn';
 import BlogRating from '@components/common/blog/BlogRating';
 import BlogShareSection from '@components/common/blog/BlogShareSection';
+import HorizontalRule from '@components/ui/HorizontalRule';
+import { CircleChevronRight, Heart } from 'lucide-react';
 import { Metadata } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 
 // SSR 메타데이터 생성
 export async function generateMetadata({
@@ -52,11 +56,14 @@ const BlogDetailPage = async ({
   params: Promise<{ id: string }>;
 }) => {
   const { id } = await params;
+
   const blog = await fetchBlogData(id);
   const recommendData = await fetchRecommendBlogData({
     type: blog?.blogDetailInfo.category,
     pageable: { page: 0, size: 4 },
   });
+
+  const blogInfo = blog.blogDetailInfo;
 
   return (
     <main className="mx-auto w-full max-w-[1100px]">
@@ -70,22 +77,23 @@ const BlogDetailPage = async ({
                 className="object-contain"
                 priority
                 fill
-                src={blog.blogDetailInfo.thumbnail ?? ''}
+                src={blogInfo.thumbnail ?? ''}
                 alt="블로그 썸네일"
                 sizes="(max-width: 768px) 100vw, 26rem"
               />
             </div>
+            {/* 블로그 헤더 */}
             <div className="flex flex-col gap-y-4">
               {/* 제목 */}
               <div>
-                {blog.blogDetailInfo.category && (
+                {blogInfo.category && (
                   <h2 className="mb-2 text-small20 font-semibold text-primary">
-                    {blogCategory[blog.blogDetailInfo.category]}
+                    {blogCategory[blogInfo.category]}
                   </h2>
                 )}
                 <h1 className="line-clamp-3 text-xlarge28 font-bold text-neutral-0 md:line-clamp-2">
-                  {blog.blogDetailInfo.title}{' '}
-                  {!blog.blogDetailInfo.isDisplayed && (
+                  {blogInfo.title}{' '}
+                  {!blogInfo.isDisplayed && (
                     <span className="text-xsmall14 text-system-error">
                       (비공개)
                     </span>
@@ -100,7 +108,7 @@ const BlogDetailPage = async ({
                       <Image
                         width={20}
                         height={20}
-                        src="/logo/logo-simple.svg"
+                        src="/logo/logo-gradient.svg"
                         alt="쥬디 프로필 사진"
                       />
                     </div>
@@ -108,12 +116,9 @@ const BlogDetailPage = async ({
                       쥬디
                     </span>
                   </div>
-                  {blog.blogDetailInfo.displayDate && (
+                  {blogInfo.displayDate && (
                     <p className="text-xsmall14 text-neutral-35">
-                      {dayjs(blog.blogDetailInfo.displayDate).format(
-                        YYYY_MM_DD,
-                      )}{' '}
-                      작성
+                      {dayjs(blogInfo.displayDate).format(YYYY_MM_DD)} 작성
                     </p>
                   )}
                 </div>
@@ -121,51 +126,68 @@ const BlogDetailPage = async ({
                 <BlogLinkShareBtn />
               </div>
             </div>
+            {/* 블로그 본문 */}
+            {blogInfo?.content && (
+              <div className="w-full break-all text-xsmall16">
+                <LexicalContent node={JSON.parse(blogInfo.content).root} />
+              </div>
+            )}
           </article>
+
+          <section className="mb-9 mt-10 flex items-center justify-between">
+            {/* 좋아요 */}
+            <button type="button" className="flex items-center gap-2">
+              <Heart width={20} height={20} color="#4D55F5" />
+              <span className="text-xsmall14 font-medium text-primary">
+                좋아요 NNN
+              </span>
+            </button>
+            {/* 공유하기 */}
+            <div className="flex items-center">
+              <BlogLinkShareBtn
+                className="border-none p-2"
+                hideCaption
+                iconWidth={20}
+                iconHeight={20}
+              />
+              <BlogKakaoShareBtn
+                className="p-2"
+                title={blogInfo.title ?? ''}
+                description={blogInfo.description ?? ''}
+                thumbnail={blogInfo.thumbnail ?? ''}
+                pathname={getBlogPathname(blogInfo)}
+              />
+              <span className="text-xsmall14 font-medium text-neutral-35">
+                공유하기
+              </span>
+            </div>
+          </section>
+
+          <HorizontalRule className="-mx-5 h-3 md:hidden" />
+          <Link
+            href="/blog/list"
+            className="flex w-full items-center justify-center gap-2 py-5"
+          >
+            <p className="text-xsmall14 font-semibold text-neutral-0">
+              <span className="text-primary">블로그 홈</span> 바로가기
+            </p>
+            <CircleChevronRight width={16} height={16} color="#5F66F6" />
+          </Link>
+          <HorizontalRule className="-mx-5 h-3 md:hidden" />
         </section>
-        <aside></aside>
+        <aside className="w-full px-5 py-9">
+          <h2 className="text-small20 font-semibold text-neutral-0">
+            렛츠커리어 프로그램 참여하고
+            <br />
+            취뽀 성공해요!
+          </h2>
+        </aside>
       </div>
       {/* 프로그램 추천 */}
       <section></section>
 
       <div className="flex w-full max-w-[1100px] flex-col items-center px-5 md:px-10 md:pt-10">
         <div className="flex w-full flex-col items-center gap-y-8 pt-8 md:px-[100px] md:pt-8">
-          <div className="flex w-full flex-col gap-y-3 py-3">
-            <h2 className="w-full text-xl font-bold text-primary">
-              {blogCategory[blog.blogDetailInfo.category || '']}
-            </h2>
-            <div className="flex w-full flex-col gap-y-5">
-              <div className="flex w-full flex-col gap-y-4">
-                <h1 className="line-clamp-4 text-xlarge28 font-bold text-neutral-0">
-                  {blog.blogDetailInfo.title}{' '}
-                  {!blog.blogDetailInfo.isDisplayed && (
-                    <span className="text-xsmall14 text-system-error">
-                      (비공개)
-                    </span>
-                  )}
-                </h1>
-                {blog.blogDetailInfo.displayDate ? (
-                  <p>
-                    {dayjs(blog.blogDetailInfo.displayDate).format(
-                      'YYYY년 MM월 DD일',
-                    )}
-                  </p>
-                ) : null}
-              </div>
-              <img
-                className="h-[218px] w-full rounded-md object-cover md:h-[484px] lg:h-[627px]"
-                src={blog.blogDetailInfo.thumbnail || ''}
-                alt="thumbnail"
-              />
-            </div>
-          </div>
-          <div className="w-full break-all text-xsmall16">
-            {blog.blogDetailInfo?.content ? (
-              <LexicalContent
-                node={JSON.parse(blog.blogDetailInfo?.content || '{}')?.root}
-              />
-            ) : null}
-          </div>
           <div className="flex w-full items-center justify-center gap-x-3 py-10">
             <div className="flex items-center justify-center rounded-full border border-primary-20 p-[9px]">
               <img
--- src/components/common/blog/BlogLilnkShareBtn.tsx
+++ src/components/common/blog/BlogLilnkShareBtn.tsx
@@ -1,11 +1,30 @@
 'use client';
 
+import { twMerge } from '@/lib/twMerge';
 import { Link } from 'lucide-react';
 
-function BlogLinkShareBtn() {
+interface Props {
+  className?: string;
+  hideCaption?: boolean;
+  iconWidth?: number | string;
+  iconHeight?: number | string;
+  iconColor?: string;
+}
+
+function BlogLinkShareBtn({
+  className,
+  hideCaption = false,
+  iconWidth,
+  iconHeight,
+  iconColor,
+}: Props) {
   return (
     <button
-      className="flex items-center gap-1.5 rounded-full border border-neutral-80 px-3 py-2"
+      type="button"
+      className={twMerge(
+        'flex items-center gap-1.5 rounded-full border border-neutral-80 px-3 py-2',
+        className,
+      )}
       onClick={async () => {
         try {
           await navigator.clipboard.writeText(
@@ -17,8 +36,16 @@ function BlogLinkShareBtn() {
         }
       }}
     >
-      <Link width={16} height={16} color="#5C5F66" />
-      <span className="text-xsmall14 font-medium">공유하기</span>
+      <Link
+        width={iconWidth ?? 16}
+        height={iconHeight ?? 16}
+        color={iconColor ?? '#5C5F66'}
+      />
+      {!hideCaption && (
+        <span className="text-xsmall14 font-medium text-neutral-35">
+          공유하기
+        </span>
+      )}
     </button>
   );
 }
