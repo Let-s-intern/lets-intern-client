c1377cbfc47447daec70bb28621d6867fba1d687
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-03 10:08:16 +0900

    [Style] 모바일 사이드 네비 바 너비 조정

--- src/components/common/ui/layout/next-nav/NavBar.tsx
+++ src/components/common/ui/layout/next-nav/NavBar.tsx
@@ -297,7 +297,7 @@ const NavBar = () => {
       {/* 사이드 네비게이션 바 */}
       <div
         className={twMerge(
-          'fixed right-0 top-0 z-50 flex h-screen w-full flex-col bg-white shadow-md transition-all duration-300 sm:w-[22rem]',
+          'fixed right-0 top-0 z-50 flex h-screen w-[17.5rem] flex-col bg-white shadow-md transition-all duration-300 sm:w-[22rem]',
           isOpen ? 'translate-x-0' : 'translate-x-full',
         )}
       >
