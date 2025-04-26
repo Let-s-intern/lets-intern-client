92ea3cd2e011c2bbde1b6ad35daa0d5d17c3f84f
Yongyeon Kim <seyeon.dev@gmail.com>
2025-04-26 12:02:19 +0900

    [Feat] 할인 금액 없으면 정가 숨기기

--- src/router-pages/common/mypage/CreditDelete.tsx
+++ src/router-pages/common/mypage/CreditDelete.tsx
@@ -128,11 +128,12 @@ const CreditDelete = () => {
                           <button
                             className="flex items-center justify-center rounded-sm border border-neutral-60 bg-white px-2.5 py-1.5 text-sm font-medium"
                             onClick={() => {
-                              paymentDetail.tossInfo?.receipt &&
+                              if (paymentDetail.tossInfo?.receipt) {
                                 window.open(
                                   paymentDetail.tossInfo.receipt.url || '',
                                   '_blank',
                                 );
+                              }
                             }}
                           >
                             영수증 보기
