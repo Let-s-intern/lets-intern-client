25893e79565cf6c99910108d2760b5c9db9aad3e
hyonun321 <hyo_nun@kakao.com>
2025-06-01 18:12:25 +0900

    [Style] ReportNavigation 스크롤 애니메이션 높이 조정

--- src/components/common/ui/layout/header/Spacer.tsx
+++ src/components/common/ui/layout/header/Spacer.tsx
@@ -1,5 +1,5 @@
 function Spacer() {
-  return <div className="h-[56px] md:h-[117px]" aria-hidden="true" />;
+  return <div className="h-[44px] md:h-[117px]" aria-hidden="true" />;
 }
 
 export default Spacer;
--- src/components/page/ReportNavigation.tsx
+++ src/components/page/ReportNavigation.tsx
@@ -92,7 +92,7 @@ const ReportNavigation = ({
   return (
     <nav
       className={twMerge(
-        'report_navigation sticky top-[3.65rem] z-20 flex w-full transform justify-center gap-x-1 px-6 transition-all duration-300 md:top-[4.275rem] md:gap-x-[100px] lg:top-[4.65rem]',
+        'report_navigation sticky top-[2.8rem] z-20 flex w-full transform justify-center gap-x-1 px-6 transition-all duration-300 md:top-[4.275rem] md:gap-x-[100px] lg:top-[4.65rem]',
         isDark ? 'bg-black/90' : 'border-b-2 border-neutral-80 bg-white',
         scrollDirection === 'DOWN' && 'top-0 md:top-0 lg:top-0',
         className,
