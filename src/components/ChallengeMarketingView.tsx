cac3186d238badc9262e72261b01a713ada02da9
Yongyeon Kim <seyeon.dev@gmail.com>
2025-06-01 12:59:21 +0900

    [Feat] 챌린지 마케팅 상페 분기처리

---
+++ src/components/ChallengeMarketingView.tsx
@@ -0,0 +1,11 @@
+import { ChallengeIdPrimitive } from '@/schema';
+
+interface Props {
+  challenge: ChallengeIdPrimitive;
+}
+
+function ChallengeMarketingView({ challenge }: Props) {
+  return <div>ChallengeMarketingView</div>;
+}
+
+export default ChallengeMarketingView;
