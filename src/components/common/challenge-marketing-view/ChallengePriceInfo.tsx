9e6cf0e118dd46ed2cfeada46f5c9e8758712308
yyeonkim <seyeon.dev@gmail.com>
2025-06-04 21:23:28 +0900

    refactor: 최종 결제 금액 영역 컴포넌트 분리

--- src/components/common/challenge-marketing-view/ChallengePriceInfo.tsx
+++ src/components/common/challenge-marketing-view/ChallengePriceInfo.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import useChallengeOptionPriceInfo from '@/hooks/useChallengeOptionPriceInfo';
+import { useInstallmentPayment } from '@/hooks/useInstallmentPayment';
 import { twMerge } from '@/lib/twMerge';
 import { ChallengePriceInfo, ChallengePricePlan } from '@/schema';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useMemo, useState } from 'react';
 
 type Plans = {
   [key in ChallengePricePlan]?: string;
@@ -33,6 +35,56 @@ const PlanButton = ({
   );
 };
 
+const FinalPriceInfo = ({
+  finalPrice,
+  sellingPrice,
+}: {
+  finalPrice: number;
+  sellingPrice: number;
+}) => {
+  const {
+    isLoading,
+    months: installmentMonths,
+    banks,
+  } = useInstallmentPayment();
+
+  const monthlyAmount = installmentMonths
+    ? Math.round(finalPrice / installmentMonths)
+    : null; // 할부 금액
+  const showMonthlyPrice = monthlyAmount && sellingPrice >= 50000; // 5만원 이상일 때 할부 가능
+
+  if (isLoading) return <span className="text-neutral-40">로딩 중..</span>;
+
+  return (
+    <div className="flex flex-col items-stretch gap-0.5 px-2.5">
+      {showMonthlyPrice && (
+        <div className="flex items-center justify-between font-medium text-neutral-20">
+          <span>최종 결제 금액</span>
+          <div>{finalPrice.toLocaleString()}원</div>
+        </div>
+      )}
+      {/* 할부가 안되면 할부 금액에 최종 결제 금액이 대신 표시됩니다 */}
+      <div className="flex items-center justify-between">
+        <span className="font-medium text-neutral-20">
+          {showMonthlyPrice ? '' : '최종 결제 금액'}
+        </span>
+        <div className="flex flex-col items-end gap-0.5">
+          <strong className="text-medium24 font-bold text-[#4A76FF]">
+            {showMonthlyPrice
+              ? `월 ${monthlyAmount.toLocaleString()}원`
+              : `${finalPrice.toLocaleString()}원`}
+          </strong>
+          {showMonthlyPrice && (
+            <span className="text-xxsmall12 text-neutral-30">
+              * {banks.join(', ')}은행 {installmentMonths}개월 무이자 할부 시
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 interface Props {
   content?: ReactNode;
   priceInfoList: ChallengePriceInfo[];
@@ -44,14 +96,60 @@ function ChallengePriceInfoWithContent({ content, priceInfoList }: Props) {
   const basicPriceInfo = priceInfoList.find(
     (item) => item.challengePricePlanType === 'BASIC',
   );
-  const standardPriceInfo = priceInfoList.find(
-    (item) => item.challengePricePlanType === 'STANDARD',
-  );
-  const premiumPriceInfo = priceInfoList.find(
-    (item) => item.challengePricePlanType === 'PREMIUM',
-  );
 
-  const getPlans = () => {
+  const deposit = basicPriceInfo?.refund ?? 0; // 환급 (보증금)
+
+  const {
+    basicRegularPrice,
+    basicDiscountAmount,
+    standardRegularPrice,
+    standardDiscountAmount,
+    premiumRegularPrice,
+    premiumDiscountAmount,
+  } = useChallengeOptionPriceInfo(priceInfoList);
+
+  const { regularPrice, discountAmount, sellingPrice } = useMemo(() => {
+    switch (active) {
+      case 'STANDARD':
+        return {
+          regularPrice: standardRegularPrice,
+          discountAmount: standardDiscountAmount,
+          sellingPrice: standardRegularPrice - standardDiscountAmount,
+        };
+      case 'PREMIUM':
+        return {
+          regularPrice: premiumRegularPrice,
+          discountAmount: premiumDiscountAmount,
+          sellingPrice: premiumRegularPrice - premiumDiscountAmount,
+        };
+      // BASIC
+      default:
+        return {
+          regularPrice: basicRegularPrice,
+          discountAmount: basicDiscountAmount,
+          sellingPrice: basicRegularPrice - basicDiscountAmount,
+        };
+    }
+  }, [
+    active,
+    basicRegularPrice,
+    basicDiscountAmount,
+    standardRegularPrice,
+    standardDiscountAmount,
+    premiumRegularPrice,
+    premiumDiscountAmount,
+  ]);
+
+  const finalPrice = sellingPrice - deposit; // 최종 금액 (환급 금액 미포함)
+
+  const plans = useMemo(() => {
+    const standardPriceInfo = priceInfoList.find(
+      (item) => item.challengePricePlanType === 'STANDARD',
+    );
+    const premiumPriceInfo = priceInfoList.find(
+      (item) => item.challengePricePlanType === 'PREMIUM',
+    );
+
     const plans: Plans = {
       BASIC: basicPriceInfo?.title ?? '베이직',
     };
@@ -60,13 +158,14 @@ function ChallengePriceInfoWithContent({ content, priceInfoList }: Props) {
     if (premiumPriceInfo) plans['PREMIUM'] = '올인원';
 
     return plans;
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [priceInfoList]);
 
   return (
     <div className="flex flex-col items-stretch gap-2">
       <div className="rounded-xs bg-neutral-95">
         <div className="flex items-center px-3 py-2">
-          {Object.entries(getPlans()).map(([key, value]) => (
+          {Object.entries(plans).map(([key, value]) => (
             <PlanButton
               key={`plan-btn-${key}`}
               active={key === active}
@@ -82,35 +181,29 @@ function ChallengePriceInfoWithContent({ content, priceInfoList }: Props) {
 
       <div className="flex flex-col gap-1.5">
         <div className="text-xsmall16 text-neutral-20">
+          {/* 정가 */}
           <div className="flex h-[26px] w-full items-center justify-between px-3 text-neutral-40">
             <span>정가</span>
-            <span>100,000원</span>
+            <span>{regularPrice.toLocaleString()}원</span>
           </div>
-          <div className="flex w-full items-center justify-between px-3 font-medium">
-            <div className="font-semibold text-system-error">10% 할인</div>
-            <div>-10,000원</div>
-          </div>
-          <div className="flex w-full items-center justify-between rounded-[2px] bg-[#FFEFED] px-3 py-1 font-medium">
-            <div>모든 미션 수행 시, 환급</div>
-            <div>-10,000원</div>
-          </div>
+          {/* 할인 금액 */}
+          {discountAmount !== 0 && (
+            <div className="flex w-full items-center justify-between px-3 font-medium">
+              <span className="font-semibold text-system-error">10% 할인</span>
+              <span>-{discountAmount.toLocaleString()}원</span>
+            </div>
+          )}
+          {/* 환급 */}
+          {deposit !== 0 && (
+            <div className="flex w-full items-center justify-between rounded-[2px] bg-[#FFEFED] px-3 py-1 font-medium">
+              <span>모든 미션 수행 시, 환급</span>
+              <span>-{deposit.toLocaleString()}원</span>
+            </div>
+          )}
         </div>
 
         {/* 최종 결제 금액 */}
-        <div className="flex flex-col items-stretch gap-0.5 px-2.5">
-          <div className="flex items-center justify-between font-medium text-neutral-20">
-            <div>최종 결제 금액</div>
-            <div>80,000원</div>
-          </div>
-          <div className="flex flex-col items-end gap-0.5">
-            <strong className="text-medium24 font-bold text-[#4A76FF]">
-              월 13,400원
-            </strong>
-            <span className="text-xxsmall12 text-neutral-30">
-              * 우리은행 6개월 무이자 할부 시
-            </span>
-          </div>
-        </div>
+        <FinalPriceInfo finalPrice={finalPrice} sellingPrice={sellingPrice} />
       </div>
     </div>
   );
