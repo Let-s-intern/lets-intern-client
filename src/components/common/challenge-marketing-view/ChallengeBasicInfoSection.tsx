29ec5fd42146d927817b8bcf611478ca7ef5848d
Yongyeon Kim <seyeon.dev@gmail.com>
2025-06-04 18:09:23 +0900

    refactor: 일정 컴포넌트 분리

--- src/components/common/challenge-marketing-view/ChallengeBasicInfoSection.tsx
+++ src/components/common/challenge-marketing-view/ChallengeBasicInfoSection.tsx
@@ -1,12 +1,9 @@
-import Announcement from '@/assets/icons/announcement.svg?react';
 import ChevronDown from '@/assets/icons/chevron-down.svg?react';
-import ClockIcon from '@/assets/icons/clock.svg?react';
-import Pin from '@/assets/icons/pin.svg?react';
 import { twMerge } from '@/lib/twMerge';
 import { ChallengeIdPrimitive, challengeTypeSchema } from '@/schema';
 import Image from 'next/image';
-import React, { ReactNode } from 'react';
-import { LuCalendarDays } from 'react-icons/lu';
+import React from 'react';
+import ChallengeSchedule from './ChallengeSchedule';
 
 const { PERSONAL_STATEMENT } = challengeTypeSchema.enum;
 
@@ -29,66 +26,6 @@ const PlanButton = ({
     >
       <div className={active ? 'h-5' : 'h-6'}>{children}</div>
     </button>
-  );
-};
-
-const IconTitle = ({
-  icon,
-  children,
-}: {
-  icon: ReactNode;
-  children?: ReactNode;
-}) => {
-  return (
-    <div className="flex items-center gap-2 font-semibold text-neutral-0">
-      {icon}
-      {children}
-    </div>
-  );
-};
-
-const ScheduleBox = ({
-  children,
-  className,
-}: {
-  children?: ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div
-      className={twMerge(
-        'flex flex-1 flex-col gap-3 rounded-xs bg-neutral-95 p-4 pb-5',
-        className,
-      )}
-    >
-      {children}
-    </div>
-  );
-};
-
-const ScheduleWrapper = ({
-  children,
-  className,
-}: {
-  children?: ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={twMerge('flex flex-col gap-1', className)}>{children}</div>
-  );
-};
-
-const ScheduleDescription = ({
-  children,
-  className,
-}: {
-  children?: ReactNode;
-  className?: string;
-}) => {
-  return (
-    <p className={twMerge('text-xsmall14 md:text-xsmall16', className)}>
-      {children}
-    </p>
   );
 };
 
@@ -216,48 +153,7 @@ const ChallengeBasicInfoSection: React.FC<Props> = ({ challenge }) => {
 
       {/* 일정 정보 */}
       <div className="mt-6 flex flex-col md:mt-5 md:flex-row md:gap-3">
-        <ScheduleBox className="rounded-b-none pb-0 md:rounded-xs md:pb-5">
-          <ScheduleWrapper className="hidden md:block">
-            <IconTitle icon={<Pin color="#4A76FF" width={20} height={20} />}>
-              시작 일자
-            </IconTitle>
-            <ScheduleDescription>2025년 01월 04일</ScheduleDescription>
-          </ScheduleWrapper>
-          <ScheduleWrapper>
-            <IconTitle
-              icon={<Announcement color="#4A76FF" width={20} height={20} />}
-            >
-              진행 기간
-            </IconTitle>
-            <ScheduleDescription>
-              2024년 01월 04일 (금) 23시 59분 - 01월 17일 (수) 23시 59분
-            </ScheduleDescription>
-          </ScheduleWrapper>
-        </ScheduleBox>
-        <ScheduleBox className="rounded-t-none md:rounded-xs">
-          <ScheduleWrapper>
-            <IconTitle
-              icon={<ClockIcon color="#4A76FF" width={20} height={20} />}
-            >
-              모집 마감
-            </IconTitle>
-            <ScheduleDescription>
-              2024년 10월 15일(화) 23시 59분
-            </ScheduleDescription>
-          </ScheduleWrapper>
-          <ScheduleWrapper>
-            <IconTitle
-              icon={<LuCalendarDays color="#4A76FF" className="h-5 w-5" />}
-            >
-              <span className='inline-block after:ml-2 after:w-16 after:text-xsmall14 after:font-normal after:text-neutral-30 after:content-["온라인_진행"]'>
-                OT 일자
-              </span>
-            </IconTitle>
-            <ScheduleDescription>
-              2024년 10월 15일(화) 23시 59분
-            </ScheduleDescription>
-          </ScheduleWrapper>
-        </ScheduleBox>
+        <ChallengeSchedule challenge={challenge} />
       </div>
     </div>
   );
