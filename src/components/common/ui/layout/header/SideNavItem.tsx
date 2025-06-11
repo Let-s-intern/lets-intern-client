52195e468ce9c239c3c57f7b3d7e89b86d2abd58
hyonun321 <hyo_nun@kakao.com>
2025-06-11 23:24:21 +0900

    [Style] 사이드 NavItem에 subNavList있을때 Down 컴포넌트가 보이도록 변경

--- src/components/common/ui/layout/header/SideNavItem.tsx
+++ src/components/common/ui/layout/header/SideNavItem.tsx
@@ -1,11 +1,11 @@
 'use client';
 
+import Down from '@/assets/icons/down.svg?react';
 import { twMerge } from '@/lib/twMerge';
 import Link from 'next/link';
 import { AnchorHTMLAttributes, MouseEvent, useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import SubNavItem, { SubNavItemProps } from './SubNavItem';
-
 /**
  * @param {boolean} force
  *   true로 설정하면 window.location.href으로 라우팅
@@ -33,10 +33,10 @@ function SideNavItem({
   const [isOpen, setIsOpen] = useState(false);
 
   const linkClassName = twMerge(
-    'flex w-full cursor-pointer rounded-xs py-3 text-small18 font-semibold text-neutral-30',
+    'text-small16 flex max-h-[40px] w-full cursor-pointer rounded-xs py-2 font-semibold text-neutral-30',
     isNew &&
       "items-center gap-1.5 after:flex after:h-4 after:w-4 after:items-center after:justify-center after:rounded-full after:bg-system-error after:text-[10px] after:font-bold after:leading-none after:text-white after:content-['N']",
-    isOpen && 'bg-primary-5',
+    subNavList && 'justify-between',
     className,
   );
   const LinkComponent: any = isNextRouter ? Link : RouterLink;
@@ -64,18 +64,27 @@ function SideNavItem({
     <div className="flex w-full flex-col px-5">
       <LinkComponent className={linkClassName} {...linkProps}>
         {children}
+        {subNavList && (
+          <Down
+            width={24}
+            className={twMerge(
+              'flex text-neutral-20 transition-transform',
+              isOpen ? 'rotate-180' : 'rotate-0',
+            )}
+          />
+        )}
       </LinkComponent>
 
       {/* 서브 메뉴 */}
       {subNavList && (
         <div
-          className={`${isOpen ? 'max-h-screen pt-2' : 'max-h-0'} flex h-fit w-full flex-col gap-1.5 overflow-hidden transition-all duration-150`}
+          className={`${isOpen ? 'max-h-screen' : 'max-h-0'} flex h-fit w-full flex-col gap-0.5 overflow-hidden transition-all duration-150`}
           onClick={() => setIsOpen(false)}
         >
           {subNavList.map((item) => (
             <SubNavItem
               key={item.href}
-              className="flex px-8 py-2 text-xsmall16 font-semibold text-neutral-20 hover:bg-white"
+              className="flex h-[36px] px-0 py-2 text-xsmall14 font-semibold text-neutral-20 hover:bg-white"
               {...item}
             >
               {item.children}
