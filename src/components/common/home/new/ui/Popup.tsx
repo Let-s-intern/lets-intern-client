4ebe243e79f3aac7649cf119f43b6544bc6b42ea
hyonun321 <hyo_nun@kakao.com>
2025-06-17 14:38:03 +0900

    style: 팝업 레이아웃 변경
    - 체크형 삭제 후 전부 버튼형으로

--- src/components/common/home/new/ui/Popup.tsx
+++ src/components/common/home/new/ui/Popup.tsx
@@ -1,21 +1,15 @@
+import { IBanner } from '@/types/Banner.interface';
+import axios from '@/utils/axios';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
-
-import { IBanner } from '../../../../../types/Banner.interface';
-import axios from '../../../../../utils/axios';
 
 const ONE_DAY = 86400000;
 
 const Popup = () => {
   const [showPopup, setShowPopup] = useState(false);
-  const [closedDuringDay, setClosedDuringDay] = useState(false);
 
-  const toggle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setClosedDuringDay(event.target.checked);
-  };
-
-  const close = () => {
-    if (closedDuringDay) {
+  const close = (storeClosedTime: boolean = false) => {
+    if (storeClosedTime) {
       localStorage.setItem('closedTime', Date.now().toString());
     }
     setShowPopup(false);
@@ -40,6 +34,11 @@ const Popup = () => {
     setShowPopup(true);
   }, []);
 
+  // const data = { 디버깅용
+  //   imgUrl: '/images/career-start-after2.jpg',
+  //   link: 'https://example.com',
+  // };
+
   const { isLoading, data } = useQuery<IBanner>({
     queryKey: ['PopUp'],
     queryFn: async () => {
@@ -62,22 +61,26 @@ const Popup = () => {
 
   return showPopup && data ? (
     <div className="fixed bottom-0 top-0 z-50 flex h-full w-screen items-center justify-center bg-neutral-0/60 p-5">
-      <div className="relative h-fit max-w-[450px] overflow-hidden rounded-md bg-static-100 pb-5 shadow-05">
+      <div className="relative h-fit max-w-[450px] overflow-hidden rounded-md bg-static-100 shadow-05">
         <img
           className="popup_banner h-auto w-full cursor-pointer"
           src={data?.imgUrl}
           onClick={clickPopup}
           alt="홈 화면 팝업 이미지"
         />
-        <img
-          onClick={close}
-          className="absolute right-4 top-5 h-6 w-6 cursor-pointer md:h-8 md:w-8"
-          src="/icons/Close_SM.svg"
-          alt="팝업 닫기 아이콘"
-        />
-        <div className="ml-4 mt-4 flex gap-1 pl-2">
-          <input type="checkbox" onChange={toggle} />
-          <span className="text-0.75">오늘 하루 보지 않기</span>
+        <div className="flex">
+          <button
+            className="w-1/2 py-5 text-center text-xxsmall12 font-semibold"
+            onClick={() => close(true)}
+          >
+            하루 동안 보지 않기
+          </button>
+          <button
+            className="w-1/2 py-5 text-center text-xxsmall12 font-semibold"
+            onClick={() => close()}
+          >
+            닫기
+          </button>
         </div>
       </div>
     </div>
