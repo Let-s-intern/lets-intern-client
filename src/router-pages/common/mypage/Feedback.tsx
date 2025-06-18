895b3762762cb3541912338f319020665b8d6c6d
hyonun321 <hyo_nun@kakao.com>
2025-06-18 23:40:48 +0900

    Style: 버튼컴포넌트 및 레이아웃 신청현황의 카드 처럼 변경

--- src/router-pages/common/mypage/Feedback.tsx
+++ src/router-pages/common/mypage/Feedback.tsx
@@ -1,4 +1,5 @@
 import { useMentorChallengeListQuery, useUserQuery } from '@/api/user';
+import LinkButton from '@/components/common/mypage/ui/button/LinkButton';
 import useAuthStore from '@/store/useAuthStore';
 
 const Feedback = () => {
@@ -16,51 +17,59 @@ const Feedback = () => {
       {isLoading ? (
         <div className="text-neutral-500">로딩 중...</div>
       ) : challengeList.length > 0 ? (
-        <>
+        <div className="flex flex-col gap-4">
           {challengeList.map((challenge) => (
             <div
               key={challenge.challengeId}
-              className="flex w-[550px] items-center justify-between gap-2 rounded-sm border border-neutral-200 p-2"
+              className="flex w-full flex-col items-start gap-4 overflow-hidden rounded-xs md:flex-row md:border md:border-neutral-85 md:p-2.5"
             >
-              <div className="flex items-center gap-4">
+              <div className="flex w-full flex-1 flex-col gap-2 md:flex-row md:gap-4">
                 <img
                   src={challenge.thumbnail || '/images/community1.png'}
                   alt="챌린지 썸네일"
-                  className="h-24 w-40 rounded-sm object-cover"
+                  className="h-[7.5rem] w-full bg-primary-light object-cover md:h-[9rem] md:w-[11rem] md:rounded-xs"
                 />
-                <div className="flex flex-col">
-                  <p className="text-xsmall16 font-bold text-neutral-900">
-                    {challenge.programStatusType === 'PREV'
-                      ? '[이전]'
-                      : challenge.programStatusType === 'CURRENT'
-                        ? '[진행중]'
-                        : '[예정]'}
-                  </p>
-                  <h3 className="text-base font-bold">{challenge.title}</h3>
-                  <p className="text-sm text-neutral-600">
-                    {challenge.shortDesc}
-                  </p>
-                  <p className="mt-2 text-xxsmall12 font-semibold">
-                    진행기간{' '}
-                    <span className="text-xxsmall12 font-semibold text-blue-700">
-                      {new Date(challenge.startDate).toLocaleDateString(
-                        'ko-KR',
-                      )}{' '}
+                <div className="flex flex-1 flex-col justify-between gap-2 py-2">
+                  <div className="flex w-full flex-col gap-y-0.5">
+                    <h2 className="font-semibold">{challenge.title}</h2>
+                    <p className="text-sm text-neutral-30">
+                      {challenge.shortDesc}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-1.5 md:justify-start">
+                    <span className="text-xs text-neutral-0">진행기간</span>
+                    <span className="text-xs font-medium text-primary-dark">
+                      {new Date(challenge.startDate)
+                        .toLocaleDateString('ko-KR', {
+                          year: '2-digit',
+                          month: '2-digit',
+                          day: '2-digit',
+                        })
+                        .replace(/\./g, '.')
+                        .replace(/\s/g, '')}{' '}
                       ~{' '}
-                      {new Date(challenge.endDate).toLocaleDateString('ko-KR')}
+                      {new Date(challenge.endDate)
+                        .toLocaleDateString('ko-KR', {
+                          year: '2-digit',
+                          month: '2-digit',
+                          day: '2-digit',
+                        })
+                        .replace(/\./g, '.')
+                        .replace(/\s/g, '')}
                     </span>
-                  </p>
+                  </div>
                 </div>
               </div>
-              <button
-                className="whitespace-nowrap rounded-xs border border-neutral-300 px-3 py-1 text-sm font-medium"
-                onClick={() => alert('이동 처리 예정')}
+              <LinkButton
+                to={`/challenge/${challenge.challengeId}`}
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 피드백 페이지 이동
-              </button>
+              </LinkButton>
             </div>
           ))}
-        </>
+        </div>
       ) : (
         <div className="text-neutral-500">참여 중인 챌린지가 없습니다.</div>
       )}
