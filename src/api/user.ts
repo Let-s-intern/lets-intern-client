a44c2d39dc3c99596862c5b82c1ad01f01838dbe
Yongyeon Kim <seyeon.dev@gmail.com>
2025-06-19 11:25:51 +0900

    fix: 유저 전체 목록 URL 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -47,7 +47,7 @@ export const useUserAdminQuery = ({
   return useQuery({
     queryKey: [UseUserAdminQueryKey, email, name, phoneNum, pageable],
     queryFn: async () => {
-      const res = await axios.get('/v2/admin/user/mentor', {
+      const res = await axios.get('/user/admin', {
         params: {
           email,
           name,
