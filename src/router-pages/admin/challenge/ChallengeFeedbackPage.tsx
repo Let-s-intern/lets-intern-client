41060f2d6bebc68c97531d087a1bd947a6313ccc
Yongyeon Kim <seyeon.dev@gmail.com>
2025-06-19 22:02:59 +0900

    feat: 수정한 내용 있으면 페이지 이탈 시 warning 표시

--- src/router-pages/admin/challenge/ChallengeFeedbackPage.tsx
+++ src/router-pages/admin/challenge/ChallengeFeedbackPage.tsx
@@ -1,35 +1,19 @@
 /** 참여자별 피드백 페이지 (피드백 작성 페이지) */
 
 import { usePatchAttendance } from '@/api/attendance';
-import { useFeedbackAttendenceQuery } from '@/api/challenge';
+import { useFeedbackAttendanceQuery } from '@/api/challenge';
 import { useAdminSnackbar } from '@/hooks/useAdminSnackbar';
+import useBeforeUnloadWarning from '@/hooks/useBeforeUnloadWarning';
 import EditorApp from '@components/admin/lexical/EditorApp';
 import Heading2 from '@components/admin/ui/heading/Heading2';
 import { Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
-export default function ChallengeFeedbackPage() {
-  const navigate = useNavigate();
-  const { programId, missionId, userId, ...rest } = useParams();
+const { missionTitle, missionRound, major, wishCompany, wishJob, link, name } =
+  JSON.parse(localStorage.getItem('attendance')!);
 
-  const { snackbar } = useAdminSnackbar();
-  const patchAttendance = usePatchAttendance();
-  const { data } = useFeedbackAttendenceQuery({
-    challengeId: programId,
-    missionId,
-    attendanceId: userId,
-  });
-
-  const {
-    missionTitle,
-    missionRound,
-    major,
-    wishCompany,
-    wishJob,
-    link,
-    name,
-  } = JSON.parse(localStorage.getItem('attendance')!);
+const AttendanceInfoList = () => {
   const { challengeOptionCode } = JSON.parse(localStorage.getItem('mission')!);
 
   const list = [
@@ -47,7 +31,30 @@ export default function ChallengeFeedbackPage() {
     </Link>,
   ];
 
+  return (
+    <ul className="list-inside list-disc">
+      {list.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default function ChallengeFeedbackPage() {
+  const navigate = useNavigate();
+  const { programId, missionId, userId } = useParams();
+
+  const { snackbar } = useAdminSnackbar();
+  const patchAttendance = usePatchAttendance();
+  const { data } = useFeedbackAttendanceQuery({
+    challengeId: programId,
+    missionId,
+    attendanceId: userId,
+  });
+
   const [content, setContent] = useState<string>();
+
+  useBeforeUnloadWarning(content !== data?.attendanceDetailVo.feedback);
 
   const handleChangeEditor = (jsonString: string) => {
     setContent(jsonString);
@@ -62,6 +69,18 @@ export default function ChallengeFeedbackPage() {
     snackbar('저장되었습니다.');
   };
 
+  const handleBackToListWithConfirm = () => {
+    if (content !== data?.attendanceDetailVo.feedback) {
+      const isConfirm = confirm(
+        '작성된 내용이 삭제될 수 있습니다.\n그래도 돌아가시겠습니까?',
+      );
+      if (!isConfirm) return;
+    }
+    navigate(
+      `/admin/challenge/operation/${programId}/feedback/mission/${missionId}/participants`,
+    );
+  };
+
   useEffect(() => {
     setContent(data?.attendanceDetailVo.feedback || undefined);
   }, [data]);
@@ -69,24 +88,13 @@ export default function ChallengeFeedbackPage() {
   return (
     <div className="mt-5 px-5">
       <Heading2 className="mb-2">{name} 피드백</Heading2>
-      <ul className="list-inside list-disc">
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <AttendanceInfoList />
       <EditorApp
         initialEditorStateJsonString={content}
         onChange={handleChangeEditor}
       />
       <div className="flex items-center justify-end gap-4">
-        <Button
-          variant="outlined"
-          onClick={() =>
-            navigate(
-              `/admin/challenge/operation/${programId}/feedback/mission/${missionId}/participants`,
-            )
-          }
-        >
+        <Button variant="outlined" onClick={handleBackToListWithConfirm}>
           리스트로 돌아가기
         </Button>
         <Button variant="contained" onClick={handleSave}>
