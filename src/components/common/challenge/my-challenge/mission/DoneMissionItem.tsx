f599e9f2c442496887696fc4ce52da7400e4635b
hyonun321 <hyo_nun@kakao.com>
2025-06-19 23:05:47 +0900

    feat: 쿼리생성하여 mission과 attendanceInfo 분리화

--- src/components/common/challenge/my-challenge/mission/DoneMissionItem.tsx
+++ src/components/common/challenge/my-challenge/mission/DoneMissionItem.tsx
@@ -1,14 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+import { useChallengeMissionAttendanceInfoQuery } from '../../../../../api/challenge';
 import { useCurrentChallenge } from '../../../../../context/CurrentChallengeProvider';
-import {
-  MyChallengeMissionByType,
-  userChallengeMissionDetail,
-} from '../../../../../schema';
-import axios from '../../../../../utils/axios';
+import { MyChallengeMissionByType } from '../../../../../schema';
 import { missionSubmitToBadge } from '../../../../../utils/convert';
 import DoneMissionDetailMenu from './DoneMissionDetailMenu';
 
@@ -23,25 +19,16 @@ const DoneMissionItem = ({ mission }: Props) => {
   const [isDetailShown, setIsDetailShown] = useState(false);
 
   const {
-    data: missionDetail,
+    data: missionDetailData,
     isLoading: isDetailLoading,
     error: detailError,
-  } = useQuery({
-    enabled: Boolean(currentChallenge?.id) && isDetailShown,
-    queryKey: [
-      'challenge',
-      currentChallenge?.id,
-      'mission',
-      mission.id,
-      'detail',
-    ],
-    queryFn: async () => {
-      const res = await axios.get(
-        `challenge/${currentChallenge?.id}/missions/${mission.id}`,
-      );
-      return userChallengeMissionDetail.parse(res.data.data).missionInfo;
-    },
+  } = useChallengeMissionAttendanceInfoQuery({
+    challengeId: currentChallenge?.id ?? '',
+    missionId: mission.id,
   });
+
+  const missionDetail = missionDetailData?.missionInfo;
+  const attendanceInfo = missionDetailData?.attendanceInfo;
 
   const currentSchedule = schedules.find((schedule) => {
     return schedule.missionInfo.id === mission.id;
@@ -111,6 +98,10 @@ const DoneMissionItem = ({ mission }: Props) => {
                 missionDetail={missionDetail}
                 schedule={currentSchedule}
                 missionByType={mission}
+                challengeId={currentChallenge?.id}
+                isFeedbackCompleted={
+                  attendanceInfo?.feedbackStatus === 'COMPLETED'
+                }
               />
             ))}
     </li>
