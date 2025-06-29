0b77f3816feea60d92c819bc9a4849f196b697b5
yyeonkim <seyeon.dev@gmail.com>
2025-06-29 11:14:06 +0900

    chore: 주석 추가

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -15,6 +15,11 @@ export default function useScrollDirection(pathname?: string) {
     if (typeof window === 'undefined') return;
 
     const handleScroll = () => {
+      // todo:
+      // useScrollDirection 훅은 순수하게 스크롤 방향만 제공하고,
+      // 페이지별 필터링 로직은 NextNavBar와 같은 개별 컴포넌트에서 처리하는 것이
+      // 더 명확하고 응집도 높은 설계가 될 것입니다.
+
       // 현재 경로가 scrollEventPage 중 하나로 시작되지 않을 때는 스크롤 이벤트를 무시
       if (
         pathname &&
@@ -23,6 +28,7 @@ export default function useScrollDirection(pathname?: string) {
         return;
       }
 
+      // pathname을 넘기지 않아도 scrollDirection 지정
       const currentScrollY = Math.max(0, window.scrollY);
 
       if (currentScrollY > lastScrollY.current) {
