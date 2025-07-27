d371fa9ad68313d3bca0d69c56a4e8ad6c72471e
hyonun321 <hyo_nun@kakao.com>
2025-07-27 17:30:53 +0900

    feat: 체크박스와 드롭다운 컴포넌트 추가
    - 그외 기타 스타일 조정

--- src/components/common/challenge/my-challenge/MissionSubmitBonusSection.tsx
+++ src/components/common/challenge/my-challenge/MissionSubmitBonusSection.tsx
@@ -1,5 +1,7 @@
 import { clsx } from 'clsx';
 import { useState } from 'react';
+import AgreementCheckbox from './AgreementCheckbox';
+import BankSelectDropdown from './BankSelectDropdown';
 import MissionSubmitButton from './MissionSubmitButton';
 import MissionToast from './MissionToast';
 
@@ -15,9 +17,18 @@ const MissionSubmitBonusSection = ({
   const [textareaValue, setTextareaValue] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [showToast, setShowToast] = useState(false);
+  const [selectedBank, setSelectedBank] = useState<string>('');
+  const [accountNumber, setAccountNumber] = useState('');
+  const [isAgreed, setIsAgreed] = useState(false);
 
   const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTextareaValue(e.target.value);
+  };
+
+  const handleAccountNumberChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setAccountNumber(e.target.value);
   };
 
   const handleSubmit = () => {
@@ -29,84 +40,86 @@ const MissionSubmitBonusSection = ({
     }
   };
 
+  const handleBankSelect = (bank: string) => {
+    setSelectedBank(bank);
+  };
+
   return (
     <section className={clsx('', className)}>
       <h2 className="mb-6 text-small18 font-bold text-neutral-0">
         미션 제출하기
       </h2>
-
-      {/* 블로그 링크 */}
-      <section>
-        <div className="mb-1.5">
-          <div className="mb-1.5 flex items-center gap-2">
-            <span className="text-xsmall16 font-semibold text-neutral-0">
-              블로그 링크
-            </span>
-          </div>
-          <div className="rounded bg-neutral-95 px-3 py-3 text-xsmall14 text-neutral-10">
-            링크가 잘 열리는지 확인해주세요.
-          </div>
+      <div className="mb-1.5">
+        <div className="mb-1.5 flex items-center gap-2">
+          <span className="text-xsmall16 font-semibold text-neutral-0">
+            챌린지 참여 목표
+          </span>
         </div>
-        <textarea
-          className={clsx(
-            'w-full resize-none rounded-xxs border border-neutral-80 bg-white',
-            'px-3 py-2 text-base text-neutral-0 placeholder:text-neutral-50',
-            'h-[44px] outline-none focus:border-primary',
-            'disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:text-neutral-50',
-          )}
-          placeholder={'링크를 입력해주세요.'}
-          value={textareaValue}
-          onChange={handleTextareaChange}
-          disabled={isSubmitted}
-        />
-      </section>
+        <div className="rounded bg-neutral-95 px-3 py-3 text-xsmall14 text-neutral-10">
+          미션 제출 후, 작성한 챌린지 목표를 카카오톡 오픈채팅방에 공유해주세요.
+        </div>
+      </div>
+      <textarea
+        className={clsx(
+          'w-full resize-none rounded-xxs border border-neutral-80 bg-white',
+          'p-3 text-base text-neutral-0 placeholder:text-neutral-50',
+          'min-h-[120px] outline-none focus:border-primary',
+          'disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:text-neutral-50',
+        )}
+        placeholder={
+          '챌린지를 신청한 목적과 계기,\n또는 챌린지 참여를 통해 이루고 싶은 목표를 자유롭게 작성해주세요.'
+        }
+        value={textareaValue}
+        onChange={handleTextareaChange}
+        disabled={isSubmitted}
+      />
 
       {/* 리워드 받을 계좌번호 */}
-      <section>
-        <div className="mb-1.5">
-          <div className="mb-1.5 flex items-center gap-2">
-            <span className="text-xsmall16 font-semibold text-neutral-0">
-              리워드 받을 계좌번호
-            </span>
-          </div>
-          <div className="rounded bg-neutral-95 px-3 py-3 text-xsmall14 text-neutral-10">
-            리워드 받을 은행과 계좌번호를 입력해주세요. 본인 명의가 아닌
-            계좌로는 리워드가 입금되지 않습니다.
-          </div>
+      <div className="mt-4 flex flex-col gap-1">
+        <span className="text-xsmall16 font-semibold text-neutral-0">
+          리워드 받을 계좌번호
+        </span>
+        <div className="flex flex-row gap-1">
+          <BankSelectDropdown
+            selectedBank={selectedBank}
+            onBankSelect={handleBankSelect}
+            disabled={isSubmitted}
+          />
+          <textarea
+            className={clsx(
+              'w-full resize-none rounded-xxs border border-neutral-80 bg-white',
+              'px-3 py-2 text-base text-neutral-0 placeholder:text-neutral-50',
+              'h-[44px] outline-none focus:border-primary',
+              'disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:text-neutral-50',
+            )}
+            placeholder={'계좌번호를 입력해주세요.'}
+            value={accountNumber}
+            onChange={handleAccountNumberChange}
+            disabled={isSubmitted}
+          />
         </div>
-        {/* 은행 선택 드롭박스 들어가야함 */}
-        <textarea
-          className={clsx(
-            'w-full resize-none rounded-xxs border border-neutral-80 bg-white',
-            'px-3 py-2 text-base text-neutral-0 placeholder:text-neutral-50',
-            'h-[44px] outline-none focus:border-primary',
-            'disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:text-neutral-50',
-          )}
-          placeholder={'계좌번호를 입력해주세요.'}
-          value={textareaValue}
-          onChange={handleTextareaChange}
-          disabled={isSubmitted}
-        />
-      </section>
+      </div>
 
       {/* 개인정보 활용 동의 */}
-      <section>
-        <div className="mb-1.5">
-          <div className="mb-1.5 flex items-center gap-2">
-            <span className="text-xsmall16 font-semibold text-neutral-0">
-              개인정보 활용 동의
-            </span>
-          </div>
-          <div className="rounded bg-neutral-95 px-3 py-3 text-xsmall14 text-neutral-10">
-            [개인정보 보호법] 제15조 및 제17조에 따라 아래의 내용으로 개인정보를
-            수집, 이용 및 제공하는데 동의합니다.
-            <br /> □ 개인정보의 수집 및 이용에 관한 사항
-            <br /> ✓ 수집하는 개인정보 항목 : 성명, 전화번호, 계좌번호
-            <br /> ✓ 개인정보의 이용 목적 : 렛츠커리어 프로그램 후기 리워드 지급
-          </div>
+      <div className="mt-4 flex flex-col gap-1">
+        <span className="text-xsmall16 font-semibold text-neutral-0">
+          개인정보 활용 동의
+        </span>
+        <div className="rounded bg-neutral-95 px-3 py-3 text-xsmall14 text-neutral-10">
+          [개인정보 보호법] 제15조 및 제17조에 따라 아래의 내용으로 개인정보를
+          수집, 이용 및 제공하는데 동의합니다.
+          <br /> □ 개인정보의 수집 및 이용에 관한 사항 <br />
+          ✓ 수집하는 개인정보 항목 : 성명, 전화번호, 계좌번호 <br />
+          ✓ 개인정보의 이용 목적 : 렛츠커리어 프로그램 후기 리워드 지급 <br />
         </div>
-        {/* 리워드 지급을 위한 개인정보 활용에 동의합니다 */}
-      </section>
+        <div className="mt-2">
+          <AgreementCheckbox
+            checked={isAgreed}
+            onCheckedChange={setIsAgreed}
+            disabled={isSubmitted}
+          />
+        </div>
+      </div>
 
       <MissionSubmitButton
         isSubmitted={isSubmitted}
