5c10e9159edee96b1491bb676726e22c3043febe
hyonun321 <hyo_nun@kakao.com>
2025-08-01 23:46:18 +0900

    feat: n회차 미션수정 함수 생성
    - 현재 서버에 존재하지않는듯함

--- src/api/attendance.ts
+++ src/api/attendance.ts
@@ -32,3 +32,21 @@ export const useSubmitMission = () => {
     onError: (error) => console.error('useSubmitZeroMission >>', error),
   });
 };
+
+/** n회차 미션 수정 /api/v1/attendance/{missionId} */
+export const usePatchMission = () => {
+  return useMutation({
+    mutationFn: async ({
+      missionId,
+      link,
+      review,
+    }: {
+      missionId: number;
+      link: string;
+      review: string;
+    }) => {
+      return axios.patch(`/attendance/${missionId}`, { link, review });
+    },
+    onError: (error) => console.error('usePatchMission >>', error),
+  });
+};
