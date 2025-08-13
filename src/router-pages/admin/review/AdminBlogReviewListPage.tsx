c8ae4cf6242e82e5fa00c405171a90fefd4dc7e5
yyeonkim <seyeon.dev@gmail.com>
2025-08-13 20:39:53 +0900

    LC-2314 feat: ADMIN 블로그 후기: 연락처, 은행, 계좌번호 컬럼 추가

--- src/router-pages/admin/review/AdminBlogReviewListPage.tsx
+++ src/router-pages/admin/review/AdminBlogReviewListPage.tsx
@@ -6,8 +6,10 @@ import {
   usePostAdminBlogReview,
 } from '@/api/review';
 import { YYYY_MMDD_THHmmss } from '@/data/dayjsFormat';
+import { PaymentMethodKey } from '@/data/getPaymentSearchParams';
 import dayjs from '@/lib/dayjs';
 import { ProgramTypeEnum } from '@/schema';
+import { bankTypeToText } from '@/utils/convert';
 import { generateUUID } from '@/utils/random';
 import { Button, Checkbox } from '@mui/material';
 import {
@@ -68,6 +70,24 @@ export default function AdminBlogReviewListPage() {
       width: 110,
       editable: true,
       sortable: false,
+    },
+    {
+      field: 'phoneNum',
+      headerName: '연락처',
+      width: 200,
+    },
+    {
+      field: 'accountType',
+      headerName: '은행명',
+      width: 110,
+      renderCell(params: GridRenderCellParams<Row, PaymentMethodKey>) {
+        return params.value ? bankTypeToText[params.value] : '-';
+      },
+    },
+    {
+      field: 'accountNum',
+      headerName: '계좌번호',
+      width: 200,
     },
     {
       field: 'title',
@@ -264,6 +284,7 @@ export default function AdminBlogReviewListPage() {
 
     setRows(initialRows);
   }, [data, setRows]);
+  console.log(data);
 
   return (
     <div className="p-5">
