4d17fb1736500a262a3a9304707c4804f5b596e7
hyonun321 <hyo_nun@kakao.com>
2025-08-19 01:34:23 +0900

    fix: zod에러 해결
    - nullable 추가가

--- src/api/challengeSchema.ts
+++ src/api/challengeSchema.ts
@@ -79,12 +79,14 @@ export const challengeMissionFeedbackSchema = z.object({
     th: z.number().nullish(),
     title: z.string().nullish(),
   }),
-  attendanceInfo: z.object({
-    link: z.string(),
-    feedbackStatus: FeedbackStatusEnum.nullable().default('WAITING'),
-    feedback: z.string().nullish(),
-    mentorName: z.string().nullish(),
-  }),
+  attendanceInfo: z
+    .object({
+      link: z.string(),
+      feedbackStatus: FeedbackStatusEnum.nullable().default('WAITING'),
+      feedback: z.string().nullish(),
+      mentorName: z.string().nullish(),
+    })
+    .nullable(),
 });
 
 export const feedbackAttendanceSchema = z.object({
