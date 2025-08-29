1b2a5dfdf7ab21ecc76d2cdbfc3413131c881af0
yyeonkim <seyeon.dev@gmail.com>
2025-08-29 21:06:32 +0900

    LC-2405 USER 보너스 미션: 블로그 링크 내용 수정

--- src/components/common/challenge/my-challenge/section/MissionGuideBonusSection.tsx
+++ src/components/common/challenge/my-challenge/section/MissionGuideBonusSection.tsx
@@ -86,7 +86,7 @@ const MissionGuideBonusSection = ({
                 <ol className="list-inside list-decimal whitespace-pre-wrap text-xsmall14 font-medium text-neutral-0 md:text-xsmall16">
                   <li>
                     이번 챌린지를 통해 배운 점, 성장한 점 등을 바탕으로 네이버
-                    블로그, 티스토리, 브런치등 택하여 후기를 작성해주세요.
+                    블로그, 티스토리, 미디엄 등 택하여 후기를 작성해주세요.
                   </li>
                   <li>
                     작성한 후기 링크를 챌린지 대시보드 &gt; 보너스 미션 제출란에
