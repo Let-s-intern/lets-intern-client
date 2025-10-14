1b7dc8a49acb287362f0a153640ddb65888498f0
hyonun321 <hyo_nun@kakao.com>
2025-10-14 14:36:25 +0900

    fix(sentry): 센트리 제대로 동작 안하는 문제 수정
    - 환경변수를 읽어오지 못했음
    - 경로상 에러바운더리가 덮어씌워지지 않았음
    - 현재 앱라우터, 페이지라우터가 중복되고 사용되어있어서 두개다 선언해서 사용해야함
    - npm run dev:sentry로 더보팩이 없는 개발환경 명령어 추가

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/nextjs';
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
