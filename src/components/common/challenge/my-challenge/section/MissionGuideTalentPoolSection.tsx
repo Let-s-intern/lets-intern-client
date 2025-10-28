0e1a9cf14955aeba4af5d71737b23f18a4e02cb3
yeeun426 <thsudkcla7@naver.com>
2025-10-29 00:38:03 +0900

    feat : 인재풀 미션용 정적값 변경

--- src/components/common/challenge/my-challenge/section/MissionGuideTalentPoolSection.tsx
+++ src/components/common/challenge/my-challenge/section/MissionGuideTalentPoolSection.tsx
@@ -68,24 +68,19 @@ const MissionGuideTalentPoolSection = ({
         <section className="flex flex-col gap-5 rounded-xs border border-neutral-80 px-4 py-4">
           {/* 인트로 섹션 */}
           <section>
-            <p className="whitespace-pre-wrap text-xsmall14 text-neutral-0 md:text-xsmall16">
+            <p className="whitespace-pre-wrap text-xsmall14 font-medium text-neutral-0 md:text-xsmall16">
               안녕하세요, 렛츠커리어입니다.
               <br />
-              챌린지를 끝까지 함께해 주신 여러분께 <b>특별한 채용 기회</b>를
-              준비했어요! 🎉 <br />
-              렛츠커리어는 더벤처스와 함께,{' '}
-              <b>
-                ‘한 번의 등록으로 여러 기업의 채용 제안’을 받을 수 있는 인재풀
-                서비스
-              </b>
-              를 운영하고 있습니다.
+              챌린지를 끝까지 함께해 주신 여러분께 특별한 기회를 드려요! 🎉
+              <br />
+              렛츠커리어는 “한 번의 등록으로 여러 기업에게 채용 제안”을 받을 수
+              있는 인재풀 서비스를 운영하고 있습니다.
               <br />
               <br />
-              챌린지 기간 동안 작성하신 이력서 / 포트폴리오를 제출하시면,
-              렛츠커리어가 대신 인제풀에 등록해드립니다!
-              <br />
-              👉 두 가지 서류 모두 제출 가능하며, 이후 수정된 서류도 언제든
-              업데이트할 수 있어요.
+              챌린지 기간 동안 완성한 이력서 / 포트폴리오/자기소개서를
+              제출하시면, 렛츠커리어가 대신 인재풀에 등록해드려요. <br />
+              👉 세 가지 서류 모두 제출 가능하며, 이후 수정된 서류도
+              마이페이지에서 언제든 업데이트할 수 있어요.
             </p>
           </section>
 
@@ -99,17 +94,16 @@ const MissionGuideTalentPoolSection = ({
               <div className="flex items-start gap-2">
                 <ol className="list-inside list-decimal whitespace-pre-wrap text-xsmall14 font-medium text-neutral-0 md:text-xsmall16">
                   <li>
-                    초기 스타트업에 적극적으로 투자하는 더벤처스에서 관리하는
+                    렛츠커리어에서 IT 스타트업 채용연계를 위해 관리하는
                     인재풀입니다.
                   </li>
                   <li>
-                    등록만 하면, <b>30개 이상의 검증된 스타트업</b>에 서류가
-                    공유되어, 여러 기업으로부터 <b>채용 제안</b>을 받을 수
-                    있습니다.
+                    등록만 하면, 렛츠커리어 제휴 스타트업 및 주요 기업들로부터
+                    채용 제안을 받을 수 있습니다.
                   </li>
                   <li>
-                    인턴십뿐 아니라 정규직까지, 적합한 회사와 적극적으로 연결해
-                    드려요.
+                    인턴십뿐 아니라 신입 정규직까지, 적합한 회사와 적극적으로
+                    연결해 드려요.
                   </li>
                 </ol>
               </div>
@@ -125,27 +119,19 @@ const MissionGuideTalentPoolSection = ({
                   <li>
                     1️⃣ 검증된 스타트업 네트워크
                     <div className="ml-5">
-                      더벤처스, 카카오벤처스, 퓨처플레이, 스트롱벤처스 등 유명
-                      VC 투자 기업 중심
+                      생활용품 커머스 와이즐리, 뷰티 스타트업 앳홈등
                     </div>
                   </li>
                   <li>
-                    2️⃣ 다양한 산업군
-                    <div className="ml-5">
-                      이커머스, OTT, AI, 블록체인, 금융/VC 등 35+개 기업 참여
-                    </div>
-                  </li>
-                  <li>
-                    3️⃣ 서류 전형 패스
+                    2️⃣ 서류 전형 패스
                     <div className="ml-5">
                       인재풀 등록만으로 빠르게 면접/채용 제안 연결
                     </div>
                   </li>
                   <li>
-                    4️⃣ 지속적 기회 제공
+                    3️⃣ 지속적 기회 제공
                     <div className="ml-5">
-                      인턴십 이후에도 계속 제안 받을 수 있고, 더벤처스
-                      프로그램에 우선 초대해드려요!
+                      인턴십 이후에도 정규직 등으로 연결
                     </div>
                   </li>
                 </ol>
--- src/components/common/challenge/my-challenge/section/PersonalInfoConsent.tsx
+++ src/components/common/challenge/my-challenge/section/PersonalInfoConsent.tsx
@@ -25,19 +25,19 @@ const PersonalInfoConsent = ({
             개인정보의 수집 및 이용에 대한 동의
             <ul>
               <li className="flex before:mx-1 before:content-['•']">
-                수집 및 이용 목적: 렛츠커리어와 더벤처스의 서류 피드백
+                수집 및 이용 목적: 렛츠커리어
               </li>
               <li className="flex before:mx-1 before:content-['•']">
-                수집 및 이용 항목(필수): 성명, 연락처, 이메일 주소, 희망 직군,
-                희망 직무, 희망 산업
+                수집 및 이용 항목(필수): 희망 직군, 희망 직무, 희망 산업,
+                이력서, 포트폴리오
               </li>
               <li className="flex before:mx-1 before:content-['•']">
-                수집 및 이용 항목(선택): 이력서 파일, 포트폴리오 파일
+                수집 및 이용 항목(선택): 자기소개서
               </li>
               <li className="flex before:mx-1 before:content-['•']">
                 개인정보의 보유 및 이용기간: 개인정보 수집·이용에 관한
                 동의일로부터 위 이용목적을 위하여 3개월간 보유 및 이용하게
-                됩니다. 단, 더벤처스 인재풀 등록에 동의하시는 경우 채용 성사를
+                됩니다. 단, 렛츠커리어 인재풀 등록에 동의하시는 경우 채용 성사를
                 위하여 3년간 보유하게 됩니다.
               </li>
             </ul>
