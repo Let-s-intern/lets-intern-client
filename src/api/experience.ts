19ac91098226cba723735e53d08c69cb8db65c95
okxooxoo <okxooxoo@naver.com>
2025-11-04 23:06:11 +0900

    fix(experience): Backend API 명세 변경에 따른 경험 조회 요청 방식 수정

--- src/api/experience.ts
+++ src/api/experience.ts
@@ -25,16 +25,13 @@ export const useGetAllUserExperienceQuery = (
   filter: ExperienceFiltersReq,
   pageable: Pageable,
 ) => {
-  const params = new URLSearchParams({
-    page: pageable.page.toString(),
-    size: pageable.size.toString(),
-    filter: JSON.stringify(filter),
-  });
-
   return useQuery({
     queryKey: [UserExperienceQueryKey, filter, pageable],
     queryFn: async () => {
-      const res = await axios.get(`/user-experience/search`, { params });
+      const res = await axios.post(
+        `/user-experience/search?page=${pageable.page}&size=${pageable.size}`,
+        filter,
+      );
       return userExperienceListSchema.parse(res.data.data);
     },
   });
