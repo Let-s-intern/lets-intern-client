835a3d16d25f41a077f1b9a42a3a1d15f2b9aa1f
yeeun426 <thsudkcla7@naver.com>
2025-11-14 19:05:05 +0900

    feat : ADMIN 챌린지 운영 > 피드백 미션 제출 링크 변경

--- src/components/admin/pages/challenge/ChallengeFeedbackPage.tsx
+++ src/components/admin/pages/challenge/ChallengeFeedbackPage.tsx
@@ -40,14 +40,16 @@ const useLocalStorageState = () => {
 
 const AttendanceInfoList = memo(function AttendanceInfoList() {
   const { mission, attendance } = useLocalStorageState();
-
   const list = [
     `${attendance?.missionTitle} / ${attendance?.missionRound}회차`,
     `피드백 유형: ${mission?.challengeOptionTitle}`,
     `참여자 정보: ${attendance?.major} / ${attendance?.wishCompany} / ${attendance?.wishJob}`,
     <Link
       key={attendance?.link}
-      href={attendance?.link || '#'}
+      href={
+        attendance?.link ||
+        `/admin/challenge/operation/${attendance?.id}/attendances/${mission?.id}/${attendance?.userId}`
+      }
       className="text-primary underline"
       target="_blank"
       rel="noopener noreferrer"
