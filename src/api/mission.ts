2c3636e65e1f44efe32d4303b5060cce252a8581
yeeun426 <thsudkcla7@naver.com>
2025-11-16 17:45:39 +0900

    feat : 인재풀 미션 API 변경

--- src/api/mission.ts
+++ src/api/mission.ts
@@ -81,3 +81,25 @@ export const usePostMissionTalentPoolMutation = () => {
     },
   });
 };
+
+/** POST [유저] 인재풀 미션 출석 생성 /api/v1/attendance/pool/{missionId} */
+export const usePostTalentPoolAttendanceMutation = (
+  missionId: number | undefined,
+) => {
+  return useMutation({
+    mutationFn: async (req: FormData) => {
+      await axios.post(`/attendance/pool/${missionId}`, req, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+    },
+    onError(error: AxiosError<ErrorResponse>) {
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        '알 수 없는 오류가 발생했습니다.';
+      alert('인재풀 제출에 실패했습니다: ' + errorMessage);
+    },
+  });
+};
