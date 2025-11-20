18e966f1aeb6549b89ae0618bee937818745fdba
hyonun321 <hyo_nun@kakao.com>
2025-11-20 14:21:13 +0900

    refactor(마이페이지/커리어보드): 컴포넌트 폴더 내에 career-board에서 작업하도록 변경
    - 기존의 career는 포괄적이라 명시적으로 변경

--- src/app/(user)/mypage/career/board/page.tsx
+++ src/app/(user)/mypage/career/board/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import CareerBoard from '@components/career/pages/CareerBoard';
+import CareerBoard from '@components/career-board';
 
 export default function Page() {
   return <CareerBoard />;
--- src/components/career-board/index.tsx
+++ src/components/career-board/index.tsx
@@ -1,9 +1,9 @@
-import TalentPoolBanner from '../banner/TalentPoolBanner';
-import CareerGrowthSection from '../sections/CareerGrowthSection';
-import CareerPlanSection from '../sections/CareerPlanSection';
-import CareerRecordSection from '../sections/CareerRecordSection';
-import ExperienceSection from '../sections/ExperienceSection';
-import DocumentSection from '../sections/ResumeSection';
+import TalentPoolBanner from './banner/TalentPoolBanner';
+import CareerGrowthSection from './sections/CareerGrowthSection';
+import CareerPlanSection from './sections/CareerPlanSection';
+import CareerRecordSection from './sections/CareerRecordSection';
+import ExperienceSection from './sections/ExperienceSection';
+import DocumentSection from './sections/ResumeSection';
 
 const CareerBoard = () => {
   const hasCareerData = true; // 커리어 관리 정보 데이터 존재 여부
