e1f768f83ebc26735c9cd5ba189499d9ae0a0864
yeeun426 <thsudkcla7@naver.com>
2025-11-23 21:37:34 +0900

    feat : 모달, 바텀시트 영역 외에 선택하면 닫힘

--- src/app/(user)/mypage/experience/page.tsx
+++ src/app/(user)/mypage/experience/page.tsx
@@ -94,8 +94,14 @@ const Experience = () => {
   return (
     <>
       {isDrawerOpen && (
-        <div className="animate-fade-in fixed bottom-0 left-0 right-0 top-0 z-[100] bg-black/50">
-          <div className="animate-slide-in-right absolute bottom-0 right-0 top-0 max-w-[600px] bg-white">
+        <div
+          className="fixed bottom-0 left-0 right-0 top-0 z-[100] animate-fade-in bg-black/50"
+          onClick={handleDrawerClose}
+        >
+          <div
+            className="absolute bottom-0 right-0 top-0 max-w-[600px] animate-slide-in-right bg-white"
+            onClick={(e) => e.stopPropagation()}
+          >
             <ExperienceForm
               onClose={handleDrawerClose}
               initialData={selectedExperience}
