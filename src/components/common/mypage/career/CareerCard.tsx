04e4c7cbb7d4338f70459bbff76b271591e74807
okxooxoo <okxooxoo@naver.com>
2025-11-26 19:22:49 +0900

    design(career-record): 경력 정보 카드의 수정, 삭제 버튼 모바일 버전에서 하단에 위치

--- src/components/common/mypage/career/CareerCard.tsx
+++ src/components/common/mypage/career/CareerCard.tsx
@@ -35,7 +35,7 @@ const CareerCard = ({
       <div className="flex items-center justify-between">
         <span className="text-sm text-neutral-0">{job}</span>
 
-        <div className="flex items-center gap-2 text-sm text-neutral-35">
+        <div className="hidden items-center gap-2 text-sm text-neutral-35 md:flex">
           <span className="cursor-pointer px-2" onClick={() => handleEdit(id!)}>
             수정
           </span>
@@ -65,6 +65,24 @@ const CareerCard = ({
         <span className="text-neutral-40">
           {startDate} - {endDate}
         </span>
+      </div>
+
+      <div className="mt-2.5 flex items-center gap-2 text-sm text-neutral-35 md:hidden">
+        <span className="cursor-pointer px-2" onClick={() => handleEdit(id!)}>
+          수정
+        </span>
+        <span className="h-3 w-[1px] bg-neutral-70" />
+        <span
+          className="cursor-pointer px-2"
+          onClick={() => setIsDeleteModalOpen(true)}
+        >
+          삭제
+        </span>
+        <UserCareerDeleteModal
+          isModalOpen={isDeleteModalOpen}
+          closeModal={() => setIsDeleteModalOpen(false)}
+          onDelete={handleDelete}
+        />
       </div>
     </div>
   );
