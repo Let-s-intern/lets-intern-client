73d08e2de23ae6c8234803ad8d03978d6e72e8ac
hyonun321 <hyo_nun@kakao.com>
2026-01-17 00:30:01 +0900

    fix: import 잘못가져온 것 수정

--- src/app/(user)/about/page.tsx
+++ src/app/(user)/about/page.tsx
@@ -1,20 +1,16 @@
 'use client';
 
+import AboutHeader from '@/domain/about/v1/header/AboutHeader';
 import ResultSection from '@/domain/about/v1/ResultSection';
 import CommunitySection from '@/domain/about/v1/section/CommunitySection';
-import {
-  default as ContactSection,
-  default as EndSection,
-} from '../../../domain/about/v1/section/ContactSection';
-import {
-  default as IntroSection,
-  default as PartnerSection,
-} from '../../../domain/about/v1/section/introduction/IntroSection';
-import ProblemSection from '../../../domain/about/v1/section/PartnerSection';
-import ProgramMenuSection from '../../../domain/about/v1/section/problem/ProblemSection';
-import ReviewSection from '../../../domain/about/v1/section/program/ProgramMenuSection';
-import SolutionSection from '../../../domain/about/v1/section/ReviewSection';
-import AboutHeader from '../../../domain/about/v1/section/solution/SolutionSection';
+import ContactSection from '@/domain/about/v1/section/ContactSection';
+import EndSection from '@/domain/about/v1/section/EndSection';
+import IntroSection from '@/domain/about/v1/section/introduction/IntroSection';
+import PartnerSection from '@/domain/about/v1/section/PartnerSection';
+import ProblemSection from '@/domain/about/v1/section/problem/ProblemSection';
+import ProgramMenuSection from '@/domain/about/v1/section/program/ProgramMenuSection';
+import ReviewSection from '@/domain/about/v1/section/ReviewSection';
+import SolutionSection from '@/domain/about/v1/section/solution/SolutionSection';
 
 const About = () => {
   return (
