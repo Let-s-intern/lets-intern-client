26ff4d87ef358f69699164388e7f54afea854abd
yeji424 <yejiborn424@gmail.com>
2026-01-24 14:50:31 +0900

    feat: HR 최신 챌린지 라우트 추가

---
+++ src/app/(user)/challenge/hr/latest/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import LoadingContainer from '@/common/loading/LoadingContainer';
+import { useLatestChallengeRedirect } from '@/hooks/useLatestChallengeRedirect';
+import { challengeTypeSchema } from '@/schema';
+
+const { HR } = challengeTypeSchema.enum;
+
+/**
+ * HR 챌린지의 latest 리다이렉트를 처리하는 컴포넌트
+ *
+ * 리다이렉트 우선순위:
+ * 1. 모집중인(active) HR 챌린지 중 B2C 챌린지가 있을 경우 해당 챌린지로 이동
+ * 2. 없을 경우 노출된 챌린지 중 활성화되지 않은 가장 최근 B2C 챌린지로 이동
+ */
+export default function HrLatest() {
+  useLatestChallengeRedirect(HR);
+
+  return (
+    <LoadingContainer className="min-h-screen" text="HR 챌린지로 이동 중..." />
+  );
+}
