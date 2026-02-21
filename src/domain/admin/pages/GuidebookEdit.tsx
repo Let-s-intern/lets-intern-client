ccb6e940182ff2df7a292949767f32ad5f882629
yeji424 <yejiborn424@gmail.com>
2026-02-21 11:35:34 +0900

    feat: 프로그램 관리에 가이드북 등록 버튼 및 admin 라우트 추가

---
+++ src/domain/admin/pages/GuidebookEdit.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import Header from '@/domain/admin/ui/header/Header';
+import Heading from '@/domain/admin/ui/heading/Heading';
+
+const GuidebookEdit = () => {
+  return (
+    <div className="mx-3 mb-40 mt-3">
+      <Header>
+        <Heading>가이드북 수정</Heading>
+      </Header>
+    </div>
+  );
+};
+
+export default GuidebookEdit;
