c3ef9c7ec45c7b8c521ec767b8a38d76d4c92b7e
yeji424 <yejiborn424@gmail.com>
2026-03-24 21:55:28 +0900

    fix: 어드민 자료정보 필수값으로 처리
    - 입력값 없으면 저장버튼 비활

--- src/domain/admin/pages/GuidebookCreate.tsx
+++ src/domain/admin/pages/GuidebookCreate.tsx
@@ -96,7 +96,7 @@ const GuidebookCreate: React.FC = () => {
           <FormSection title="가격 정보">
             <GuidebookPriceSection input={input} setInput={setInput} />
           </FormSection>
-          <FormSection title="자료 정보">
+          <FormSection title="자료 정보" required>
             <GuidebookResourceSection
               input={input}
               setInput={setInput}
@@ -120,7 +120,12 @@ const GuidebookCreate: React.FC = () => {
         <Button
           variant="contained"
           color="primary"
-          disabled={loading}
+          disabled={
+            loading ||
+            (resourceSource === 'url'
+              ? !input.contentUrl
+              : !input.contentFileUrl)
+          }
           startIcon={<FaSave size={12} />}
           onClick={onClickSave}
         >
