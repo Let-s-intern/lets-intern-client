c50cbf23bd8f0c99f5d6f0d6b4a102a7aa37c498
yeji424 <yejiborn424@gmail.com>
2026-04-06 09:00:26 +0900

    refactor: 코드리뷰 반영

--- src/domain/blog/ui/CheckBox.tsx
+++ src/domain/blog/ui/CheckBox.tsx
@@ -2,6 +2,9 @@
 
 import { ReactNode, useState } from 'react';
 
+const CHECKED_ICON_PATH = '/icons/checkbox-checked.svg';
+const UNCHECKED_ICON_PATH = '/icons/checkbox-unchecked-box3.svg';
+
 interface CheckBoxProps {
   initialChecked: boolean;
   children: ReactNode;
@@ -18,15 +21,11 @@ const CheckBox = ({ initialChecked, children }: CheckBoxProps) => {
         role="checkbox"
         className="mt-[2px] h-5 w-5 shrink-0"
         aria-checked={checked}
-        aria-label={checked ? '체크 해제' : '체크'}
+        aria-label="체크"
       >
         <img
-          src={
-            checked
-              ? '/icons/checkbox-checked.svg'
-              : '/icons/checkbox-unchecked-box3.svg'
-          }
-          alt={checked ? '체크' : '체크 안 함'}
+          src={checked ? CHECKED_ICON_PATH : UNCHECKED_ICON_PATH}
+          alt=""
           className="h-5 w-5"
         />
       </button>
