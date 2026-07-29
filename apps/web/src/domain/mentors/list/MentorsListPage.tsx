a83cdee11a92b2614cd6cc2dec84202549092ee6
yeji424 <yejiborn424@gmail.com>
2026-07-29 15:28:36 +0900

    feat: 멘토 목록 페이지 배너 섹션 추가

---
+++ apps/web/src/domain/mentors/list/MentorsListPage.tsx
@@ -0,0 +1,11 @@
+import BannerSection from './BannerSection';
+
+const MentorsListPage = () => {
+  return (
+    <main className="flex w-full flex-col">
+      <BannerSection />
+    </main>
+  );
+};
+
+export default MentorsListPage;
