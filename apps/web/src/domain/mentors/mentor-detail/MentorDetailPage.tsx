2c24526fe52259b6606930163742d32aa95a6c06
yeji424 <yejiborn424@gmail.com>
2026-07-30 18:59:48 +0900

    feat: 멘토 상세페이지 소개 섹션 추가

--- apps/web/src/domain/mentors/mentor-detail/MentorDetailPage.tsx
+++ apps/web/src/domain/mentors/mentor-detail/MentorDetailPage.tsx
@@ -1,5 +1,6 @@
 import { DUMMY_MENTOR_DETAIL } from '../data/dummyMentorDetail';
 import MentorHeroSection from './MentorHeroSection';
+import MentorIntroSection from './MentorIntroSection';
 
 interface MentorDetailPageProps {
   mentorId: string;
@@ -9,8 +10,9 @@ const MentorDetailPage = (_props: MentorDetailPageProps) => {
   const mentor = DUMMY_MENTOR_DETAIL;
 
   return (
-    <main className="flex w-full flex-col">
+    <main className="mx-auto flex w-full max-w-[1120px] flex-col gap-20 px-5 py-10 md:mb-14 md:px-0">
       <MentorHeroSection mentor={mentor} />
+      <MentorIntroSection mentor={mentor} />
     </main>
   );
 };
